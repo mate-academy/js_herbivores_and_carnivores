c14d1f24629882a9b394c8558b85217f76065068
Victor-Zinchenko-mate <65622349+Victor-Zinchenko-mate@users.noreply.github.com>
2020-07-29 17:16:31 +0300

    SolutionClass

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,40 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+  }
+  static removeAnimal(dead) {
+    this.alive = this.alive.filter(animal => animal !== dead);
+  }
 }
-
+Animal.alive = [];
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    Animal.alive.push(this);
+    this.hidden = false;
+  }
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    Animal.alive.push(this);
+  }
+  bite(kind) {
+    if (kind instanceof Herbivore && !kind.hidden) {
+      kind.health -= 50;
+    }
+
+    if (kind.health === 0) {
+      Animal.removeAnimal(kind);
+    }
+  }
 }
 
 module.exports = {
