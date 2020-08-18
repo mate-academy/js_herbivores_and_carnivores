cccc486a3f4508508eb8d5638b50389a57ee9901
artsashko <artyomsashko@gmail.com>
2020-08-18 18:10:39 +0300

    Optimized

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -4,41 +4,29 @@ class Animal {
   constructor(name) {
     this.name = name;
     this.health = 100;
+    Animal.alive.push(this);
   }
 }
 
 Animal.alive = [];
 
 class Herbivore extends Animal {
-  constructor(name, health) {
-    super(name, health);
+  constructor() {
+    super();
     this.hidden = false;
-    Animal.alive.push(this);
   }
 
   hide() {
-    if (this.hidden) {
-      this.hidden = false;
-    } else {
-      this.hidden = true;
-    }
+    this.hidden = !this.hidden;
   }
 }
 
 class Carnivore extends Animal {
-  constructor(name, health) {
-    super(name, health);
-    this.hidden = false;
-    Animal.alive.push(this);
-  }
   bite(herbivore) {
     if (herbivore instanceof Herbivore && herbivore.hidden === false) {
       herbivore.health -= 50;
     }
-
-    if (herbivore.health === 0) {
-      Animal.alive = Animal.alive.filter(animal => animal.health > 0);
-    }
+    Animal.alive = Animal.alive.filter(animal => animal.health > 0);
   }
 }
 
