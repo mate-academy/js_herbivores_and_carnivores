acb3d5defdce5dc2bad12b28c46d07cb9e35297d
Zarva <zarva6596@gmail.com>
2020-08-19 16:09:44 +0300

    first commit

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,36 @@
 'use strict';
 
 class Animal {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
   // write your code here
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  bite(herb) {
+    if (herb instanceof Herbivore && herb.hidden === false) {
+      herb.health -= 50;
+
+      if (herb.health === 0) {
+        const i = Animal.alive.indexOf(herb);
+
+        Animal.alive.splice(i, 1);
+      }
+    }
+  }
 }
 
 module.exports = {
