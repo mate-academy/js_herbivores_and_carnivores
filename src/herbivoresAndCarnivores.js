f22a297a045c7279793db86bc6453eb45aa6a340
dmitry <akulinind@gmail.com>
2020-10-05 18:59:20 +0300

    add Carnivore class

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,13 +1,14 @@
 'use strict';
 
 class Animal {
-  // static alive = [];
-
   constructor(name) {
     this.name = name;
     this.health = 100;
+    Animal.alive.push(this);
   }
 }
+
+Animal.alive = [];
 
 class Herbivore extends Animal {
   constructor(name) {
@@ -20,7 +21,15 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (animal instanceof Herbivore && animal.hidden === false) {
+      animal.health -= 50;
+    }
+
+    if (animal.health <= 0) {
+      Animal.alive = Animal.alive.filter(item => item !== animal);
+    }
+  }
 }
 
 module.exports = {
