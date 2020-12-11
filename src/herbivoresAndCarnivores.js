14a7da152f48ad68d56c91b1042461e92fde2ca5
AndrewKreshchenko <mail2andyk@gmail.com>
2020-12-11 20:38:06 +0200

    Make fixes and improvements

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,22 +1,20 @@
 'use strict';
 
 class Animal {
-  constructor(name) {
+  constructor(name, health = 100) {
     this.name = name;
-    this.health = 100;
+    this.health = health;
+    Animal.alive.push(this);
   }
 }
 
 Animal.alive = [];
 
 class Herbivore extends Animal {
-  constructor(name) {
-    super(name);
+  constructor(name, health = 100) {
+    super(name, health);
 
-    this.health = 100;
     this.hidden = false;
-
-    Animal.alive.push(this);
   }
 
   hide() {
@@ -25,16 +23,8 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
-  constructor(name) {
-    super(name);
-
-    this.health = 100;
-
-    Animal.alive.push(this);
-  }
-
   bite(obj) {
-    if (obj instanceof Herbivore === true && !obj.hidden) {
+    if (obj instanceof Herbivore && !obj.hidden) {
       obj.health -= 50;
     }
 
