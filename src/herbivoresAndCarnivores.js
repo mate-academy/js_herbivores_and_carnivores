9733dc7beede77f13d1948fe56454deee4bee227
ShyianO <ol.shyian@gmacil.com>
2021-01-08 11:03:25 +0200

    Fixed solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -23,7 +23,7 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(target) {
-    if (target.hidden === false) {
+    if (target.hidden === false && target instanceof Herbivore) {
       target.health -= 50;
     }
 
