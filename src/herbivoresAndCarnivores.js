a904e3f7d2630fbd4cd50a322fbadd20291beffa
Oleksandr Vasilkov <vasilkoff1337@gmail.com>
2021-01-16 19:42:31 +0200

    after code review

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -4,14 +4,11 @@ class Animal {
   constructor(name) {
     this.health = 100;
     this.name = name;
-    this.index = Animal.index;
     Animal.alive.push(this);
-    Animal.index++;
   }
 }
 
 Animal.alive = [];
-Animal.index = 0;
 class Herbivore extends Animal {
   constructor(name) {
     super(name);
@@ -19,21 +16,18 @@ class Herbivore extends Animal {
   }
 
   hide() {
-    if (this.hidden === false) {
-      this.hidden = true;
-    } else {
-      this.hidden = false;
-    };
+    this.hidden = !this.hidden;
   }
 }
 
 class Carnivore extends Animal {
   bite(obj) {
-    if (obj.hidden === false && obj instanceof Herbivore) {
+    if (!obj.hidden && obj instanceof Herbivore) {
       obj.health -= 50;
 
       if (obj.health === 0) {
-        Animal.alive.splice(obj.index, obj.index);
+        Animal.alive = Animal.alive.filter(animal => animal !== obj);
+        // Animal.alive.filter(animal => animal.health > 0);
       }
     }
   }
