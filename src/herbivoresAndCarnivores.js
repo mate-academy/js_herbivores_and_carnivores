3451ada79c6b6d9fb03228cf62d3d08915689a24
Yurs Chabarenko <yurschabarenko@Yurss-MacBook-Pro.local>
2021-03-04 08:30:17 +0200

    first commit

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,37 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
+Animal.alive = [];
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (animal.hidden === false) {
+      animal.health = animal.health - 50;
+    }
+
+    Animal.alive = Animal.alive.filter(
+      isAlive => isAlive.health > 0
+    );
+
+    return animal;
+  }
 }
 
 module.exports = {
