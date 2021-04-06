66bd5179e01953e19df554b3076c8296996151ba
Mariia Kornieva <mariiakornieva@gmail.com>
2021-04-06 08:11:54 +0300

    Fix review findings

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -4,6 +4,7 @@ class Animal {
   constructor(name) {
     this.name = name;
     this.health = 100;
+    Animal.alive.push(this);
   }
 }
 
@@ -13,7 +14,6 @@ class Herbivore extends Animal {
   constructor(name) {
     super(name);
     this.hidden = false;
-    Animal.alive.push(this);
   }
 
   hide() {
@@ -22,20 +22,13 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
-  constructor(name) {
-    super(name);
-    Animal.alive.push(this);
-  }
-
   bite(herbivore) {
-    if (Animal.alive.includes(herbivore)) {
-      if (herbivore instanceof Herbivore && !herbivore.hidden) {
-        herbivore.health -= 50;
-      }
+    if (herbivore instanceof Herbivore && !herbivore.hidden) {
+      herbivore.health -= 50;
+    }
 
-      if (herbivore.health === 0) {
-        Animal.alive.splice(Animal.alive.indexOf(herbivore), 1);
-      }
+    if (herbivore.health === 0) {
+      Animal.alive.splice(Animal.alive.indexOf(herbivore), 1);
     }
   }
 }
