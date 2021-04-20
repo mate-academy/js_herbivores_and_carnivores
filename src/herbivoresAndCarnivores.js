441257fa719c1f5984768d36c2f9814e7683903b
Kostrica <romiorw@ukr.net>
2021-04-20 18:19:13 +0300

    fixed to "hidden" in "herbivore"

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -4,12 +4,16 @@ class Animal {
   constructor(name) {
     this.name = name;
     this.health = 100;
-    this.hidden = false;
     Animal.alive.push(this);
   }
 }
 
 class Herbivore extends Animal {
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
   hide() {
     this.hidden = true;
   }
