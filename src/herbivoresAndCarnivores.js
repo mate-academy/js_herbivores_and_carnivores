0142ce179bbc0a4ff2b1ebc102f775a70a22e813
Ivan Major <tarstarcas@gmail.com>
2021-04-21 15:15:41 +0300

    Animals

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,40 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  };
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  };
+
+  hide() {
+    this.hidden = (this.hidden === false)
+      ? this.hidden = true
+      : this.hidden = false;
+  };
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(prey) {
+    if (prey instanceof Herbivore && prey.hidden === false) {
+      if (prey.health > 50) {
+        prey.health -= 50;
+      } else {
+        const ind = Animal.alive.indexOf(prey);
+
+        Animal.alive.splice(ind, 1);
+      };
+    };
+  };
 }
 
 module.exports = {
