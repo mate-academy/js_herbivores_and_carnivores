962147c10f53d3fa45947acb1593eeff3caadc3e
Andriy Fesych <andriyhimself@gmail.com>
2021-05-10 00:58:04 +0300

    Solution_0.1

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,38 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) { // Почему eslint не дает мне написать
+    super(name); // здесь просто hidden = false, без этого
+    this.hidden = false; // тупого конструктора
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(victim) {
+    if ((victim instanceof Herbivore) && (victim.hidden === false)) {
+      victim.health -= 50;
+
+      if (victim.health <= 0) {
+        const removeIndex = Animal.alive.indexOf(victim);
+
+        Animal.alive.splice(removeIndex, 1);
+      }
+    }
+  }
 }
 
 module.exports = {
