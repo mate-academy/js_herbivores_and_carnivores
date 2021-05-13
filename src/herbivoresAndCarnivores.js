45c13c93c0b6629354ea867c01aca7387e920a22
Bogdan Kotsupey <80522234+Bogdan-Kotsupey@users.noreply.github.com>
2021-05-14 02:08:35 +0300

    ver 2.1

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -18,10 +18,12 @@ class Herbivore extends Animal {
   }
 }
 
+Herbivore.prototype.hidden = false;
+
 class Carnivore extends Animal {
   bite(animal) {
-    if (animal.hidden === false) {
-      animal.health = animal.health - 50;
+    if (animal instanceof Herbivore && animal.hidden !== true) {
+      animal.health -= 50;
     }
 
     Animal.alive = Animal.alive.filter(element => element.health > 0);
