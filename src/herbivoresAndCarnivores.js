66983edc1527c02140860c036f967f4749ed23e6
Bogdan Kotsupey <80522234+Bogdan-Kotsupey@users.noreply.github.com>
2021-05-14 00:06:46 +0300

    ready for check

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,35 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = []; // eslint-disable-line no-use-before-define
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hidden = false;
+  constructor(name, health = 100) {
+    super(name, health = 100);
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+    return this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (animal.hidden === false) {
+      animal.health = animal.health - 50;
+    }
+
+    Animal.alive = Animal.alive.filter( element => element.health > 0)
+  }
 }
 
 module.exports = {
