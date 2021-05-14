957a26809e791d020612e3543577d34129b30631
Anastasiya <anastasiya.triumf@gmail.com>
2021-05-14 15:54:01 +0300

    fixed

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -4,6 +4,7 @@ class Animal {
   constructor(name, health = 100) {
     this.name = name;
     this.health = health;
+    Animal.alive.push(this);
   }
 }
 
@@ -13,31 +14,20 @@ class Herbivore extends Animal {
   constructor(name, health) {
     super(name, health);
     this.hidden = false;
-    Animal.alive.push(this);
   }
-  hide(hidden) {
-    if (!hidden) {
-      this.hidden = true;
-    } else {
-      this.hidden = false;
-    }
+  hide() {
+    this.hidden = !this.hidden;
   }
 }
 
 class Carnivore extends Animal {
-  constructor(name) {
-    super(name);
-    Animal.alive.push(this);
-  }
   bite(prey) {
     if (prey instanceof Herbivore && !prey.hidden) {
       prey.health -= 50;
     }
 
     if (prey.health === 0) {
-      const index = Animal.alive.indexOf(prey);
-
-      Animal.alive.splice(index, 1);
+      Animal.alive = Animal.alive.filter(animal => animal.health > 0);
     }
   }
 }
