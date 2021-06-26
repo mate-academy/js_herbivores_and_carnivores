41d1ba325bce5e2e8856651f38a76bbd085ac253
Андрій Наливайко <andrijnalivajko@MacBook-Pro-Andrij.local>
2021-06-26 17:09:30 +0300

    Sample

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,45 @@
 'use strict';
 
 class Animal {
-  // write your code here
-}
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+  };
+};
 
 class Herbivore extends Animal {
-  // write your code here
-}
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+    Animal.alive.push(this);
+  };
+
+  hide() {
+    this.hidden = true;
+  };
+};
 
 class Carnivore extends Animal {
-  // write your code here
-}
+  constructor(name) {
+    super(name);
+    this.carnivore = true;
+    Animal.alive.push(this);
+  };
+
+  bite(victim) {
+    if (!victim.hidden && !victim.carnivore) {
+      victim.health -= 50;
+    };
+
+    if (victim.health === 0) {
+      const i = Animal.alive.indexOf(victim);
+
+      delete Animal.alive[i];
+    }
+  };
+};
 
 module.exports = {
   Animal,
