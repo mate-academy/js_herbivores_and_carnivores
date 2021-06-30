8049c402e42021262bc4c5d0dae1918b1e52beac
Artem Yemelyanov <ae1666528@gmail.com>
2021-06-30 17:00:50 +0300

    Implemented animals hierarchy

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,51 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+Animal.animalTimeToDie = 0;
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+
+    this.damage = 50;
+  }
+
+  bite(herb) {
+    if ((herb instanceof Carnivore) || herb.hidden) {
+      return;
+    }
+
+    herb.health -= this.damage;
+
+    if (herb.health === Animal.animalTimeToDie) {
+      const animalToDieIdx = Animal.alive.findIndex(animal => {
+        return herb === animal;
+      });
+
+      Animal.alive.splice(animalToDieIdx, 1);
+    }
+  }
 }
 
 module.exports = {
