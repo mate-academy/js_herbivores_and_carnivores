153149f465344db54fe4a90b2bbf91dc1665b2d9
Oleksii Lymonov <alex25_92@ukr.net>
2021-07-03 19:11:02 +0300

    first_version

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,43 @@
 'use strict';
 
+const biteTakesAway = 50;
+const fullHealth = 100;
+
 class Animal {
   // write your code here
+  constructor(name, health = fullHealth) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
+
+Animal.alive = [];
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  bite(animal) {
+    if (animal instanceof Herbivore
+      && animal.hidden === false) {
+      animal.health -= biteTakesAway;
+    }
+
+    if (animal.health <= 0) {
+      Animal.alive.splice(Animal.alive.indexOf(animal), 1);
+    }
+  }
 }
 
 module.exports = {
