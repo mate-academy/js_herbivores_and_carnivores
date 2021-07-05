b9c7f5c8f163feba50b8612fa97ba520953002da
Artem Dzhereleiko <dgerik2015@gmail.com>
2021-07-05 12:40:37 +0300

    fix coments

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -4,17 +4,14 @@ class Animal {
   constructor(name) {
     this.name = name;
     this.health = 100;
+    Animal.alive.push(this);
   }
 
   static alive = [];
 }
 
 class Herbivore extends Animal {
-  constructor(name) {
-    super(name);
-    this.hidden = false;
-    Animal.alive.push(this);
-  }
+  hidden = false;
 
   hide() {
     this.hidden = !this.hidden;
@@ -22,18 +19,13 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
-  constructor(name) {
-    super(name);
-    Animal.alive.push(this);
-  }
-
   bite(animal) {
-    if (animal.hidden === false) {
+    if (!animal.hidden && animal instanceof Herbivore) {
       animal.health -= 50;
     }
 
-    if (animal.health === 0) {
-      Animal.alive.splice(Animal.alive.indexOf(animal), 1);
+    if (animal.health <= 0) {
+      Animal.alive = Animal.alive.filter(item => item.health > 0);
     }
   }
 }
