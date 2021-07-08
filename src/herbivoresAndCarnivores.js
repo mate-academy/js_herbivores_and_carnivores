9325261dd87bf99c8e4572ccc50099d5e43d1aa5
Oleksii Blezniuk <oleksii.blezniuk@gmail.com>
2021-07-08 19:24:15 +0300

    task is completed

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,7 +1,6 @@
 'use strict';
 
 class Animal {
-  // write your code here
   constructor(name) {
     this.name = name;
     this.health = 100;
@@ -12,7 +11,6 @@ class Animal {
 Animal.alive = [];
 
 class Herbivore extends Animal {
-  // write your code here
   constructor(name) {
     super(name);
     this.hidden = false;
@@ -24,7 +22,6 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
-  // write your code here
   bite(animal) {
     if (animal instanceof Herbivore && !animal.hidden) {
       animal.health -= 50;
