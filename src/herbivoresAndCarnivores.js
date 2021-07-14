5da6f29f5ce5c925346af50220630d2fc9c8e927
Artem Krulykovskyi <krulikovskiy.a@gmail.com>
2021-07-14 10:55:44 +0300

    replaced property hiden to Herbivore

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -6,12 +6,16 @@ class Animal {
   constructor(name) {
     this.name = name;
     this.health = 100;
-    this.hidden = false;
     Animal.alive.push(this);
   }
 }
 
 class Herbivore extends Animal {
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
   hide() {
     this.hidden = !this.hidden;
   }
