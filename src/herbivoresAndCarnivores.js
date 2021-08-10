14ecdc1b1df182ce2d671068df0fba7f1956091d
Alexandr Somov <aleksandr.somov2011@gmail.com>
2021-08-10 23:28:40 +0300

    'Solution'

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,35 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
+  hide() {
+    this.hidden = this.hidden !== true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    Animal.alive.push(this);
+  }
+  bite(herbivore) {
+    if (herbivore instanceof Herbivore && !herbivore.hidden) {
+      herbivore.health = herbivore.health - 50;
+    }
+    Animal.alive = Animal.alive.filter(animal => animal.health > 0);
+  }
 }
 
 module.exports = {
