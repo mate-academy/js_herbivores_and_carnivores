cfc4c59c50c6dc6328ae7b0fab7b083769ed9a37
Sasha Lifashkin <alifashkin@outlook.com>
2021-08-14 17:02:15 +0300

    done

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,44 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+
+    this.hidden = false;
+    this.kind = 'herbivore';
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+
+    this.kind = 'carnivore';
+  }
+
+  bite(animal) {
+    if (animal.kind === 'herbivore' && !animal.hidden) {
+      animal.health -= 50;
+    }
+
+    if (!animal.health) {
+      Animal.alive.sort((a, b) => b.health - a.health).pop();
+    }
+  }
 }
 
 module.exports = {
