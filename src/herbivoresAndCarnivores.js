009dc86f12838591ab0bf256955509d970d1a62b
Olesya Parfylo <olesya.parfylo@gmail.com>
2021-08-15 18:48:53 +0300

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,39 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite() {
+    function callback(animal) {
+      if (animal.hidden === false) {
+        animal.health = animal.health - 50;
+      }
+
+      return animal.health !== 0;
+    }
+
+    Animal.alive = Animal.alive.map(callback);
+  }
 }
 
 module.exports = {
