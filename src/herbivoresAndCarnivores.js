ff08f906fd33a3b1739c58e6c6da363747aba3fc
Oleksandr Morozov <oleksandr.morozov@protonmail.com>
2021-08-19 17:40:14 +0300

    mentors suggestions have been implemented

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -3,16 +3,8 @@
 class Animal {
   static alive = [];
 
-  static removeDeadAnimal() {
-    const deadAnimal = Animal.alive.find(animal => (
-      animal.health === 0
-    ));
-
-    const index = Animal.alive.findIndex(animal => (
-      animal === deadAnimal
-    ));
-
-    Animal.alive.splice(index, 1);
+  static checkingForAliveAnimals() {
+    Animal.alive = Animal.alive.filter(animal => animal.health > 0);
   }
 
   constructor(name, health = 100) {
@@ -31,20 +23,16 @@ class Herbivore extends Animal {
   }
 
   hide() {
-    if (this.hidden === true) {
-      this.hidden = false;
-    } else {
-      this.hidden = true;
-    }
+    this.hidden = !this.hidden;
   }
 }
 
 class Carnivore extends Animal {
   bite(animal) {
-    if (animal instanceof Herbivore && animal.hidden === false) {
+    if (animal instanceof Herbivore && !animal.hidden) {
       animal.health -= 50;
 
-      Animal.removeDeadAnimal();
+      Animal.checkingForAliveAnimals();
     }
   }
 }
