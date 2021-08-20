583014ffff05319a615cf3e82f1ed18cd1bbcfc4
Andrii Hryhoruk <andriihryhoruk5544@gmail.com>
2021-08-20 11:28:38 +0300

    save

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -29,9 +29,7 @@ class Carnivore extends Animal {
       animal.health -= 50;
     }
 
-    if (animal.health <= 0) {
-      Animal.alive.splice(Animal.alive.indexOf(animal), 1);
-    }
+    Animal.alive = Animal.alive.filter(beast => beast.health > 0);
   }
 }
 
