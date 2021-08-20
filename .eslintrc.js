2df5d8ae84ddedf1d8e720ccc537279110eb025b
Andrii Serhovetc <serhovetc88@gmail.com>
2021-08-20 18:12:05 +0300

    solution done

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,9 @@ module.exports = {
   env: {
     jest: true
   },
-  parser: 'babel-eslint',
   rules: {
     'no-proto': 0
   },
-  plugins: ['jest']
+  plugins: ['jest'],
+  parser: "babel-eslint",
 };
