9fc8dab56d7b6952df9bb0f9d8be21d7dd27d22c
Taras Holiuk <taras.holiuk@gmail.com"dsveagj 3rm>
2021-08-20 15:51:50 +0300

    instanceof added, filter changed

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -14,7 +14,6 @@ class Herbivore extends Animal {
   constructor(name) {
     super(name);
 
-    this.type = 'herbivore';
     this.hidden = false;
   }
 
@@ -24,20 +23,13 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
-  constructor(name) {
-    super(name);
-
-    this.type = 'carnivore';
-  }
-
   bite(animal) {
-    if (animal.type === 'herbivore' && animal.hidden === false) {
+    if (animal instanceof Herbivore && animal.hidden === false) {
       animal.health -= 50;
     }
 
     if (animal.health === 0) {
-      Animal.alive = Animal.alive.filter((beast) =>
-        animal.name !== beast.name);
+      Animal.alive = Animal.alive.filter((beast) => beast.health > 0);
     }
   }
 }
