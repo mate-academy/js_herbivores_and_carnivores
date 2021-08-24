d2cbfbfd20de3f3fb902fb18fe8a0861d498fe40
Igor <2492345@gmail.com>
2021-08-24 15:22:24 +0300

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -14,8 +14,10 @@ Animal.alive = [];
 class Herbivore extends Animal {
   constructor(name) {
     super(name);
+
     this.hidden = false;
   }
+
   hide() {
     this.hidden = !this.hidden;
   }
@@ -25,10 +27,10 @@ class Carnivore extends Animal {
   bite(animal) {
     if (animal instanceof Herbivore && !animal.hidden) {
       animal.health -= 50;
-    }
 
-    if (animal.health === 0) {
-      Animal.alive = Animal.alive.filter(creature => creature.health > 0);
+      if (animal.health === 0) {
+        Animal.alive = Animal.alive.filter(creature => creature.health > 0);
+      }
     }
   }
 }
