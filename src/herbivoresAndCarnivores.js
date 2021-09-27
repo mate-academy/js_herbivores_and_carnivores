2f020d40309d3bd166de6ecfc379f5c97b4bf054
Roman Prokopiv <rprokopiv@gmail.com>
2021-09-27 21:23:28 +0300

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,49 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health = 100, hidden = false) {
+    super(name, health);
+
+    this.hidden = hidden;
+  }
+
+  hide() {
+    if (this.hidden) {
+      this.hidden = false;
+    } else {
+      this.hidden = true;
+    }
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+  }
+
+  bite(victim) {
+    if (victim instanceof Herbivore) {
+      if (!victim.hidden) {
+        victim.health -= 50;
+      }
+    }
+
+    for (let i = 0; i < Animal.alive.length; i++) {
+      if (Animal.alive[i].health <= 0) {
+        Animal.alive.splice(i, 1);
+      }
+    }
+  }
 }
 
 module.exports = {
