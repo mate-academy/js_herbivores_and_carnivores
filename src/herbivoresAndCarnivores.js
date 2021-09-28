7048bee411f56c6311be8e6c22a1e82ed1b3d152
Vladyslav Banul <vladyslav.banul@gmail.com>
2021-09-28 19:35:04 +0300

    IsDrozdovHere?

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,47 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    if (!Animal.alive) {
+      Animal.alive = [];
+    }
+    Animal.alive.push(this);
+  }
+
+  removeCorpse(name) {
+    Animal.alive.splice(
+      Animal.alive.findIndex((animal) => animal.name === name) - 1, 1);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+    this.type = 'herbivore';
+  }
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.type = 'carnivore';
+  }
+  bite(victim) {
+    if (!victim.hidden && victim.type === 'herbivore') {
+      victim.health -= 50;
+    }
+
+    if (victim.health <= 0) {
+      victim.removeCorpse(victim.name);
+    }
+  }
 }
 
 module.exports = {
