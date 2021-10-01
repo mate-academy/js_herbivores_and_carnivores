c5d7f73fb6d7748df61a8edbc031afc5d5f48add
Oleksandr Krasnoshchokov <krasnoshchokovoleksandr@gmail.com>
2021-10-01 23:37:59 +0300

    Done

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,44 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+
+    this.hidden = false;
+
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+
+    Animal.alive.push(this);
+  }
+
+  bite(target) {
+    if (!target.hidden && !(target instanceof Carnivore)) {
+      target.health -= 50;
+    }
+
+    if (!target.health) {
+      Animal.alive = Animal.alive.filter(item => item.name !== target.name);
+    }
+  }
 }
 
 module.exports = {
