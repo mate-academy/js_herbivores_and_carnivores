2a9bff797b77a94cd693f7b9e21df92e2be1bd12
Roman Toritsyn <romantoritsyn@gmail.com>
2021-10-04 19:03:30 +0300

    2 versions

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,45 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
+
+  // static cleanList() {
+  //   Animal.alive = Animal.alive.filter(animal => animal.health > 0);
+  // }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  };
+
+  hide() {
+    if (this.hidden === true) {
+      this.hidden = false;
+    } else {
+      this.hidden = true;
+    }
+  };
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(prey) {
+    if (!(prey instanceof Carnivore) && prey.hidden === false) {
+      prey.health -= 50;
+    }
+    // Animal.cleanList();
+
+    if (prey.health <= 0) {
+      Animal.alive = Animal.alive.filter(animal => animal.name !== prey.name);
+    }
+  }
 }
 
 module.exports = {
