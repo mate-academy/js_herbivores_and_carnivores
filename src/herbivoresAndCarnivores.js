f698ff6f3cd5edada6ce7675635543bd91aa6f23
Liliia Kuleba <kuleba.lilia.1872003@gmail.com>
2021-10-04 19:58:44 +0300

    solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -27,7 +27,9 @@ class Carnivore extends Animal {
       victim.health -= 50;
     }
 
-    Animal.alive = Animal.alive.filter(animal => animal.health > 0);
+    if (victim.health <= 0) {
+      Animal.alive = Animal.alive.filter(animal => animal.health > 0);
+    }
   }
 }
 
