36b3d975ffa7c5d8e3936a65c6183074cac84a36
Serhii Dvorets <dvorets.sergey@gmail.com>
2021-10-05 22:26:32 +0300

    1

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,47 @@
 'use strict';
 
+
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.health = health;
+    this.name = name;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(health, name, hidden = false) {
+    super(health, name);
+    this.hidden = hidden;
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    if (this.hidden === true) {
+      this.hidden = false;
+    } else if (this.hidden === false) {
+      this.hidden = true;
+    }
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(health, name) {
+    super(health, name);
+    Animal.alive.push(this);
+  }
+
+  bite(name) {
+    let index = Animal.alive.findIndex(creature => creature === name);
+    if (Animal.alive[index].hasOwnProperty('hidden') && Animal.alive[index].hidden === false) {
+      Animal.alive[index].health -= 50;
+    }
+
+    if (Animal.alive[index].health === 0) {
+      Animal.alive.splice(index, 1);
+    }
+  }
 }
 
 module.exports = {
