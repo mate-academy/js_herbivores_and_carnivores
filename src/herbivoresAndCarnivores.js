321127c58b23450a80159303136d169764e34d20
ruslan-bondarenko <ruslan21bondarenko@gmail.com>
2021-10-05 11:21:48 +0300

    fix02

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,40 +1,42 @@
 'use strict';
 
 class Animal {
-  static getAliveAnimals() {
-    this.alive = this.alive.filter(animal => animal.health > 0);
-  }
-
   constructor(name) {
     this.name = name;
     this.health = 100;
-
-    Animal.alive.push(this);
   }
 }
 
 Animal.alive = [];
 
 class Herbivore extends Animal {
-  constructor(name, hide) {
+  constructor(name) {
     super(name);
 
     this.hidden = false;
+
+    Animal.alive.push(this);
   }
 
   hide() {
-    this.hidden = true;
+    this.hidden = !this.hidden;
   }
 }
 
 class Carnivore extends Animal {
+  constructor(name) {
+    super(name);
+
+    Animal.alive.push(this);
+  }
+
   bite(animal) {
-    if (animal instanceof Herbivore && animal.hidden === false) {
+    if (!animal.hidden && animal instanceof Herbivore) {
       animal.health -= 50;
     }
 
     if (animal.health <= 0) {
-      Animal.getAliveAnimals();
+      Animal.alive = Animal.alive.filter(item => item.name !== animal.name);
     }
   }
 }
