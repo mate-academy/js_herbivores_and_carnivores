1c0eb76c5c94b5bf5ec47bacaa72b9472a9eaaa9
Roman Toritsyn <romantoritsyn@gmail.com>
2021-10-05 12:02:19 +0300

    done

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -23,13 +23,13 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(animal) {
-    if (!(animal instanceof Carnivore) && animal.hidden === false) {
+    if (animal instanceof Herbivore && !animal.hidden) {
       animal.health -= 50;
     }
 
     if (animal.health <= 0) {
       Animal.alive
-      = Animal.alive.filter(someAnimal => someAnimal.name !== animal.name);
+      = Animal.alive.filter(everyAnimal => everyAnimal.name !== animal.name);
     }
   }
 }
