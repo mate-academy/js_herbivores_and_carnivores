84bf58e7de46a5d5120e40013235213a466228fa
Oleksij Lyashenko <oleksijlyashenko@MacBook-Air-Mary.local>
2021-10-07 14:36:10 +0300

    solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,51 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hidden = false;
+
+  // eslint-disable-next-line no-useless-constructor
+  constructor(name) {
+    super(name);
+  }
+
+  hide() {
+    if (this.hidden === false) {
+      this.hidden = true;
+    } else {
+      this.hidden = false;
+    }
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  // eslint-disable-next-line no-useless-constructor
+  constructor(name) {
+    super(name);
+  }
+
+  bite(animal) {
+    Animal.alive.map((person, index) => {
+      // eslint-disable-next-line max-len
+      if (person === animal && animal.constructor.name === 'Herbivore' && person.hidden !== true) {
+        person.health -= 50;
+
+        if (person.health === 0) {
+          Animal.alive.splice(index);
+        }
+      }
+    });
+  }
 }
 
 module.exports = {
