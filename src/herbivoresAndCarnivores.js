abf2a8effdd5eb516ef8e55a808432850d421763
Oleksandr Strilchuk <virass.m@gmail.com>
2021-10-26 18:17:58 +0300

    fix bite method

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -22,7 +22,7 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(animal) {
-    if (animal.hidden === false) {
+    if (!animal.hidden && animal instanceof Herbivore) {
       animal.health -= 50;
     }
     Animal.checkHealth();
