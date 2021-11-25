40cc4eb7ccd35022a4ef4efa77e867157b94f58d
Daryna Dudar <keks9ra1286@gmail.com>
2021-11-25 17:35:42 +0200

    fixed targets

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -24,7 +24,7 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(herbivore) {
-    if (herbivore.hidden === false) {
+    if (herbivore instanceof Herbivore && herbivore.hidden === false) {
       herbivore.health -= 50;
     }
 
