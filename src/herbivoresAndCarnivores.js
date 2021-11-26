5dca9cf0eb4df42f0b6978127b3b73bc7eee2d34
Mira Tyshchenko <tyshchenkomira@gmail.com>
2021-11-26 17:04:14 +0200

    implemented task

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,35 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(harbivore) {
+    if (harbivore instanceof Herbivore && !harbivore.hidden) {
+      harbivore.health -= 50;
+    }
+
+    Animal.alive = Animal.alive.filter((animal) => animal.health <= 0);
+  }
 }
 
 module.exports = {
