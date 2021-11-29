0a6f2415e2252078d6968e820e282e70c27737e5
Mariia Bila <freiheit.maria@gmail.com>
2021-11-29 14:48:23 +0200

    some fixes

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,20 +1,19 @@
 'use strict';
 
 class Animal {
-  constructor(name, health = 100) {
+  constructor(name) {
     this.name = name;
-    this.health = health;
+    this.health = 100;
     Animal.alive.push(this);
   }
 }
 
 Animal.alive = [];
 class Herbivore extends Animal {
-  constructor(name, health, hidden = false) {
+  constructor(name, health) {
     super(name, health);
 
-    this.hidden = hidden;
-    Animal.alive.push(this);
+    this.hidden = false;
   }
 
   hide() {
@@ -28,8 +27,8 @@ class Carnivore extends Animal {
       target.health -= 50;
 
       if (target.health === 0) {
-        Animal.alive = Animal.alive.filter(animal => animal.name
-          !== target.name);
+        Animal.alive = Animal.alive.filter(animal => (animal.name
+          !== target.name));
       }
     }
   }
