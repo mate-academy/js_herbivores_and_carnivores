ad64fcbcbbe7aefd54cc60f6e004acd72e04e607
Yulia Koshkina <julyasav4uk@gmail.com>
2021-12-30 15:16:14 +0200

    first commit

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,44 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health, hidden = false) {
+    super(name, health);
+    this.hidden = hidden;
+  }
+
+  hide() {
+    if (this.hidden === false) {
+      this.hidden = true;
+    } else {
+      this.hidden = false;
+    }
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(victim) {
+    if (victim.hidden === false) {
+      victim.health -= 50;
+    }
+
+    if (victim.health === 0) {
+      const index = Animal.alive
+        .findIndex(animal => animal.health === 0);
+
+      Animal.alive.splice(index, 1);
+    }
+  }
 }
+
+Animal.alive = [];
 
 module.exports = {
   Animal,
