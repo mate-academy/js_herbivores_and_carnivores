b0b5d9664fdd1ecb93c96678a65b0814949f3881
Roma Afanasiev <romafanasiev123@gmail.com>
2022-01-30 15:04:57 +0200

    add function

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,38 @@
 
 class Animal {
   // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
 }
+
+Animal.alive = [];
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(name) {
+    super(name);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+
+  bite(herb) {
+    if (!herb.hidden && herb instanceof Herbivore === true) {
+      herb.health - 50 !== 0
+        ? herb.health -= 50
+        : delete Animal.alive.splice(herb);
+    }
+  }
 }
 
 module.exports = {
