824a3c8bf087b92e047f7fdd778277158fc829f4
Ihor Barban <barbanihor@gmial.com>
2022-02-05 13:31:06 +0200

    Done

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,33 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (!(animal instanceof Carnivore) && animal.hidden !== true) {
+      animal.health -= 50;
+    }
+    Animal.alive = Animal.alive.filter(health => health < 0);
+  };
 }
 
 module.exports = {
