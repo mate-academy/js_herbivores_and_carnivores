497727c5888bcfc7c14aefaf608ecbf67d8da453
Dorosh13 <dorosh.d.13@gmail.com>
2022-02-07 16:33:52 +0200

    fixed

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -6,6 +6,8 @@ class Animal {
   constructor(name, health = 100) {
     this.name = name;
     this.health = health;
+
+    Animal.alive.push(this);
   }
 }
 
@@ -14,20 +16,14 @@ class Herbivore extends Animal {
     super(name, health);
 
     this.hidden = false;
-    Animal.alive.push(this);
   }
 
   hide() {
-    this.hidden = true;
+    this.hidden = !this.hidden;
   }
 }
 
 class Carnivore extends Animal {
-  constructor(name, health = 100) {
-    super(name, health);
-    Animal.alive.push(this);
-  }
-
   bite(animal) {
     if (animal instanceof Herbivore && animal.hidden !== true) {
       animal.health -= 50;
