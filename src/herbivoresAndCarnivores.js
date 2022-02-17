91837499413ffcf472fa998b8b33f77e054b19e4
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-02-17 18:52:04 +0200

    herbivoresAndCarnivores script was created

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,53 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health) {
+    this.name = name;
+
+    if (!health) {
+      this.health = 100;
+    } else {
+      this.health = health;
+    }
+    this.hidden = false;
+  }
+
+  die() {
+    for (let i = 0; i < Animal.alive.length; i++) {
+      if (Animal.alive[i].health <= 0) {
+        delete Animal.alive[i];
+      }
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor() {
+    super();
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor() {
+    super();
+    Animal.alive.push(this);
+  }
+
+  bite(herbivore) {
+    if (!herbivore.hidden && herbivore instanceof Herbivore) {
+      herbivore.health -= 50;
+    }
+
+    super.die();
+  }
 }
+
+Animal.alive = [];
 
 module.exports = {
   Animal,
