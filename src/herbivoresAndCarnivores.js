630e5c4d5ca310bfdcda6545ed49af8d0fcb31c8
Yaroslav Rehulskyi <barsinvest84@gmail.com>
2022-02-19 16:31:48 +0200

    Improving code after reviewer remark

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,11 @@
 'use strict';
 
 class Animal {
-  static birth(beast) {
-    this.alive.push(beast);
-  }
-
   constructor(name) {
     this.name = name;
     this.health = 100;
 
-    Animal.birth(this);
+    Animal.alive.push(this);
   }
 }
 
@@ -28,14 +24,10 @@ class Carnivore extends Animal {
   bite(victim) {
     if (!(victim instanceof Carnivore) && !victim.hidden) {
       victim.health -= 50;
+    }
 
-      if (victim.health === 0) {
-        const index = Animal.alive.indexOf(victim);
-
-        if (index !== -1) {
-          Animal.alive.splice(index, 1);
-        }
-      }
+    if (victim.health === 0) {
+      Animal.alive.splice(Animal.alive.indexOf(victim), 1);
     }
   }
 }
