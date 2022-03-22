674f85c6045dc7275de067900e9fc5c6135cd9d8
Maria Luschik <marialyschik@Marias-MacBook-Air.local>
2022-03-22 11:56:59 +0200

    done

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,38 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(bitten) {
+    if (bitten instanceof Herbivore && !bitten.hidden) {
+      bitten.health -= 50;
+    }
+
+    if (bitten.health === 0) {
+      Animal.alive = Animal.alive.filter(animal => !bitten);
+    }
+  }
 }
+
+Animal.alive = [];
 
 module.exports = {
   Animal,
