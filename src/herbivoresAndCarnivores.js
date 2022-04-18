b97b9face47db64e87e4bcbeea4caf1e85cd7f9f
Oksana Tyshchenko <oksanasergeevna30@gmail.com>
2022-04-18 15:05:55 +0300

    HerbivoresAndCarnivores classes were created

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,47 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health, hiddenValue = false) {
+    super(name, health);
+    this.hidden = hiddenValue;
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    if (this.hidden === true) {
+      this.hidden = false;
+    } else {
+      this.hidden = true;
+    }
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    Animal.alive.push(this);
+  }
+
+  bite(victim) {
+    if (victim instanceof Herbivore && victim.hidden === false) {
+      victim.health -= 50;
+    }
+
+    if (victim.health === 0) {
+      const index = Animal.alive.indexOf(victim);
+
+      Animal.alive.splice(index, 1);
+    }
+  }
 }
 
 module.exports = {
