1336b8a498d8603116ff5b8a705c7fc191e7a703
Oksana Tyshchenko <oksanasergeevna30@gmail.com>
2022-04-18 17:26:45 +0300

    Some changes

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -4,6 +4,7 @@ class Animal {
   constructor(name, health = 100) {
     this.name = name;
     this.health = health;
+    Animal.alive.push(this);
   }
 }
 
@@ -13,33 +14,21 @@ class Herbivore extends Animal {
   constructor(name, health, hiddenValue = false) {
     super(name, health);
     this.hidden = hiddenValue;
-    Animal.alive.push(this);
   }
 
   hide() {
-    if (this.hidden === true) {
-      this.hidden = false;
-    } else {
-      this.hidden = true;
-    }
+    this.hidden = !this.hidden;
   }
 }
 
 class Carnivore extends Animal {
-  constructor(name, health) {
-    super(name, health);
-    Animal.alive.push(this);
-  }
-
   bite(victim) {
     if (victim instanceof Herbivore && victim.hidden === false) {
       victim.health -= 50;
     }
 
     if (victim.health === 0) {
-      const index = Animal.alive.indexOf(victim);
-
-      Animal.alive.splice(index, 1);
+      Animal.alive = Animal.alive.filter(animal => animal.health > 0);
     }
   }
 }
