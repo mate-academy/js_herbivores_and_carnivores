9703008dd24ddb40d2c617cc580553833eb82505
Yegor Kochetkov <kegor.fx@gmail.com>
2022-04-19 18:26:46 +0300

    Move hidden property to herbivores

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -4,7 +4,6 @@ class Animal {
   constructor(name, health = 100) {
     this.name = name;
     this.health = health;
-    this.hidden = false;
 
     Animal.alive.push(this);
   }
@@ -13,6 +12,11 @@ class Animal {
 Animal.alive = [];
 
 class Herbivore extends Animal {
+  constructor(name, health) {
+    super(name, health);
+
+    this.hidden = false;
+  }
   hide() {
     this.hidden = !this.hidden;
   }
