0047c111b858e1239e1c87f7198fbbefcdc6d39f
Alexandr Nikytenko <nika200382@gmail.com>
2022-04-21 19:12:52 +0300

    update

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,7 +1,6 @@
 'use strict';
 
 class Animal {
-  // write your code here
   constructor(name) {
     this.health = 100;
     this.name = name;
@@ -13,28 +12,24 @@ class Animal {
 Animal.alive = [];
 
 class Herbivore extends Animal {
-  // write your code here
   constructor(name) {
     super(name);
     this.hidden = false;
   }
 
   hide() {
-    this.hidden = !this.hidden;
+    this.hidden = true;
   }
 }
 
 class Carnivore extends Animal {
-  // write your code here
   bite(animal) {
     if (animal instanceof Herbivore
       && !animal.hidden) {
       animal.health -= 50;
 
       if (animal.health <= 0) {
-        Animal.alive.splice(Animal.alive.indexOf(animal), 1);
-
-        return 0;
+        Animal.alive = Animal.alive.filter(liveAnimal => liveAnimal.health > 0);
       }
     }
   }
