7f2c04c7c60661ad763e2c223e7faa4933b25546
Roman Nikitenko <Lomkaaa@ukr.net>
2022-04-27 17:03:23 +0300

    Task completed

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,49 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+  }
+
+  static deathAlive(animal) {
+    for (let i = 0; i < Animal.alive.length; i++) {
+      if (Animal.alive[i] === animal) {
+        delete Animal.alive[i];
+      }
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health, hidden = false) {
+    super(name);
+    this.hidden = hidden;
+
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    Animal.alive.push(this);
+  }
+
+  bite(animal) {
+    if (!animal.hidden && !(animal instanceof Carnivore)) {
+      animal.health -= 50;
+    }
+
+    if (animal.health <= 0) {
+      Animal.deathAlive(animal);
+    }
+  }
 }
 
 module.exports = {
