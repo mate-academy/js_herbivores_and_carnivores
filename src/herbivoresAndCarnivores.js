48cd782cbbcd3d3a3a41618c4c4f5d92860d8e06
Kostiantyn <havryshkostya@gmail.com>
2022-05-09 21:28:39 +0400

    getb s ukrainy moskalb ne krasiviy

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,35 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health) {
+    this.name = name;
+    this.health = health || 100;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+  }
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
+Animal.alive = [];
+
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (animal.__proto__ === Herbivore.prototype && !animal.hidden) {
+      animal.health -= 50;
+    }
+
+    if (animal.__proto__ === Herbivore.prototype && animal.health <= 0) {
+      Animal.alive.splice(animal);
+    }
+  }
 }
 
 module.exports = {
