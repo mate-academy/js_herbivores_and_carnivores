b7440f378b65260bbf322ba5109489bd82e20e91
Amit Patil <amitp0156@gmail.com>
2022-05-17 21:06:20 +0530

    minor change to make code simple

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -23,7 +23,7 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(victim) {
-    if (!(victim instanceof Carnivore) && !victim.hidden) {
+    if (victim instanceof Herbivore && !victim.hidden) {
       victim.health -= 50;
     }
 
