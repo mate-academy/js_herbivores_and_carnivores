5c55fc86500217fa900b52bdeb44949e6b15a735
Andriyk0 <andriy22221@gmail.com>
2022-05-17 17:56:58 +0300

    animal

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,33 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herbObj) {
+    if (herbObj.hidden === false) {
+      herbObj.health -= 50;
+      Animal.alive = Animal.alive.filter(animal => animal.health <= 0);
+    }
+  }
 }
 
 module.exports = {
