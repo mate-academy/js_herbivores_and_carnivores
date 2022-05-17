b9654da3d96c7c3e514183f890a44b7f61fc5ab7
Uliton <x3m@Oleksandrs-MacBook-Pro.local>
2022-05-17 22:18:25 +0200

    animals creater

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,42 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+  }
+
+  static alive = [];
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, hidden) {
+    super(name);
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    this.hidden = true;
+  };
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    Animal.alive.push(this);
+  }
+  bite(carnivore) {
+    if (carnivore.hidden === false) {
+      carnivore.health -= 50;
+    }
+
+    for (let i = 0; i < Animal.alive.length; i++) {
+      if (Animal.alive[i].health <= 0) {
+        Animal.alive.splice(i, 1);
+      }
+    }
+  };
 }
 
 module.exports = {
