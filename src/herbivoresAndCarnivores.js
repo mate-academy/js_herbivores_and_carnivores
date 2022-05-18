b1f7b0be047560d9e665ecc4be14d0d0f6394ce6
Volodymyr <lesksv2000@gmail.com>
2022-05-18 17:41:48 +0300

    animals was killed

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,31 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hidden = false;
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(poorAnimal) {
+    if (poorAnimal instanceof Herbivore && poorAnimal.hidden === false) {
+      poorAnimal.health -= 50;
+    }
+
+    if (poorAnimal.health === 0) {
+      Animal.alive = Animal.alive.filter(animal => animal.health !== 0);
+    }
+  }
 }
 
 module.exports = {
