6166c9347e5e43b6b4732785cf021eb213735f8b
Daria Liubymova <dellachaise@gmail.com>
2022-05-18 15:01:07 +0300

    Added solution to 'Herbivores and Сarnivores' task

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,45 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health = 100, hidden = false) {
+    super(name, health);
+    this.hidden = hidden;
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    return (this.hidden = !this.hidden);
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health, hidden) {
+    super(name, health, hidden);
+    Animal.alive.push(this);
+  }
+
+  bite(object) {
+    const isCarvinore = object instanceof Carnivore;
+
+    if (object.hidden !== true && !isCarvinore) {
+      object.health -= 50;
+
+      if (object.health === 0) {
+        const index = Animal.alive.indexOf(object);
+
+        Animal.alive.splice(index, 1);
+      }
+    }
+  }
 }
 
 module.exports = {
