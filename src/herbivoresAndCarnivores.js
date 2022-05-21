6e1450476e3005dc7daf88946487296115c16491
Ihor Yeremenko <igoree86@gmail.com>
2022-05-21 22:28:25 +0300

    The_first_commit

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,43 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  health = 100;
+
+  static alive = [];
+  static addToAlive(obj) {
+    this.alive.push(obj);
+  }
+
+  static deleteDead() {
+    const index = this.alive.findIndex(animal => animal.health === 0);
+
+    this.alive.splice(index, 1);
+  }
+
+  constructor(name) {
+    this.name = name;
+
+    Animal.addToAlive(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hidden = false;
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (!animal.hidden && (animal instanceof Herbivore)) {
+      animal.health -= 50;
+
+      if (animal.health <= 0) {
+        Animal.deleteDead(animal);
+      }
+    }
+  }
 }
 
 module.exports = {
