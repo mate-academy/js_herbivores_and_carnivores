8b3279c73f67654020a46b801824c0681c4a6d19
Max <59259022+WhiteAshDc@users.noreply.github.com>
2022-06-09 12:45:00 +0200

    result

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,51 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health) {
+    this.name = name;
+    this.health = health || 100;
+    Animal.alive.push(this);
+  };
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+    Animal.alive.push(Animal);
+  }
+
+  hide() {
+    if (this.hidden === false) {
+      this.hidden = true;
+    } else {
+      this.hidden = false;
+    }
+
+    return this;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    Animal.alive.slice(this);
+  }
+
+  bite(beast) {
+    Animal.alive.filter(el => el.health <= 0);
+
+    if (beast.hidden === false
+      && beast instanceof Herbivore) {
+      beast.health -= 50;
+    };
+
+    if (beast.health <= 0) {
+      Animal.alive.splice(beast);
+    }
+  }
 }
 
 module.exports = {
