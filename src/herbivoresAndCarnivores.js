1a618d0532185a67cb14200ed0d282171b4deb98
Oleksandr Kapustei <angelion541@gmail.com>
2022-06-22 18:20:58 +0300

    solution edited

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -4,18 +4,17 @@ class Animal {
   constructor(name) {
     this.name = name;
     this.health = 100;
+    Animal.alive.push(this);
   };
 }
 
 Animal.alive = [];
 
 class Herbivore extends Animal {
-  constructor(name) {
-    super(name);
+  constructor() {
+    super();
 
     this.hidden = false;
-
-    Animal.alive.push(this);
   }
 
   hide() {
@@ -24,20 +23,12 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
-  constructor(name) {
-    super(name);
-
-    Animal.alive.push(this);
-  };
-
   bite(person) {
-    if (person instanceof Herbivore && person.hidden === false) {
+    if (person instanceof Herbivore && !person.hidden) {
       person.health -= 50;
     }
 
-    if (person.health === 0) {
-      Animal.alive = Animal.alive.filter(animal => animal.name !== person.name);
-    }
+    Animal.alive = Animal.alive.filter(animal => animal.health);
   }
 }
 
