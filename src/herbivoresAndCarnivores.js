cc158304a793a98782f08f9aff5fb7366415f7ae
Yaroslav Baiduk <byaa99@gmail.com>
2022-06-23 21:05:55 +0300

    Compleated

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,37 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden ? this.hidden = false : this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(attackTo) {
+    if (attackTo instanceof Herbivore & !attackTo.hidden) {
+      attackTo.health -= 50;
+    }
+
+    if (!attackTo.health) {
+      Animal.alive = Animal.alive.filter(isAlive => isAlive.health > 0);
+    }
+  }
 }
 
 module.exports = {
