377901759d0e6e619e3b1dcdff5496b4404375b6
Kostya Berehoviy <kostyaber3@gmail.com>
2022-06-23 22:28:15 +0300

    Animals Life Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,34 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
+Animal.alive = [];
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    if (this.hidden === false) {
+      this.hidden = true;
+    }
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (animal instanceof Herbivore === true && animal.hidden === false) {
+      animal.health -= 50;
+    }
+    Animal.alive = Animal.alive.filter((herbivore) => herbivore.health > 0); ;
+  }
 }
 
 module.exports = {
