416d995ac3d8c8752cefa86bb5ed5e2f9b63045e
StepBand <vladsvhik@gmail.com>
2022-06-24 12:56:36 +0300

    done

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,37 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    Animal.alive = [];
+    this.name = name;
+    this.health = 100;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    Animal.alive.push(this);
+  }
+
+  bite(target) {
+    if (target.hidden === false) {
+      target.health -= 50;
+    }
+    Animal.alive = Animal.alive.filter(element => element.health !== 0);
+  }
 }
 
 module.exports = {
