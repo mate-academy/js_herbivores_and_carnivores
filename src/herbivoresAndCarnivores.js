f3be5d86da5e40ce294387c56583e22393d29f06
Yura Gavrilyuk <gavrilyuk220@gmail.com>
2022-06-24 14:06:15 +0300

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,9 +1,9 @@
 'use strict';
 
 class Animal {
-  constructor(name, health) {
+  constructor(name) {
     this.name = name;
-    this.health = health || 100;
+    this.health = 100;
     Animal.alive.push(this);
   }
 }
@@ -27,7 +27,7 @@ class Carnivore extends Animal {
       animal.health -= 50;
     }
 
-    if (animal.health === 0) {
+    if (!animal.health) {
       Animal.alive = Animal.alive.filter(herbivore => herbivore.health);
     }
   }
