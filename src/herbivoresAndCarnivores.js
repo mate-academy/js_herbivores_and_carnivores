d49e3dd081fe3fc8d38ba3870dcc1f56c880c55c
SpaceHook <maxdumiak39@gmail.com>
2022-06-24 14:07:40 +0300

    ready

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,44 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(health = 100, name) {
+    this.health = health;
+    this.name = name;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health, hidden = false) {
+    super(health, name);
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(health, name);
+  }
+
+  bite(herbivore) {
+    if (herbivore instanceof Herbivore) {
+      if (!herbivore.hidden) {
+        herbivore.health -= 50;
+      }
+    }
+
+    this.isAlive();
+  }
+
+  isAlive() {
+    Animal.alive = Animal.alive.filter(beast => beast.health !== 0);
+  }
 }
 
 module.exports = {
