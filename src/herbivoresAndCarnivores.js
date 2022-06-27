e3559c75622429729c11cb5e6a2a5eac77f92570
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-06-27 14:45:40 +0300

    first commit

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,37 @@
 
 class Animal {
   // write your code here
+  constructor(name, health = 100) {
+    this.health = health;
+    this.name = name;
+    Animal.alive.push(this);
+  }
 }
+
+Animal.alive = [];
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+  }
+  hide() {
+    this.hidden = (!this.hidden);
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  bite(herb) {
+    if (herb instanceof Herbivore) {
+      herb.health -= (herb.hidden ? 0 : 50);
+    }
+
+    if (herb.health <= 0) {
+      Animal.alive = Animal.alive.filter(el => el !== herb);
+    }
+  }
 }
 
 module.exports = {
