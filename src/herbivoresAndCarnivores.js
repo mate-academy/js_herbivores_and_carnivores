26507d0c16238a75603d6317d8a7b1f33a7e9314
Max Shylenko <maxshylenko@gmail.com>
2022-07-06 02:24:18 +0200

    herbivores and carnivores done

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,41 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor() {
+    super();
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herbivore) {
+    if ((herbivore instanceof Herbivore) && !herbivore.hidden) {
+      herbivore.health -= 50;
+
+      if (herbivore.health === 0) {
+        const index = Animal.alive.lastIndexOf(herbivore);
+
+        Animal.alive.splice(index, 1);
+      }
+    }
+  }
 }
+
+Animal.alive = [];
 
 module.exports = {
   Animal,
