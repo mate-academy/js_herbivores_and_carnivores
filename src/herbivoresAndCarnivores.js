b4d5b43330c4c9182608576e3eb1bf8f62d3c3af
fledith <barateon023@gmail.com>
2022-07-06 14:45:02 +0300

    changed deletion method bcz previous method didnt work valid

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -38,7 +38,7 @@ class Carnivore extends Animal {
     herbivore.health -= 50;
 
     if (herbivore.health === 0) {
-      Animal.alive.splice(Animal.alive.indexOf(herbivore.name), 1);
+      Animal.alive = Animal.alive.filter(animal => animal.health > 0);
     }
   }
 }
