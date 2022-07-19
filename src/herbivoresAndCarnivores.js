c1e30ae88a0b40e005e97fe9f7415452f39a694c
Natalya Tsybulko <tsybulko.natalya395@gmail.com>
2022-07-20 01:40:04 +0300

    solve the herbivoresAndCarnivores problem

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,41 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor({ health = 100, name }) {
+    this.health = health;
+    this.name = name;
+
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor({ health = 100, name, hidden = false }) {
+    super(health, name);
+
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (animal instanceof Herbivore
+      && animal.hidden === false) {
+      animal.health -= 50;
+
+      if (animal.health === 0) {
+        const indexOfANimal = Animal.alive.indexOf(animal);
+
+        Animal.alive.splice(indexOfANimal, 1);
+      }
+    }
+  }
 }
 
 module.exports = {
