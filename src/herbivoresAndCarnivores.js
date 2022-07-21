757bc25e32162b3542560e69588a46ca7750e2bc
Volodymyr Opanasiuk <opanasiukvlad@gmail.com>
2022-07-21 17:58:47 +0300

    initial commit

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,41 @@
 
 class Animal {
   // write your code here
+
+  constructor(health, name) {
+    this.health = health || 100;
+    this.name = name;
+
+    Animal.add();
+  }
 }
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(health, name) {
+    super(health, name);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  constructor(health, name) {
+    super(health, name);
+
+    this.hidden = false;
+  }
+
+  bite(victim) {
+    if (victim instanceof Herbivore && victim.hidden === false) {
+      victim.health -= 50;
+    }
+  }
 }
 
 module.exports = {
