b064af4b5ea1afa0ef7471ed3564864b91ff4315
Denys Korotkoruchko <denenergii@gmail.com>
2022-07-21 13:58:15 +0300

    ready

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,36 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+
+    Animal.alive.push(this);
+  }
 }
+Animal.alive = [];
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor() {
+    super();
+    this.hidden = false;
+  }
+
+  hide() {
+    return (this.hidden)
+      ? (this.hidden = false)
+      : (this.hidden = true);
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herbivore) {
+    if (!herbivore.hidden && herbivore instanceof Herbivore) {
+      herbivore.health -= 50;
+    }
+
+    Animal.alive = Animal.alive.filter(item => item.health > 0);
+  }
 }
 
 module.exports = {
