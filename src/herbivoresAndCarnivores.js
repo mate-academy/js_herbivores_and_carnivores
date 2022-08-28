000970ddfcdb4e5e50f68e8c93f8829b3ddb8465
Vadls <vadim_tanasienko54541@ukr.net>
2022-08-29 00:39:41 +0300

    Add animals classes with methods

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,47 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+  }
+
+  static adder(instance) {
+    this.alive.push(instance);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+    Animal.adder(this);
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    Animal.adder(this);
+  }
+
+  bite(instance) {
+    if (!(instance instanceof Carnivore) && instance.hidden === false) {
+      Animal.alive = Animal.alive.filter(element => {
+        if (element.name === instance.name) {
+          element.health -= 50;
+        }
+
+        return element.health > 0;
+      });
+    }
+  }
 }
 
 module.exports = {
