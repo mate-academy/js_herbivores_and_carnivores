c617d6f184af0b8c1c6c7f2b94114ba75954a6ed
Igor Bondarenko <igorbondarenko35@gmail.com>
2022-08-28 20:02:27 +0300

    Animal, Herbivore, Сarnivore solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,43 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static removeAnimal() {
+    Animal.alive.splice(Animal.alive.indexOf(this.name), 1);
+  }
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(creature) {
+    if (creature instanceof Herbivore
+      && creature.hidden === false) {
+      creature.health -= 50;
+    }
+
+    if (creature.health === 0) {
+      const isAnimalDead = Animal.alive.indexOf(creature);
+
+      Animal.alive.splice(isAnimalDead, 1);
+    }
+  }
 }
 
 module.exports = {
