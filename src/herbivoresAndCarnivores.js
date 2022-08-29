755bac5dbbf31fa1a8fb4679b8e625714de82bc3
Andrii Dmytruk <andriydmitr2014@gmail.com>
2022-08-29 11:48:08 +0300

    Animal, Herbivore and Carnivore classes implemented

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,40 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    if (!Animal.alive) {
+      Animal.alive = [];
+    }
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(prey) {
+    if (!(prey instanceof Carnivore) && !prey.hidden) {
+      prey.health -= 50;
+
+      if (prey.health <= 0) {
+        const index = Animal.alive.findIndex(el => el === prey);
+
+        Animal.alive.splice(index, 1);
+      }
+    }
+  }
 }
 
 module.exports = {
