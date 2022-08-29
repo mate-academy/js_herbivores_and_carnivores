34a019bae8f8ae11738e1718e6974b8513b85eee
Anton <shulezhko.antony@gmail.com>
2022-08-29 14:17:46 +0300

    done

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,49 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+
+    this.role = 'herbivore';
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+
+    this.role = 'carnivore';
+  }
+
+  bite(somebody) {
+    if (somebody.role !== 'carnivore'
+    && somebody.hidden === false) {
+      somebody.health -= 50;
+    }
+
+    if (somebody.health <= 0) {
+      for (let i = 0; i < Animal.alive.length; i++) {
+        if (Animal.alive[i].name === somebody.name) {
+          delete Animal.alive[i];
+        }
+      }
+    }
+  }
 }
 
 module.exports = {
