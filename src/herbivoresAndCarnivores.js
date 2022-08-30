ea49d24e6d6ee44d5ac3a3dedae14f3716a87892
Yevhenii Podplietnii <yevheniy.podplietnii@gmail.com>
2022-08-30 20:11:26 +0300

    fixed removing from Animal.alive array

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -25,8 +25,11 @@ class Carnivore extends Animal {
   bite(animal) {
     if (animal instanceof Herbivore && !animal.hidden) {
       animal.health -= 50;
+
+      if (animal.health <= 0) {
+        Animal.alive.splice(Animal.alive.indexOf(animal), 1);
+      }
     }
-    Animal.alive = Animal.alive.filter(({ health }) => health > 0);
   }
 }
 
