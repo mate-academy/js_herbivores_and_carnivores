f7e09e51efe56b807a112e812f30b774b54f9660
Oleksandr Sobchuk <osobchuk1@gmail.com>
2022-08-30 19:44:07 +0300

    Herbivores and Carnivores

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,36 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health) {
+    this.name = name;
+    this.health = health || 100;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herb) {
+    if (herb.hidden === false) {
+      herb.health -= 50;
+    }
+
+    Animal.alive = Animal.alive
+      .filter(diedAnimal => diedAnimal.health > 0);
+  }
 }
 
 module.exports = {
