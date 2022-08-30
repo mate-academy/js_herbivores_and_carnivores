99ffd493780cfb4f222681afc15ec168e0f3e918
Kolia Plemennyk <plemianniknikolay@gmail.com>
2022-08-30 17:06:01 +0300

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,42 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+
+    this.hidden = false;
+  }
+
+  /* I read the checklist, but I didn't quite understand point 3,
+so in case I did not understand correctly,
+I would like to immediately clarify something
+- why should the method not toggle the state of the hide property?
+An animal cannot hide all its life. */
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+  }
+
+  bite(dinner) {
+    dinner.health -= dinner.hidden === false ? 50 : 0;
+    Animal.alive = Animal.alive.filter(animal => animal.health > 0);
+  }
 }
 
 module.exports = {
