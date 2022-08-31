39c44bcc495878a21932e9be3770a533b1bc9755
fenderukr <priststoreua@gmail.com>
2022-08-31 15:49:31 +0300

    HerbivoresAndCarnivores0.1

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,30 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.health = 100;
+    this.name = name;
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(objFromHerbivore) {
+    if (objFromHerbivore instanceof Herbivore
+      && objFromHerbivore.hidden !== true) {
+      objFromHerbivore.health -= 50;
+      Animal.alive = Animal.alive.filter(animal => animal.health > 0);
+    }
+  }
 }
 
 module.exports = {
