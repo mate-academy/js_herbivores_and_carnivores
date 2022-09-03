1c5e54da6f67a2a945efc10230a9d2db81bf3cee
Oleksandr Malyshev <sanyokmalyshev@gmail.com>
2022-09-03 16:01:31 +0800

    Task done

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,40 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  static checkHealth(animal) {
+    if (animal.health <= 0) {
+      Animal.alive = Animal.alive.filter(elem => elem !== animal);
+    }
+  }
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (animal.hidden || animal instanceof Carnivore) {
+      return;
+    }
+    animal.health -= 50;
+    Animal.checkHealth(animal);
+  }
 }
 
 module.exports = {
