fe0c886aece3a7895524bbeb45a90c4a307a770e
Volodymir Tymtsias <vovanuch2007@gmail.com>
2022-09-07 15:48:43 +0300

    Solution Herbivores and Сarnivores

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,48 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static addAlive(animal) {
+    this.alive.push(animal);
+  }
+
+  static removeAlive(animal) {
+    for (let i = 0; i < this.alive.length; i++) {
+      if (this.alive[i].name === animal.name) {
+        this.alive.splice(i, 1);
+      }
+    }
+  }
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.addAlive(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herbivoreAnimal) {
+    if (herbivoreAnimal.hidden === false) {
+      herbivoreAnimal.health -= 50;
+
+      if (herbivoreAnimal.health === 0) {
+        Animal.removeAlive(herbivoreAnimal);
+      }
+    }
+  }
 }
 
 module.exports = {
