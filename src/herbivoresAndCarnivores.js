1fb86e896d65be5eb1c7ae681f9bdc896f4a6089
dmitriyn3679 <dmitriyn3679@gmail.com>
2022-09-21 16:19:05 +0300

    done!

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,44 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.allAnimals.push(this);
+  }
+
+  static get alive() {
+    const res = [];
+
+    for (const animal of Animal.allAnimals) {
+      if (animal.health > 0) {
+        res.push(animal);
+      }
+    }
+
+    return res;
+  };
 }
 
+Animal.allAnimals = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  };
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (animal instanceof Herbivore && animal.hidden !== true) {
+      animal.health -= 50;
+    }
+  };
 }
 
 module.exports = {
