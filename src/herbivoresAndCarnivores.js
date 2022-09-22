4271ce6d6967d005868095c17ab25d4fa1510b45
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-09-22 18:49:37 +0300

    animals solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,34 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  health = 100;
+
+  constructor(name) {
+    this.name = name;
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hidden = false;
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(name) {
+    const victim = Animal.alive.find(animal => animal === name);
+    const checkPrototype = victim.__proto__.constructor.name === 'Herbivore';
+
+    victim.health = checkPrototype && !victim.hidden
+      ? victim.health - 50
+      : victim.health;
+    Animal.alive = Animal.alive.filter(animal => animal.health > 0);
+  }
 }
 
 module.exports = {
