ffa0ba19f13dad70d1c93e1c25ad838c09282c1c
Killdex <kdcompany2018@gmail.com>
2022-09-25 16:50:30 +0300

    wild nature created

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,37 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health) {
+    this.name = name;
+    this.health = health || 100;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(victim) {
+    if (!(victim.hidden === true || victim instanceof Carnivore)) {
+      victim.health -= 50;
+
+      if (victim.health === 0) {
+        Animal.alive.splice(Animal.alive.indexOf(victim), 1);
+      }
+    }
+  }
 }
 
 module.exports = {
