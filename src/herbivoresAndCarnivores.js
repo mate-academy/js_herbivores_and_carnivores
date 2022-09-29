3d62e8a9d36da2e73b9b0ac52519d01cf2fd0f84
Vitalii Derlemenko <vitaliidox@gmail.com>
2022-09-29 22:26:01 +0300

    fix filter bittenBEast and constructor herbivore

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -4,7 +4,7 @@ class Animal {
   constructor(name, health = 100) {
     this.name = name;
     this.health = health;
-    this.hidden = false;
+
     Animal.alive.push(this);
   };
 }
@@ -14,6 +14,7 @@ Animal.alive = [];
 class Herbivore extends Animal {
   constructor(name, health = 100) {
     super(name, health);
+    this.hidden = false;
   }
 
   hide() {
@@ -31,9 +32,11 @@ class Carnivore extends Animal {
       && !beast.hidden) {
       beast.health -= 50;
 
-      Animal.alive = Animal.alive.filter(bittenBeast => {
-        return bittenBeast.health <= 0;
-      });
+      if (beast.health <= 0) {
+        Animal.alive = Animal.alive.filter(bittenBeast => {
+          return bittenBeast.health > 0;
+        });
+      }
     }
   }
 }
