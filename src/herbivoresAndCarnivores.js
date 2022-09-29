51c8b15ea5866faa388e68b3dbdf714c9712a7df
Anton Iskryk <iskryk.anton@gmail.com>
2022-09-29 20:39:02 +0300

    Solution v01

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,53 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.health = 100;
+    this.name = name;
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(health, name, hidden = false) {
+    super(name);
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(health, name, hidden = false) {
+    super(health, name);
+  }
+
+  bite(herbivore) {
+    if (herbivore instanceof Carnivore === false && !herbivore.hidden) {
+      herbivore.health -= 50;
+    }
+
+    if (herbivore.health === 0) {
+      Animal.alive.splice(herbivore);
+    }
+  }
 }
+
+/*
+Create a Сarnivore class. Carnivore has a bite method, which
+takes a herbivore object and decreases the object's health
+by 50. The method does not work if it is another сarnivore,
+or the herbivore is currently hiding.
+
+All alive animals should be in the static Animal.alive array.
+If the health of the animal reaches 0, the beast dies and
+it should be removed from the static array Animals.alive.
+*/
 
 module.exports = {
   Animal,
