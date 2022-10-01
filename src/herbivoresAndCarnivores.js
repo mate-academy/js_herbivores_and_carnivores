99edaaecc5c7837f953178c73640b11aaa51b7a0
Serhii Romaniuk <deepspark@gmail.com>
2022-10-01 15:23:58 +0300

    implement classes

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,42 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+  }
+
+  bite(beast) {
+    if (beast.hidden === false) {
+      beast.health -= 50;
+    }
+
+    if (beast.health <= 0) {
+      const index = Animal.alive.indexOf(beast);
+
+      Animal.alive.splice(index, 1);
+    }
+  }
 }
 
 module.exports = {
