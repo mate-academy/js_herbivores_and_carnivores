64a991f1a2df631b1de7d838dfadf530467b647a
Arsen-Shpak <a.shpakovich2000@gmail.com>
2022-10-02 17:02:09 +0300

    animals

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,44 @@
 
 class Animal {
   // write your code here
+  static addAnimal(animal) {
+    this.alive.push(animal);
+  }
+
+  static removeAnimal() {
+    this.alive = this.alive.filter((animal) => animal.health > 0);
+  };
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.addAnimal(this);
+  }
 }
+Animal.alive = [];
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  bite(animal) {
+    if (animal instanceof Herbivore && !animal.hidden) {
+      animal.health -= 50;
+    }
+
+    if (animal.health <= 0) {
+      Animal.removeAnimal(animal);
+    }
+  }
 }
 
 module.exports = {
