a2747ea16562825d6a5f0577d0390395166a2900
Bohdan Borsuk <barsuchellos@gmail.com>
2022-10-03 02:41:57 +0300

    complete the task

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,42 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health) {
+    this.name = name;
+    this.health = 100;
+
+    Animal.alive.push(this);
+  }
+
+  static died(animal) {
+    this.alive = this.alive.filter(el => el !== animal);
+  };
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health, hidden) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(el) {
+    if (el instanceof Herbivore && el.hidden === false) {
+      el.health -= 50;
+    };
+
+    if (el.health === 0) {
+      Animal.died(el);
+    }
+  }
 }
+
+Animal.alive = [];
 
 module.exports = {
   Animal,
