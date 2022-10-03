4e00ce2df14e4b8ab161e0cee4213eac30bf97c1
Nikita Ovchinnikov <nikitaovch99@gmail.com>
2022-10-03 20:31:44 +0300

    fixes after mentor review

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,27 +1,29 @@
 'use strict';
 
 class Animal {
-  static alive = [];
   constructor(name) {
     this.name = name;
     this.health = 100;
-    this.hidden = false;
     Animal.alive.push(this);
   }
 }
+Animal.alive = [];
 
 class Herbivore extends Animal {
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
   hide() {
     this.hidden = true;
   }
 }
-
 class Carnivore extends Animal {
   bite(animal) {
     if (animal instanceof Herbivore && animal.hidden === false) {
       animal.health -= 50;
+      Animal.alive = Animal.alive.filter(alive => alive.health > 0);
     }
-    Animal.alive = Animal.alive.filter(alive => alive.health > 0);
   }
 }
 
