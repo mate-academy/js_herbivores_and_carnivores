d150d2f5d45286497cf4969eef2f9a002baac378
Serhii Hluhovskyi <glugovsky@proton.me>
2022-10-04 18:01:19 +0300

    Added herbivore check for bite method

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -29,7 +29,7 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(target) {
-    if (target.hidden === false) {
+    if (target.hidden === false && target instanceof Herbivore) {
       target.health -= 50;
 
       if (target.health <= 0) {
