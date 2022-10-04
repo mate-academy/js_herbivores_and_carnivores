1524a4a499e059f0fe9459d0580289b4daa0e14e
Maksym845 <mzuravel845@gmail.com>
2022-10-04 17:51:41 +0300

    condition for filtering was added

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -23,11 +23,13 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(target) {
-    if (target instanceof Carnivore !== true && target.hidden !== true) {
+    if (target instanceof Herbivore && !target.hidden) {
       target.health -= 50;
     }
 
-    Animal.alive = Animal.alive.filter(animal => animal.health > 0);
+    if (target.health <= 0) {
+      Animal.alive = Animal.alive.filter(animal => animal.health > 0);
+    }
   }
 }
 
