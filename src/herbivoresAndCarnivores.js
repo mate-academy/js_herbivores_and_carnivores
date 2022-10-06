d2edb6b79f79f8c004333e0c6b3db0783646aa9f
Bibicutu <pauleugen@gmail.com>
2022-10-06 23:35:17 +0300

    First commit

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,50 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static addAnimal(animal) {
+    if (this.alive === undefined) {
+      this.alive = [];
+    }
+    this.alive.push(animal);
+  }
+
+  static removeAnimal(animal) {
+    this.alive = this.alive.filter(liveAnimal => animal !== liveAnimal);
+  }
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+    Animal.addAnimal(this);
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    Animal.addAnimal(this);
+  }
+
+  bite(animal) {
+    if (!(animal instanceof Carnivore) && !animal.hidden) {
+      animal.health -= 50;
+    }
+
+    if (animal.health <= 0) {
+      Animal.removeAnimal(animal);
+    }
+  }
 }
 
 module.exports = {
