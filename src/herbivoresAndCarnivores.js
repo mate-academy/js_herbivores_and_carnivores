c4429aba32ad5b6d6457d0a4154bebceec5b0793
Anastasiia Tilikina <anastasiia.tilikina@gmail.com>
2022-10-12 17:20:20 +0300

    Implement Herbivore and Carnivore task

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,36 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  static addAnimal(animal) {
+    this.alive.push(animal);
+  }
+
+  health = 100;
+
+  constructor(name) {
+    this.name = name;
+    Animal.addAnimal(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hidden = false;
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (animal instanceof Herbivore && !animal.hidden) {
+      animal.health -= 50;
+    }
+
+    Animal.alive = Animal.alive.filter(beast => beast.health > 0);
+  }
 }
 
 module.exports = {
