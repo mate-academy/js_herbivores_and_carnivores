1fc79c2be4676d3c761edc27d7f06fe74b0bf58e
Kate Riazantseva <katyarezvova96@gmail.com>
2022-10-24 16:29:54 +0300

    new Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,24 +1,18 @@
 'use strict';
 
 class Animal {
-  constructor(name, health) {
+  constructor(name, health = 100) {
     this.name = name;
-    this.health = health || 100;
+    this.health = health;
     Animal.alive.push(this);
   }
 }
 Animal.alive = [];
 
 class Herbivore extends Animal {
-  constructor(health, name) {
-    super(health, name);
+  constructor(name, health) {
+    super(name, health);
     this.hidden = false;
-
-    if (health <= 0) {
-      const index = Animal.alive.indexOf(this);
-
-      Animal.alive.splice(index, 1);
-    }
   }
 
   hide() {
@@ -31,11 +25,7 @@ class Carnivore extends Animal {
     if (animal instanceof Herbivore && animal.hidden === false) {
       animal.health -= 50;
 
-      if (animal.health === 0) {
-        const index = Animal.alive.indexOf(animal);
-
-        Animal.alive.splice(index, 1);
-      }
+      Animal.alive = Animal.alive.filter(el => el.health > 0);
     }
   }
 }
