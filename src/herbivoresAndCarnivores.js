cf9ead7d7c86ff3993e019d2fcdc92240c27f70b
bokorzita <104830913+bokorzita@users.noreply.github.com>
2022-10-26 22:59:15 +0200

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,53 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.health = 100;
+    this.name = name;
+
+    let shouldInclude = true;
+
+    for (const animal of Animal.alive) {
+      if (animal.name === this.name) {
+        shouldInclude = false;
+      }
+    }
+
+    if (shouldInclude === true) {
+      Animal.alive.push(this);
+    }
+    Animal.alive.sort((a, b) => b.health - a.health);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  bite(obj) {
+    if (obj instanceof Herbivore && obj.hidden === false) {
+      obj.health -= 50;
+    }
+
+    if (obj instanceof Herbivore && obj.health <= 0) {
+      Animal.alive.pop();
+    }
+  }
 }
 
 module.exports = {
