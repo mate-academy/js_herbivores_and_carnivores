6f1714bf1971f67b3f78916766c4f0a7455f9d12
metin <metinbicaksiz@gmail.com>
2022-11-01 22:51:20 +0300

    solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,50 @@
 
 class Animal {
   // write your code here
+  // static alive = [];
+
+  constructor() {
+    this.health = 100;
+  }
 }
+
+Animal.alive = [];
 
 class Herbivore extends Animal {
   // write your code here
+  constructor() {
+    super();
+    this.hidden = false;
+
+    Animal.alive.push(this);
+  };
+
+  hide() {
+    this.hidden = true;
+  };
 }
 
 class Carnivore extends Animal {
   // write your code here
+  constructor() {
+    super();
+
+    Animal.alive.push(this);
+  };
+
+  bite(name) {
+    if (name.hidden === false
+      && name instanceof Herbivore
+      && name.health > 0) {
+      name.health -= 50;
+    }
+
+    if (name.health === 0) {
+      const dead = Animal.alive.indexOf(name);
+
+      Animal.alive.splice(dead, 1);
+    }
+  };
 }
 
 module.exports = {
