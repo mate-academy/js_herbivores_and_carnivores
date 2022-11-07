0969d70623f337316eb6c71875b09b106a3a2b80
Masha <teenderrr@gmail.com>
2022-11-07 20:05:00 +0200

    animals

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,52 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  // static alive = []; there was an Error from Linter
+  // I couldn`t fix it with npm or plugins
+
+  static addToAlive(animal) {
+    this.alive.push(animal);
+  }
+
+  static removeFromAlive(animal) {
+    if (this.alive.includes(animal)) {
+      const indexOfAnimal = this.alive.indexOf(animal);
+
+      this.alive.splice(indexOfAnimal);
+    }
+  }
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.addToAlive(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health, hidden = false) {
+    super(name, health);
+
+    this.hidden = hidden;
+  }
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (animal instanceof Herbivore) {
+      animal.health = animal.hidden ? animal.health : animal.health - 50;
+    }
+
+    if (animal instanceof Herbivore && animal.health === 0) {
+      Animal.removeFromAlive(animal);
+    }
+  }
 }
+
+Animal.alive = [];
 
 module.exports = {
   Animal,
