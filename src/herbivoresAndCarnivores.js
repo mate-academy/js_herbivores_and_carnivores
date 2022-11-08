fb278e0b7e28642cd428d90625c6d85a45532c85
Max Skorokhod <mrzandras@gmail.com>
2022-11-08 19:48:47 +0200

    hunterXhunter

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,36 @@
 'use strict';
-
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health, hidden = false) {
+    super(name, health);
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(target) {
+    if (target instanceof Herbivore && target.hidden !== true) {
+      target.health -= 50;
+    }
+
+    if (target.health === 0) {
+      Animal.alive = Animal.alive.filter(animal => animal !== target);
+    }
+  }
 }
+
+Animal.alive = [];
 
 module.exports = {
   Animal,
