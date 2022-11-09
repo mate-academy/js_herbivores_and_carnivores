ae0b3f867bb43ab78ceb9b4b7734ce96bb6a57b4
Max Skorokhod <mrzandras@gmail.com>
2022-11-09 10:21:48 +0200

    changesAfterSTrev

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,16 @@
 'use strict';
 class Animal {
-  constructor(name, health = 100) {
+  constructor(name) {
     this.name = name;
-    this.health = health;
+    this.health = 100;
     Animal.alive.push(this);
   }
 }
 
 class Herbivore extends Animal {
-  constructor(name, health, hidden = false) {
+  constructor(name, health) {
     super(name, health);
-    this.hidden = hidden;
+    this.hidden = false;
   }
 
   hide() {
