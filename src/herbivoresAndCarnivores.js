5512e9de4750dd32915293570b93bc5efbff6698
Roman Maksymuk <romamaksymuk@gmail.com>
2022-11-09 09:44:43 +0200

    tests passed

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,47 @@
 
 class Animal {
   // write your code here
+
+  constructor(name, health = 100) {
+    this.health = health;
+    this.name = name;
+    Animal.addAnimal(this);
+  }
+
+  static addAnimal(animal) {
+    Animal.alive.push(animal);
+  }
+
+  static removeAnimal(animal) {
+    Animal.alive = Animal.alive.filter(item => item.name !== animal.name);
+  }
 }
+
+Animal.alive = [];
 
 class Herbivore extends Animal {
   // write your code here
+  constructor() {
+    super();
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  bite(herbivore) {
+    if (herbivore.hidden === false) {
+      herbivore.health -= 50;
+    }
+
+    if (herbivore.health <= 0) {
+      Animal.removeAnimal(herbivore);
+    }
+  }
 }
 
 module.exports = {
