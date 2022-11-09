82cb581f399225bf517587fc994066679d850620
Kovalskyi Semen <esceyp249>
2022-11-09 14:05:06 +0200

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,44 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static pushAnimal(animal) {
+    Animal.alive.push(animal);
+  }
+
+  static deleteAnimal(animal) {
+    Animal.alive = Animal.alive.filter(beast => beast !== animal);
+  }
+
+  constructor(name) {
+    this.health = 100;
+    this.name = name;
+
+    Animal.pushAnimal(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herb) {
+    if (herb instanceof Herbivore && !herb.hidden) {
+      herb.health -= 50;
+    }
+
+    if (herb.health === 0) {
+      Animal.deleteAnimal(herb);
+    }
+  }
 }
 
 module.exports = {
