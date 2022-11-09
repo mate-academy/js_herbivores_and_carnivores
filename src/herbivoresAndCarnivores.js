6a10c72ef440f3ef49a62830fde236e220939e99
Artem66 <melodpi@gmail.com>
2022-11-09 06:39:46 +0000

    implemented classes Animal, Heerbivore, Carnivore

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,47 @@
 
 class Animal {
   // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+  }
+
+  static alive = [];
+
+  static addAnimal(animal) {
+    this.alive.push(animal);
+  }
 }
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+    Animal.addAnimal(this);
+  }
+
+  hide() {
+    this.hidden = this.hidden ? 0 : true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  constructor(name) {
+    super(name);
+    Animal.addAnimal(this);
+  }
+
+  bite(herbivore) {
+    if (!herbivore.hidden && herbivore.constructor === Herbivore) {
+      herbivore.health -= 50;
+    }
+
+    if (herbivore.health === 0) {
+      Animal.alive.splice(Animal.alive.indexOf(herbivore), 1);
+    }
+  }
 }
 
 module.exports = {
