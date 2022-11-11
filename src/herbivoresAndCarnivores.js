69acc7ca97d389797fc0caf4da4bd2afdb291057
Bohdan Rudenko <RudBeee@gmail.com>
2022-11-11 14:08:27 +0200

    vol_1

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,40 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(x) {
+    if (x.hidden === false) {
+      Animal.alive.forEach(element => {
+        if (element.name === x.name) {
+          element.health -= 50;
+        }
+      });
+    }
+
+    const result = Animal.alive.filter(elem => elem.health > 0);
+
+    Animal.alive = result;
+  }
 }
 
 module.exports = {
