edec33a55e0457096a5c1f13f961b8d00d89cb50
Demut <uddarendeli1@gmail.com>
2022-12-03 01:00:57 +0300

    Classes created for Herbivore Carnivore and the base Animal class according to the requirements. Passed the tests

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,39 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herbivore) {
+    if (herbivore instanceof Herbivore && !herbivore.hidden) {
+      herbivore.health -= 50;
+
+      if (herbivore.health <= 0) {
+        const index = Animal.alive.indexOf(herbivore);
+
+        if (index >= 0) {
+          Animal.alive.splice(index, 1);
+        }
+      }
+    }
+  }
 }
 
 module.exports = {
