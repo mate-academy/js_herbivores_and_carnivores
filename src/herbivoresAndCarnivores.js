ab656a084d653c932cb2ea6d8c095cbe8a30490f
christian m wolf <108151633+devWizHub@users.noreply.github.com>
2022-12-06 11:55:13 +0100

    commit

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,15 +2,45 @@
 
 class Animal {
   // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+  }
 }
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(name, health) {
+    super(name, health);
+
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  constructor(name, health) {
+    super(name, health);
+
+    Animal.alive.push(this);
+  }
+
+  bite(herbi) {
+    if (herbi instanceof Herbivore && !herbi.hidden) {
+      herbi.health -= 50;
+    }
+
+    Animal.alive = Animal.alive.filter(animal =>
+      animal.health > 0);
+  }
 }
+
+Animal.alive = [];
 
 module.exports = {
   Animal,
