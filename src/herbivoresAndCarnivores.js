59dd5f86d2771cb45cc902b9498765c05507d2bf
GabrielDeveloper <89942450+GabrielMaksimovich@users.noreply.github.com>
2022-12-09 20:47:06 +0200

    put Animal.push to Animal class and deleted from other classes, add if-block for filter dead animals

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,18 +1,19 @@
 'use strict';
 
 class Animal {
+  // write your code here
   constructor(name, health = 100) {
     this.name = name;
     this.health = health;
+    Animal.alive.push(this);
   }
 }
 
 class Herbivore extends Animal {
-  constructor(name, health, hidden = false) {
-    super(name, health);
-
-    this.hidden = hidden;
-    Animal.alive.push(this);
+  // write your code here
+  constructor() {
+    super();
+    this.hidden = false;
   }
 
   hide() {
@@ -21,15 +22,13 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
-  constructor(name, health) {
-    super(name, health);
+  // write your code here
+  bite(herbivore) {
+    if (herbivore instanceof Herbivore && !herbivore.hidden) {
+      herbivore.health -= 50;
+    }
 
-    Animal.alive.push(this);
-  }
-
-  bite(obj) {
-    if (!obj.hidden && !(obj instanceof Carnivore)) {
-      obj.health -= 50;
+    if (herbivore.health <= 0) {
       Animal.alive = Animal.alive.filter(animal => animal.health > 0);
     }
   }
