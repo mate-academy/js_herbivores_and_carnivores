242284ca1eed5b3dbe92cac2b51bd70ee57a5530
Yuliia Poliit <poliit.yulia@gmail.com>
2022-12-12 15:43:16 +0100

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -23,13 +23,13 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
-  bite(animal) {
-    if (animal instanceof Herbivore && !animal.hidden) {
-      animal.health -= 50;
+  bite(target) {
+    if (target instanceof Herbivore && !target.hidden) {
+      target.health -= 50;
     }
 
-    if (animal.health <= 0) {
-      Animal.alive = Animal.alive.filter(animals => animals !== animal);
+    if (target.health <= 0) {
+      Animal.alive = Animal.alive.filter(animal => target.health > 0);
     }
   }
 }
