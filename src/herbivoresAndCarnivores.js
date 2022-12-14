f5e18a7c2b3a2aff79577ddcd0b3cea87dbaaf2c
Viktoriia Kravchenko <kravchenkovictoriya9@gmail.com>
2022-12-14 14:59:46 +0200

    Animal, Herbivore and Carnivore classes were added

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,42 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static add(creature) {
+    this.alive.push(creature);
+  }
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+
+    Animal.add(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health, hidden = false) {
+    super(name, health);
+
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(creature) {
+    if (creature instanceof Herbivore && !creature.hidden) {
+      creature.health -= 50;
+    }
+
+    if (creature.health === 0) {
+      Animal.alive = Animal.alive.filter(beast => beast.health > 0);
+    }
+  }
 }
 
 module.exports = {
