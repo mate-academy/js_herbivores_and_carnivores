01c4d0e7211094619c084ac7200ce946a6721eb4
Serhii Redko <sergey@redko.com.ua>
2022-12-14 13:46:17 +0200

    a solution that works well for removing students from Mate

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,39 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animalForDinner) {
+    if (
+      !animalForDinner.hidden
+      && animalForDinner instanceof Herbivore
+    ) {
+      animalForDinner.health -= 50;
+    }
+    Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
+  }
 }
+
+Animal.alive = [];
 
 module.exports = {
   Animal,
