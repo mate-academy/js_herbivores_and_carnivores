ba9b5c8964d24e7fbb36fe3d6ea09f0aca4baf0e
Victoria Kovalenko <v.kovalenkoooooo@gmail.com>
2022-12-15 10:05:10 +0200

    solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,29 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, hidden = false) {
+    this.name = name;
+    this.health = 100;
+    this.hidden = hidden;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(bittenAnimal) {
+    if (bittenAnimal.hidden === false && bittenAnimal instanceof Herbivore) {
+      bittenAnimal.health = bittenAnimal.health - 50;
+      Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
+    }
+  }
 }
 
 module.exports = {
