02205856fb757da5c9f596c70bda2ec87f6cc306
Kukharuk <its.up.to.you.dk@gmail.com>
2022-12-15 14:54:24 +0200

    animalsDK

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,43 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    Animal.alive.push(this);
+  }
+
+  bite(animal) {
+    if (animal.hidden === false) {
+      animal.health -= 50;
+    }
+
+    if (animal.health === 0) {
+      Animal.alive.indexOf(animal);
+      Animal.alive.splice(Animal.alive.indexOf(animal), 1);
+    }
+  }
 }
+
+Animal.alive = [];
 
 module.exports = {
   Animal,
