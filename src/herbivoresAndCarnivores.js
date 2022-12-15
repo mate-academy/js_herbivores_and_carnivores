5e3cbc686bf3ba9c2a5e7598ecb012c028bfe193
Ivan Melnychuk <jacksmilereacher@gmail.com>
2022-12-15 20:57:07 +0200

    added Animal, Herbivore, Carnivore classes

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,38 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health) {
+    this.health = health || 100;
+    this.name = name;
+    Animal.alive.push(this);
+  }
 }
+Animal.alive = [];
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health, hidden) {
+    super(name, health);
+    this.hidden = hidden || false;
+  }
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herbivore) {
+    const isHerbivore = herbivore instanceof Herbivore;
+    const isVisible = !herbivore.hidden;
+
+    if (isVisible && isHerbivore) {
+      herbivore.health -= 50;
+
+      if (herbivore.health <= 0) {
+        Animal.alive = Animal.alive.filter(animal =>
+          animal.health > 0);
+      }
+    }
+  }
 }
 
 module.exports = {
