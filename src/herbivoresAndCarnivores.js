c1ef407d1f1e3df62f3caac622adbed4f9554d99
Mohamed Dahi <mohamedjane362@gmail.com>
2022-12-16 10:56:01 +0300

    Herbivores and caranivores

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,35 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.health = 100;
+    this.name = name;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animalName) {
+    if (animalName instanceof Herbivore && !animalName.hidden) {
+      animalName.health -= 50;
+    }
+
+    if (!animalName.health) {
+      Animal.alive = Animal.alive.filter(animal => animal !== animalName);
+    }
+  }
 }
 
 module.exports = {
