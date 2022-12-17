d4bc0765bbd069c71b1afb1ab2a9aafc4136da50
Denis Bokov <denisbokov1703@gmail.com>
2022-12-17 13:07:22 +0100

    Solution(2)

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,8 +1,6 @@
 'use strict';
 
 class Animal {
-  static alive = [];
-
   constructor(name) {
     this.name = name;
     this.health = 100;
@@ -10,6 +8,8 @@ class Animal {
     Animal.alive.push(this);
   }
 }
+
+Animal.alive = [];
 
 class Herbivore extends Animal {
   constructor(name) {
