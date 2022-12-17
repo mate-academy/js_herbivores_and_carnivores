32f799b99f621cceb9180073aa4c6dd54c53841a
volkan yildiz <yildizvolkan91@gmail.com>
2022-12-17 21:48:05 +0300

    sol1

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,42 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor() {
+    super();
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(beast) {
+    if (beast instanceof Carnivore || beast.hidden) {
+      return;
+    }
+
+    beast.health -= 50;
+
+    if (beast.health <= 0) {
+      const checkName = animal => animal.name !== beast.name;
+
+      Animal.alive = Animal.alive.filter(checkName); ;
+    }
+  }
 }
 
 module.exports = {
