3255641c995df9c29da90ec13dbdaca4ef04c3f0
Artem Rymarchuk <rumarchyk204@gmail.com>
2022-12-18 11:35:23 +0100

    Fixes

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -5,13 +5,14 @@ class Animal {
     this.name = name;
     this.health = 100;
 
-    Animal.alive.push((this));
+    Animal.alive.push(this);
   }
 }
 
 class Herbivore extends Animal {
   constructor(name) {
     super(name);
+
     this.hidden = false;
   }
 
@@ -21,12 +22,15 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
-  bite(juicySteak) {
-    if (juicySteak instanceof Herbivore && !juicySteak.hidden) {
-      juicySteak.health -= 50;
+  bite(herbivore) {
+    const visibleHerbivore
+      = herbivore instanceof Herbivore && !herbivore.hidden;
+
+    if (visibleHerbivore) {
+      herbivore.health -= 50;
     }
 
-    if (juicySteak.health === 0) {
+    if (herbivore.health === 0) {
       Animal.alive = Animal.alive.filter(animal => animal.health > 0);
     }
   }
