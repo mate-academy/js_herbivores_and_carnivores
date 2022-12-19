f450e58ceb0dbacbf7ef60d13d9d4487ab01c084
Jack Ashkar <jackelashkar@gmail.com>
2022-12-19 14:58:22 +0300

    finished

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,42 @@
 
 class Animal {
   // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    Animal.alive.push(this);
+  }
 }
+
+Animal.alive = [];
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(name) {
+    super(name);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  bite(beast) {
+    if (beast instanceof Herbivore && !beast.hidden) {
+      beast.health -= 50;
+    }
+
+    if (beast.health <= 0) {
+      const checkName = animal => animal.name !== beast.name;
+
+      Animal.alive = Animal.alive.filter(checkName); ;
+    }
+  }
 }
 
 module.exports = {
