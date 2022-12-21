55bc7c722cb653436036006f2e851e3d0552359a
Rafael Mamedov <rafmamedov@gmail.com>
2022-12-21 10:49:26 +0200

    Changes implemented

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -24,7 +24,7 @@ class Herbivore extends Animal {
 class Carnivore extends Animal {
   bite(animal) {
     if (animal instanceof Herbivore
-      && animal.hidden !== true) {
+      && !animal.hidden) {
       animal.health -= 50;
     }
 
