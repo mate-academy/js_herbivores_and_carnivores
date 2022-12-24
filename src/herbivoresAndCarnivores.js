fe7a2e1d6be73cc10391a69c7c5b55904a40bfe5
EhorSaprykin <astralik1993@gmail.com>
2022-12-24 19:57:58 +0200

    Task done

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,42 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static addAnimal(animal) {
+    Animal.alive.push(animal);
+  }
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.addAnimal(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herb) {
+    if (herb instanceof Herbivore && !herb.hidden) {
+      herb.health -= 50;
+
+      if (herb.health === 0) {
+        const removedAnimal = Animal.alive.indexOf(herb);
+
+        Animal.alive.splice(removedAnimal, 1);
+      }
+    }
+  }
 }
 
 module.exports = {
