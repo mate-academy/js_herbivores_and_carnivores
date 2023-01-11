95b8b9fbe9bee82d146ee08e3c8d9d96cc12283b
ihorivna007 <emmakorovnik@gmail.com>
2023-01-11 17:57:57 +0200

    initial commit

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,37 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(victim) {
+    if (victim instanceof Herbivore && !victim.hidden) {
+      victim.health -= 50;
+    }
+
+    if (!victim.health) {
+      Animal.alive.splice(Animal.alive.indexOf(victim));
+    }
+  }
 }
+
+Animal.alive = [];
 
 module.exports = {
   Animal,
