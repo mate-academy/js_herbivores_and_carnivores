26f8f4ed85cd69c856f91bdf2dbdb9a388aadd22
svitjojo <kornotavr@gmail.com>
2023-01-12 14:52:09 +0200

    Classes Animal, Herbivore, Carnivire was added

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,37 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.health = 100;
+    this.name = name;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+  hide() {
+    this.hidden = true;
+  };
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(attack) {
+    if (!attack.hidden && attack instanceof Herbivore) {
+      attack.health -= 50;
+    }
+
+    if (Animal.alive.includes(attack) && !attack.health) {
+      const died = Animal.alive.indexOf(attack);
+
+      Animal.alive.splice(died, 1);
+    }
+  }
 }
 
 module.exports = {
