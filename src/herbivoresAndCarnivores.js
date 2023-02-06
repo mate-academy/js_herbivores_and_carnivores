9836bc1058cf28c4fff31d4f5eb83b164c047e00
Oleksandr <98667890az@gmail.com>
2023-02-06 23:32:37 +0200

    Fixed

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,9 +1,9 @@
 'use strict';
 
 class Animal {
-  constructor(name, health) {
+  constructor(name, health = 100) {
     this.name = name;
-    this.health = health || 100;
+    this.health = health;
     Animal.alive.push(this);
   }
 }
@@ -14,23 +14,17 @@ class Herbivore extends Animal {
   constructor(name, health, hidden) {
     super(name, health, hidden);
 
-    this.hidden = hidden || false;
+    this.hidden = false;
   }
 
   hide() {
-    if (this.hidden === true) {
-      this.hidden = false;
-    }
-
-    if (this.hidden === false) {
       this.hidden = true;
-    }
   }
 }
 
 class Carnivore extends Animal {
   bite(animal) {
-    if (animal instanceof Herbivore && animal.hidden === false) {
+    if (animal instanceof Herbivore && !animal.hidden) {
       animal.health -= 50;
 
       Animal.alive = Animal.alive.filter(item => item.health > 0);
