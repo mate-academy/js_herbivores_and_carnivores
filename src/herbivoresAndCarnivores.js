008e7385c566545af55409379d6a12b0655308aa
Jaxxxon78 <jaxxxon78@ukr.net>
2023-02-06 16:27:43 +0200

    done

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,44 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(
+    name,
+    health,
+  ) {
+    this.name = name;
+    this.health = health || 100;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(
+    name,
+    health,
+    hidden,
+  ) {
+    super(
+      name,
+      health,
+      hidden,);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(item) {
+    if (item.hidden !== true && item instanceof Herbivore) {
+      item.health -= 50;
+    }
+    Animal.alive = Animal.alive.filter(el => el.health > 0);
+  }
 }
 
 module.exports = {
