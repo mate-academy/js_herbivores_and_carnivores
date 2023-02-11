710bd22d14d07c22b9ac03a2f579600aaa48a4aa
Vladyslav Ashurov <ashurovvladislav@gmail.com>
2023-02-11 18:01:28 +0200

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,45 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.health = health;
+    this.name = name;
+
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = []; // need to use static propery instead
+
+Animal.deleteDead = function() {
+  for (let i = 0; i < Animal.alive.length; i++) {
+    if (Animal.alive[i].health === 0) {
+      Animal.alive.splice(i, 1);
+    }
+  }
+};
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health, hidden = false) {
+    super(name, health);
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(obj) {
+    if (!obj.hidden && obj instanceof Herbivore) {
+      obj.health -= 50;
+
+      if (obj.health === 0) {
+        Animal.deleteDead();
+      }
+    }
+  }
 }
 
 module.exports = {
