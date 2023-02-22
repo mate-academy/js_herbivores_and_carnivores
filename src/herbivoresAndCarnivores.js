ba677d1ca074696ac4f6b40c395ba573fc4b20be
Nazarii Piatnochka <nazar.piatnochka@gmail.com>
2023-02-22 19:17:37 +0200

    Tests were completed successfully

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,42 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health) {
+    this.name = name;
+    this.health = health || 100;
+
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, healt) {
+    super(name, healt);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
+}
+
+if (Herbivore.health <= 0) {
+  Animal.alive = Animal.alive.filter(animal => animal.health > 0);
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herbivore) {
+    if (!herbivore.hidden && herbivore.hidden === false) {
+      herbivore.health -= 50;
+
+      if (herbivore.health <= 0) {
+        Animal.alive.splice(Animal.alive.indexOf(herbivore), 1);
+      }
+    }
+  }
 }
 
 module.exports = {
