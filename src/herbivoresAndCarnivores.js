161269f4615e1cda2515ba291f7b1735fe140841
gretoq <dovhun3d@gmail.com>
2023-03-01 15:46:52 +0200

    Made factory of herbivores and carnivores which are have some abilities

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,42 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    this.pushAlive();
+  }
+
+  pushAlive() {
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, hidden = false) {
+    super(name);
+
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(whom) {
+    const isHerbivore = whom instanceof Herbivore;
+
+    if (isHerbivore && !whom.hidden) {
+      whom.health -= 50;
+
+      Animal.alive = Animal.alive.filter(animal => animal.health > 0);
+    }
+  }
 }
 
 module.exports = {
