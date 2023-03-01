9e528a35a760a85aba0279088e1c8fbbc8c77d7f
Rashyd <rashydlezgi@gmail.com>
2023-03-01 19:08:41 +0200

    IF statement was rewrited

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -25,7 +25,7 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(herbivore) {
-    if (!herbivore.hidden) {
+    if (herbivore instanceof Herbivore && !herbivore.hidden) {
       herbivore.health -= 50;
 
       if (herbivore.health <= 0) {
