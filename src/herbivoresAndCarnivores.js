81b2413c5744478a0a8a10bec8749a51d809b6e0
Sergii Mishchenko <serge.mishchenko@gmail.com>
2023-03-01 12:41:02 +0300

    Herbivores and Сarnivores were created

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,35 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  health = 100;
+  hidden = false;
+
+  constructor(name) {
+    this.name = name;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(beast) {
+    if (beast.hidden === false && beast instanceof Herbivore === true) {
+      beast.health -= 50;
+
+      for (let i = 0; i < Animal.alive.length; i++) {
+        if (Animal.alive[i].health === 0) {
+          delete Animal.alive[i];
+        }
+      }
+    }
+  }
 }
 
 module.exports = {
