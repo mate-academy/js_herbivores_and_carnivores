f92d2581db24dcbf9b1e01232ab264cb380dad43
vadosik15 <vadim.bratus@gmail.com>
2023-03-05 14:45:05 +0200

    did the program

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,43 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    Animal.alive.push(this);
+  }
+
+  static animalDied(diedAnimal) {
+    Animal.alive = Animal.alive
+      .filter((animal) => animal !== diedAnimal);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(prey) {
+    if (!prey.hidden && prey instanceof Herbivore) {
+      prey.health -= 50;
+    }
+
+    if (prey.health <= 0) {
+      Animal.animalDied(prey);
+    }
+  }
 }
 
 module.exports = {
