f1cdb5a89a3509085c95fef634a187158a0bb2a4
Sergey Lipikhin <football3@ukr.net>
2023-03-06 13:45:45 +0200

    Remove negative health handling

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -7,18 +7,6 @@ class Animal {
 
     Animal.alive.push(this);
   }
-
-  set health(value) {
-    if (value >= 0) {
-      this._health = value;
-    } else {
-      this._health = 0;
-    }
-  }
-
-  get health() {
-    return this._health;
-  }
 }
 
 Animal.alive = [];
