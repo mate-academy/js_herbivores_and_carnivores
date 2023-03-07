54e9ed35cba83efba23633f7d823458a83b79749
NikitaStambul <stambul.n@gmail.com>
2023-03-07 10:33:06 +0200

    codestyle fix

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -26,11 +26,9 @@ class Carnivore extends Animal {
     if (animal instanceof Herbivore && !animal.hidden) {
       animal.health -= 50;
 
-      if (animal.health === 0) {
-        Animal.alive = Animal.alive.filter(element => {
-          return element !== animal;
-        });
-      }
+      Animal.alive = Animal.alive.filter(({ health }) => {
+        return health > 0;
+      });
     }
   }
 }
