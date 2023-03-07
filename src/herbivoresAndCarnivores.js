3d43ca1274a3a7902d8a356c3c29c5e57c804957
Bozhena Bodnar <bozhena2705bodnar@gmail.com>
2023-03-07 17:26:03 +0100

    fix some in code

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -9,10 +9,9 @@ class Animal {
     this.name = name;
     this.health = health;
     Animal.add(this);
-    Animal.notDeath();
   }
 
-  static notDeath() {
+  static removeDead() {
     this.alive = this.alive.filter(animal => animal.health > 0);
   }
 }
@@ -35,7 +34,7 @@ class Carnivore extends Animal {
   bite(animal) {
     if (animal instanceof Herbivore && animal.hidden === false) {
       animal.health -= 50;
-      Animal.notDeath();
+      Animal.removeDead();
     }
   }
 }
