078d9e8821dfa5a57cfacca14b11fa2b975b04e6
Anna Grechana <grechanaanna@gmial.com>
2023-03-10 22:06:15 +0200

    1st version

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,44 @@
 'use strict';
 
 class Animal {
+  static alive =[]
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+
+    if (this.health) {
+      Animal.alive.push(this);
+    }
+  }
   // write your code here
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  } // write your code here
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+  }
+
+  bite(food) {
+    if (!(food instanceof Carnivore || food.hidden)) {
+      food.health -= 50;
+    }
+
+    if (!food.health) {
+      Animal.alive.pop(food);
+    }// write your code here
+  }
 }
 
 module.exports = {
