a88fe6d0d28fb2fbd32785f8d24d2a3944c2c93a
halyna serpykhovitina <hserpykhovitina@gmail.com>
2023-03-15 19:12:56 +0200

    Solutin with changes

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -4,6 +4,7 @@ class Animal {
   constructor(name, health = 100) {
     this.name = name;
     this.health = health;
+    Animal.alive.push(this);
   }
 }
 
@@ -13,7 +14,6 @@ class Herbivore extends Animal {
   constructor(name, health, hidden = false) {
     super(name, health);
     this.hidden = hidden;
-    Animal.alive.push(this);
   }
 
   hide() {
@@ -22,18 +22,19 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
-  constructor(name, health) {
-    super(name, health);
-    Animal.alive.push(this);
+  // constructor(name, health) {
+  // super(name, health)
+  // Animal.alive.push(this);
+
+  bite(target) {
+    if (target instanceof Herbivore & target.hidden === false) {
+      target.health -= 50;
+
+      if (target.health === 0) {
+        Animal.alive = Animal.alive.filter(item => item.health !== 0);
+      }
+    };
   }
-
-  bite(sameObject) {
-    if (sameObject instanceof Herbivore & sameObject.hidden === false) {
-      sameObject.health -= 50;
-
-      Animal.alive = Animal.alive.filter(item => item.health !== 0);
-    }
-  };
 }
 
 module.exports = {
