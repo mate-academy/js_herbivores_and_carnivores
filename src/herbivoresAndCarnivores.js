54cae3a2445682d1dcbf34116ed290a0395cde0c
halyna serpykhovitina <hserpykhovitina@gmail.com>
2023-03-17 20:31:08 +0200

    Solutin with all changes

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -23,11 +23,11 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(target) {
-    if (target instanceof Herbivore & target.hidden === false) {
+    if (target instanceof Herbivore && !target.hidden) {
       target.health -= 50;
 
-      if (target.health === 0) {
-        Animal.alive = Animal.alive.filter(item => item.health !== 0);
+      if (target.health <= 0) {
+        Animal.alive = Animal.alive.filter(item => item.health > 0);
       }
     };
   }
