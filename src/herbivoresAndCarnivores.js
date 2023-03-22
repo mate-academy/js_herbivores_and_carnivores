3dc9f4a9a38ab6b556029eee1ccf1152710d31ae
Daria <hamdsad8@gmail.com>
2023-03-22 12:58:06 +0200

    created animal class

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,53 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    this.alive = [];
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health, hidden = false) {
+    super(name, health);
+    this.hidden = hidden;
+
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+
+    Animal.alive.push(this);
+  }
+
+  bite(animal) {
+    const isHerbivore = animal.hasOwnProperty('hidden');
+
+    if (!isHerbivore || animal.hidden) {
+      return;
+    }
+
+    animal.health -= 50;
+
+    if (animal.health <= 0) {
+      Animal.alive = Animal.alive.filter(
+        (storedAnimal) => storedAnimal.name !== animal.name
+      );
+    }
+  }
 }
+
+Animal.alive = [];
 
 module.exports = {
   Animal,
