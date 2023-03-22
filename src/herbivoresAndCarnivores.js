29efa2d5afcc36e5ad4bace43fee252cb9d93743
Mikhieiev Ivan <navigator79@gmail.com>
2023-03-22 20:40:57 +0200

    ClassTask

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,47 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(
+    name,
+    health = 100,
+  ) {
+    this.health = health;
+    this.name = name;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(
+    health,
+    name,
+    hidden = false,
+  ) {
+    super(health, name);
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herbivore) {
+    if (herbivore instanceof Herbivore && !herbivore.hidden) {
+      herbivore.health -= 50;
+    }
+
+    Animal.alive = Animal.alive.filter(animal => {
+      if (animal instanceof Herbivore && animal.health === 0) {
+        return false;
+      }
+
+      return true;
+    });
+  }
 }
 
 module.exports = {
