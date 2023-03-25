b600cfa924c84dcefb9c2d90cf3ed14d2910255d
DmytroHorshunov <cnimipignak@icloud.com>
2023-03-25 19:02:27 +0200

    Animal

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,41 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health = 100, hidden) {
+    super(name, health, hidden);
+  }
+
+  hide() {
+    this.hidden = true;
+    Animal.alive = Animal.alive.filter(p => p.health > 0);
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health = 100, hidden) {
+    super(name, health, hidden);
+  }
+
+  bite(animal) {
+    if (!animal.hidden) {
+      animal.health -= 50;
+      Animal.alive = Animal.alive.filter(p => p.health > 0);
+
+      if (animal instanceof Carnivore) {
+        animal.health = this.health;
+      }
+    }
+  }
 }
 
 module.exports = {
