88ee51e9dda028f5dce4a7d1f6da221aa408a022
Anna Peretiatko <tobossanna@gmail.com>
2023-03-27 12:40:15 +0200

    HerbivoresAndCarnivores solution v.2

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,33 +1,19 @@
 'use strict';
 
 class Animal {
-  constructor(health, name) {
+  constructor(name, health = 100) {
     this.health = health;
-    this.health = 100;
-
     this.name = name;
-
-    if (!Animal.alive) {
-      Animal.alive = [];
-    }
     Animal.alive.push(this);
-  }
-
-  bite(animal) {
-    if (animal instanceof Herbivore && animal.hidden === false) {
-      animal.health -= 50;
-    }
-
-    if (animal.health === 0) {
-      Animal.alive = Animal.alive.filter(item => item.health > 0);
-    }
   }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  constructor(health, name, hidden) {
-    super(health, name);
-    this.hidden = hidden || false;
+  constructor(name, health, hidden = false) {
+    super(name, health);
+    this.hidden = hidden;
   }
 
   hide() {
@@ -36,6 +22,15 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
+  bite(animal) {
+    if (animal instanceof Herbivore && animal.hidden === false) {
+      animal.health -= 50;
+    }
+
+    if (!animal.health) {
+      Animal.alive = Animal.alive.filter(item => item.health > 0);
+    }
+  }
 }
 
 module.exports = {
