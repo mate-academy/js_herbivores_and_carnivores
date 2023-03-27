f564a9159295c80aa68f788c960a3d400d34bcf3
Vadym Dubniak <dubnyakv@gmail.com>
2023-03-27 19:51:29 +0300

    added animal classes realizaiton

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,40 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(prey) {
+    if (prey instanceof Carnivore || prey.hidden) {
+      return;
+    }
+
+    prey.health -= 50;
+
+    if (prey.health <= 0) {
+      Animal.alive = Animal.alive.filter(animal => animal.name !== prey.name);
+    }
+  }
 }
 
 module.exports = {
