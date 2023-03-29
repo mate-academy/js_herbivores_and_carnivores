f545d5dfaceb663233f413dca2b1dafc9ddb5af9
Pavlo Reutskiy <pavel.reutskiy@gmail.com>
2023-03-29 14:52:53 +0300

    developed nature observation tool

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,41 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  // static alive = [];
+
+  static controlPopulation(creature) {
+    this.alive.push(creature);
+  }
+
+  constructor(name, health) {
+    this.name = name;
+    this.health = health || 100;
+    Animal.controlPopulation(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  };
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(prey) {
+    if (prey instanceof Herbivore && !prey.hidden) {
+      prey.health -= 50;
+    }
+
+    Animal.alive = Animal.alive.filter(({ health }) => health !== 0);
+  };
 }
 
 module.exports = {
