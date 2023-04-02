a3c872279b5d1bd485a9ef4ed618df67450faaf9
Tolik Serhiychuk <sergiytchoukk@gmail.com>
2023-04-02 18:12:48 +0300

    Herbivores snd carnivores solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,43 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    Animal.alive.push(this);
+  }
+
+  bite(victimObject) {
+    if (victimObject.hidden !== true && victimObject instanceof Herbivore) {
+      victimObject.health -= 50;
+    }
+
+    if (victimObject.health <= 0) {
+      Animal.alive = Animal.alive.filter(animal => animal !== victimObject);
+
+      return Animal.alive;
+    }
+  }
 }
+
+Animal.alive = [];
 
 module.exports = {
   Animal,
