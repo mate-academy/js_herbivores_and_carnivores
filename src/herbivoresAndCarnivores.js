04a33fbe3dedcf4147325dda2d1fe4c2560f09e0
Victor Buldenko <gookidoo@gmail.com>
2023-04-13 18:31:33 +0300

    done

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,43 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+    Animal.alive.push(this);
+  }
+  bite(target) {
+    const status = target instanceof Herbivore;
+
+    if (target.hidden === false && status) {
+      target.health -= 50;
+    }
+
+    if (!target.health) {
+      Animal.alive = Animal.alive.filter(el => {
+        return el !== target;
+      });
+    }
+  }
 }
 
 module.exports = {
