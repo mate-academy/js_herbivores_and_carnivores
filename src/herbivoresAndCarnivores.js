844be705db1174c3aca543a351629e75edc185c2
Kyryl Maslov <kyrylmaslov2007@gmail.com>
2023-04-13 23:28:19 +0200

    herbivores and canvivores was created

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,49 @@
 'use strict';
-
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+  }
+
+  static addAnimal(animal) {
+    this.alive.push(animal);
+  }
+
+  static deleteAnimal(herbivore) {
+    this.alive = this.alive.filter((ann, animalIndex) =>
+      animalIndex !== this.alive.findIndex(animal => animal === herbivore));
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+
+    this.hidden = false;
+    Animal.addAnimal(this);
+  }
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+
+    Animal.addAnimal(this);
+  }
+  bite(animal) {
+    if (animal.hasOwnProperty('hidden') && animal.hidden !== true) {
+      animal.health -= 50;
+
+      if (animal.health === 0) {
+        Animal.deleteAnimal(animal);
+      }
+    }
+  }
 }
 
 module.exports = {
