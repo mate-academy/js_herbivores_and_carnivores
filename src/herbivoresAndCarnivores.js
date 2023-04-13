2783ff4d71d17fa8f0ef9e20a5fa26014be1570e
Katarzyna Kubisiak <kasiakubisiak@Katarzynas-MacBook-Pro.local>
2023-04-13 17:45:20 +0200

    Nested loops fix

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -31,10 +31,7 @@ class Carnivore extends Animal {
   bite(animal) {
     if (animal instanceof Herbivore && !animal.hidden) {
       animal.health -= 50;
-
-      if (animal.health <= 0) {
-        animal.killAnimal();
-      }
+      animal.killAnimal();
     }
   }
 }
