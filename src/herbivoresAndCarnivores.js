095ad4eb8ab07faff24e9d256d24c6f3ed6cd1f9
Roman Moroz <65686226+morozit@users.noreply.github.com>
2023-04-13 22:53:36 +0300

    Moroz

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,36 +1,49 @@
 'use strict';
 
 class Animal {
-  constructor(health = 100, name, hidden) {
+  constructor(health = 100, name) {
     this.health = health;
     this.name = name;
-    this.hidden = hidden;
+
+    Animal.alive.push(this);
   }
 
-  static alive = [];
-
-  static removeAnimal(animal) {
-
-  }
+  // kill() {
+  //   Animal.alive = Animal.alive.filter(beast => beast.health < 0);
+  // }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  constructor(health, name, hidden) {
-    super(health, name, hidden);
+  constructor(health, name, hidden = false) {
+    super(health, name);
+    this.hidden = hidden;
   }
   hide() {
-    hidden = false;
+    this.hidden = true;
   }
 }
 
 class Carnivore extends Animal {
+  // constructor(health, name) {
+  //   super(health, name);
+  // }
 
-  constructor(health, name, hidden) {
-    super(health, name, hidden);
-  }
-  // write your code here
-  bite() {
-    health -= 50;
+  bite(animal) {
+    if (animal instanceof Carnivore && !animal.hidden) {
+      // return;
+      animal.health -= 50;
+    }
+
+    // animal.health -= 50;
+
+    // if (animal.health <= 0) {
+    //   animal.kill();
+    // }
+    if (animal.health <= 0) {
+      Animal.alive = Animal.alive.filter(beast => beast !== animal);
+    }
   }
 }
 
