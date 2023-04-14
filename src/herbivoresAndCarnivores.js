7c11030c8864279050fb8ad2eea1f9c7caf51505
Paweł Smoliński <ski1993@o2.pl>
2023-04-14 12:10:44 +0200

    solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,39 @@
 
 class Animal {
   // write your code here
+  constructor(name, health = 100) {
+    if (!Animal.alive) {
+      Animal.alive = [];
+    }
+    this.health = health;
+    this.name = name;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(health, name) {
+    super(health, name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  };
 }
 
 class Carnivore extends Animal {
   // write your code here
+  bite(target) {
+    if (target instanceof Herbivore && target.hidden === false) {
+      target.health -= 50;
+    }
+
+    if (target.health === 0) {
+      Animal.alive = Animal.alive.filter(animal => animal !== target);
+    }
+  }
 }
 
 module.exports = {
