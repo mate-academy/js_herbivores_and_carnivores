f00e84e4a4789c8773d27701af89401e424dd51a
anton_chornobai <antonchornobajj@gmail.com>
2023-04-16 20:09:43 +0200

    Task was done

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,40 @@
 'use strict';
 
 class Animal {
-  // write your code here
-}
+  constructor(name) {
+    this.health = 100;
+    this.name = name;
+    Animal.alive.push(this)
+  }
+} 
+
+ Animal.alive = [];
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+    this.health = 100;
+  }
+
+  hide() {
+    const hide = this.hidden;
+    hide === false ? this.hidden = true : this.hidden = false;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor( name) {
+    super(name)
+    this.health = 100;
+  };
+
+  bite(herbivore) {
+    if (herbivore.hidden === false && herbivore instanceof Herbivore) {
+      herbivore.health -= 50
+      Animal.alive = Animal.alive.filter(animal => animal.health > 0);
+    }
+  }
 }
 
 module.exports = {
