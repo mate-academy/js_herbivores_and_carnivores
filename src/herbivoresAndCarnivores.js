45ce1e64e7de092cd572439b44a05e71bbdb06e3
Mateusz Michalowski <mateusz.michalowski000@gmail.com>
2023-04-17 17:22:06 +0200

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,41 @@
 
 class Animal {
   // write your code here
+  constructor(name) {
+    this.health = 100;
+    this.name = name;
+    Animal.alive.push(this);
+  }
+
+  static alive = [];
 }
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+
+  bite(prey) {
+    if (prey.hidden === false && prey instanceof Herbivore) {
+      prey.health -= 50;
+    }
+
+    if (prey.health <= 0) {
+      Animal.alive.splice(
+        Animal.alive.indexOf(
+          animal => Animal.alive.animal.name === prey.name), 1);
+    }
+  }
 }
 
 module.exports = {
