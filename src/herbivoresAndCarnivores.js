95241dadbcc46c56ce0ce4836b566c792c0d5a17
Rufat Mammadov <rufat.mammadov.m@gmail.com>
2023-04-25 21:10:30 +0300

    herbivores & carnivores - first try

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,40 @@
 
 class Animal {
   // write your code here
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
 }
+
+Animal.alive = [];
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(name) {
+    super(name);
+
+    this.hidden = false;
+  };
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  bite(object) {
+    if (object.hidden === false) {
+      object.health -= 50;
+    }
+
+    if (object.health === 0) {
+      Animal.alive.splice(Animal.alive.indexOf(object), 1);
+    }
+  }
 }
 
 module.exports = {
