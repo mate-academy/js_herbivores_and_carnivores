b532aa736e5d8d5f7a14eff33ff9d8934a783e75
DefaultGitUser <usingforgit@gmail.com>
2023-05-08 11:05:05 +0300

    sol

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,38 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  };
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herbivore) {
+    if (herbivore instanceof Herbivore
+      && !herbivore.hidden) {
+      herbivore.health -= 50;
+    }
+
+    // Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
+    if (herbivore.health <= 0) {
+      delete Animal.alive[Animal.alive.indexOf(herbivore)];
+    }
+  }
 }
 
 module.exports = {
