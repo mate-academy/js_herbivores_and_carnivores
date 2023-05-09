67e56e5fd1b466f6d334af472aec9ccd5d7bd551
Yaroslav Liutyi <slavko5481@gmail.com>
2023-05-09 22:26:56 +0300

    updated

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -9,7 +9,6 @@ class Animal {
 }
 
 Animal.alive = [];
-
 class Herbivore extends Animal {
   constructor(name) {
     super(name);
@@ -19,14 +18,13 @@ class Herbivore extends Animal {
     this.hidden = true;
   }
 }
-
 class Carnivore extends Animal {
   bite(animal) {
-    if (animal instanceof Herbivore && animal.hidden === false) {
+    if (animal instanceof Herbivore && !animal.hidden) {
       animal.health -= 50;
     }
 
-    if (animal.health === 0) {
+    if (animal.health >= 0) {
       Animal.alive = Animal.alive.filter(i => i.health > 0);
     }
   }
