ae23826862ee218029a2559a9ccd0eb4b1cf748e
Igor Galkin <igor_galkin@ukr.net>
2023-05-16 19:41:07 +0300

    1st commi

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,36 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(obj) {
+    if (obj.hidden === false) {
+      obj.health -= 50;
+    }
+
+    const filteredAnimal = Animal.alive.filter(({ health }) => health > 0);
+
+    Animal.alive = filteredAnimal;
+  }
 }
 
 module.exports = {
