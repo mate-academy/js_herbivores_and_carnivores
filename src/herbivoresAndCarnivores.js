0db77d1192a2449a97bf424bfa9855516a308c78
AlexKherson <q0992358472@gmail.com>
2023-05-23 23:45:32 +0300

    added classes Animal, Herbivore, Carnivore

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,35 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (!animal.hidden && !(animal instanceof Carnivore)) {
+      animal.health -= 50;
+
+      Animal.alive = Animal.alive
+        .filter(item => item.health !== 0);
+    }
+  }
 }
 
 module.exports = {
