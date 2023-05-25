e560f7085696dc92b868d2b802854dde6acb15ec
Leonid Krainik <rialleons@gmail.com>
2023-05-25 12:41:08 +0300

    Create Herbivores and Carnivores classes

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,40 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(...args) {
+    super(...args);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (animal instanceof Herbivore && !animal.hidden) {
+      animal.health -= 50;
+    }
+
+    if (animal.health <= 0) {
+      const removeIndex = Animal.alive.indexOf(animal);
+
+      Animal.alive.splice(removeIndex, 1);
+    }
+  }
 }
+
+Animal.alive = [];
 
 module.exports = {
   Animal,
