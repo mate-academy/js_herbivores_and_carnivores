4cff48410eba35f45327f7a91c9972a71cfd96ad
Oleksii Lysenko <nano77@ukr.net>
2023-05-26 22:19:01 +0300

    add functionality to herbivoresAndCarnivores

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,41 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    Animal.alive.push(this);
+    this.health = 100;
+    this.name = name;
+  }
 }
 
+Animal.alive = [];
+
+Animal.reduceAllDeadAnimals = () => {
+  Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
+};
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    const isHerbivore = animal instanceof Herbivore;
+    const isAlive = Animal.alive.includes(animal);
+    const isNotHidding = !animal.hidden;
+
+    if (isHerbivore && isAlive && isNotHidding) {
+      animal.health -= 50;
+      Animal.reduceAllDeadAnimals();
+    }
+  }
 }
 
 module.exports = {
