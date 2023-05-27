eb84b54f08e7e4338ad2a414d54aee94238e3b26
Katya Zaremska <unnarummel@gmail.com>
2023-05-27 12:21:01 +0300

    add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -4,17 +4,30 @@ class Animal {
   constructor(name, health = 100) {
     this.name = name;
     this.health = health;
-
     Animal.alive.push(this);
+  }
+
+  static removeFromAlive(animal) {
+    Animal.alive = Animal.alive.filter((el) => el.health > 0);
+  }
+
+  bite(animal) {
+    if (animal instanceof Herbivore && !animal.hidden) {
+      animal.health -= 50;
+    }
+
+    if (animal.health <= 0) {
+      Animal.removeFromAlive(animal);
+    }
   }
 }
 
 Animal.alive = [];
 
 class Herbivore extends Animal {
-  constructor(name, health = 100, hidden = false) {
+  constructor(name, health = 100) {
     super(name, health);
-    this.hidden = hidden;
+    this.hidden = false;
   }
 
   hide() {
@@ -25,13 +38,6 @@ class Herbivore extends Animal {
 class Carnivore extends Animal {
   constructor(name, health = 100) {
     super(name, health);
-  }
-
-  bite(animal) {
-    if (animal instanceof Herbivore && !animal.hidden) {
-      animal.health -= 50;
-    }
-    Animal.alive = Animal.alive.filter(el => el.health !== 0);
   }
 }
 
