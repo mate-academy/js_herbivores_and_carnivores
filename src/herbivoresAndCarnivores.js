e46f0eef235d72be5ed1d7db85b552300bf5e9f4
Vania Yarovyi <yarovyi@gmail.com>
2023-05-29 17:23:17 +0300

    add solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,36 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static get alive() {
+    return this.animalsCreated
+      .filter(animal => animal.health > 0);
+  }
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.animalsCreated.push(this);
+  }
 }
 
+Animal.animalsCreated = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(victim) {
+    if (victim.constructor.name === 'Herbivore' && victim.hidden !== true) {
+      victim.health -= 50;
+    }
+  }
 }
 
 module.exports = {
