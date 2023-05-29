ee0d7376c15875a17dd3575cf43be096ecbd15ec
Vladyslav Lapshyn <lapshin.vlad96@gmail.com>
2023-05-29 21:27:48 +0300

    made task js herbivores and carnivores

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,46 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  static addAnimal(animal) {
+    this.alive.push(animal);
+  }
+
+  static removeAnimal() {
+    this.alive = this.alive.filter(animal => animal.health > 0);
+  }
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    Animal.addAnimal(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(victim) {
+    if (victim.hidden !== true && victim instanceof Herbivore) {
+      victim.health -= 50;
+    }
+
+    if (victim.health <= 0) {
+      Animal.removeAnimal();
+    }
+  }
 }
 
 module.exports = {
