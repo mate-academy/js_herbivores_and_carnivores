af072a1a04a15bfcd5bcfc262dd8574db6789bf8
Yehor Myronov <2726048@gmail.com>
2023-05-30 14:31:28 +0300

    add corrections

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,12 +1,15 @@
 'use strict';
 
 class Animal {
+  static removeDied() {
+    this.alive = this.alive.filter(animal => animal.health > 0);
+  }
+
   constructor(name) {
     this.name = name;
     this.health = 100;
     Animal.alive.push(this);
   }
-
 }
 
 Animal.alive = [];
@@ -25,12 +28,9 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(animal) {
-    if (animal instanceof Herbivore && animal.hidden === false) {
+    if (animal instanceof Herbivore && !animal.hidden) {
       animal.health -= 50;
-    }
-
-    if (animal.health <= 0) {
-      Animal.alive = Animal.alive.filter(prey => prey.name !== animal.name);
+      Animal.removeDied();
     }
   }
 }
