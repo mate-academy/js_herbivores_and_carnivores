61fea514dd0f736a179b77cb5ea228690b602cd6
Anton Cherepinets <manickmage@ukr.net>
2023-06-01 23:24:19 +0300

    sol

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,13 +1,17 @@
 'use strict';
 
 class Animal {
-  static alive = [];
   constructor(name, health = 100) {
     this.name = name;
     this.health = health;
     Animal.alive.push(this);
   }
+  static removeDeadAnimals() {
+    Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
+  }
 }
+
+Animal.alive = [];
 
 class Herbivore extends Animal {
   constructor(name, health) {
@@ -25,10 +29,8 @@ class Carnivore extends Animal {
       animal.health -= 50;
     }
 
-    if (animal.health === 0) {
-      const index = Animal.alive.indexOf(animal);
-
-      Animal.alive.splice(index, 1);
+    if (animal.health <= 0) {
+      Animal.removeDeadAnimals();
     }
   }
 }
