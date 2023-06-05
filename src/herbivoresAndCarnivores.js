cffb9944c366393ab601c016e5e7a545817b34e6
Liza Strykharchuk <ol220681vol@gmail.com>
2023-06-05 18:30:54 +0300

    code

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,45 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health) {
+    this.name = name;
+    this.health = health || 100;
+    Animal.alive.push(this);
+  }
+
+  die() {
+    const index = Animal.alive.indexOf(this);
+
+    if (index !== -1) {
+      Animal.alive.splice(index, 1);
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(isBite) {
+    if (isBite instanceof Herbivore && isBite.hidden === false) {
+      isBite.health -= 50;
+    }
+
+    if (isBite.health >= 0) {
+      isBite.die();
+    }
+  }
 }
+
+Animal.alive = [];
 
 module.exports = {
   Animal,
