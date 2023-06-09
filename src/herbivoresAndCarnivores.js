f2fe5b117d5640a9ac5a9b760699a5ad9208232e
Vadym Vitruk <vadym_v@ukr.net>
2023-06-09 17:38:01 +0300

    fixed according review

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,11 +1,9 @@
 'use strict';
 
-const healPoints = require('./healPoints');
-
 class Animal {
   constructor(name) {
     this.name = name;
-    this.health = healPoints(100);
+    this.health = 100;
     Animal.alive.push(this);
   }
 
@@ -34,9 +32,9 @@ class Carnivore extends Animal {
   }
 
   bite(animal) {
-    animal instanceof Herbivore && !animal.hidden
-      ? animal.health -= this.damage
-      : animal.health = this.health;
+    if (animal instanceof Herbivore && !animal.hidden) {
+      animal.health -= this.damage;
+    }
 
     if (animal.health <= 0) {
       Animal.removeDiedAmimals();
