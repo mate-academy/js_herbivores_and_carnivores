87d1f96d3065bbf9e2fd72c1f96db117d922b220
Serhii Sorokotiaha <dp080793ssk@gmail.com>
2023-06-14 00:36:08 +0300

    Solution v2

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -24,7 +24,7 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(animal) {
-    if (animal.hidden !== true && animal instanceof Herbivore) {
+    if (!animal.hidden && animal instanceof Herbivore) {
       animal.health -= 50;
     }
 
