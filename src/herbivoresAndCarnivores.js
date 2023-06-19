a865d90dd69ca8fc37503670e6b4997ff873b08a
your_username <web.shepel@gmail.com>
2023-06-19 14:18:16 +0300

    solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,44 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    Animal.alive.push(this);
+  }
+
+  decreaseHealth(num) {
+    const posOfAll = Animal.alive.indexOf(this);
+
+    this.health -= num;
+
+    if (this.health <= 0 && posOfAll > -1) {
+      Animal.alive.splice(posOfAll, 1);
+    }
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (animal instanceof Herbivore && !animal.hidden) {
+      animal.decreaseHealth(50);
+    }
+  }
 }
 
 module.exports = {
