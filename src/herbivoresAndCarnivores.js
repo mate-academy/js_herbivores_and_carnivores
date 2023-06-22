9b6357d91e75c6a32f5bad4427f2004aab76a402
Eugene Kulish <genekulish13@gmail.com>
2023-06-22 16:10:22 +0300

    solve the task

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,42 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+  }
+
+  static alive = [];
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    this.hidden = (this.hidden === false);
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    Animal.alive.push(this);
+  }
+
+  bite(animal) {
+    animal.health
+      -= (animal instanceof Herbivore && animal.hidden === false)
+        ? 50
+        : 0;
+
+    Animal.alive = (animal.health === 0)
+      ? Animal.alive.filter(beast => beast.health > 0)
+      : Animal.alive;
+  }
 }
 
 module.exports = {
