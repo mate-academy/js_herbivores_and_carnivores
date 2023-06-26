78f6f9aadad71a58f5c070f6b8e127f363353e55
Denis Hurskiy <denis.gurskiy@gmail.com>
2023-06-26 22:11:45 +0300

    herbivores

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,47 @@
 
 class Animal {
   // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
 }
+
+Animal.alive = [];
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  constructor(name) {
+    super(name);
+    Animal.alive.push(this);
+  }
+
+  bite(animal) {
+    if (animal instanceof Herbivore) {
+      if (animal.hidden === false) {
+        animal.health -= 50;
+
+        if (animal.health === 0) {
+          Animal.alive = Animal.alive
+            .filter(a => a.health > 0);
+        }
+      }
+    }
+  }
 }
 
 module.exports = {
