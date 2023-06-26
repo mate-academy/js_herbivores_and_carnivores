5f70f19bc0c8eb2ddcf4ae2d66ebac6a201186d2
Ihor Oksentiuk <winbka.lem@gmail.com>
2023-06-26 18:53:48 +0300

    Finished task

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,40 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health, hidden = false) {
+    super(name, health);
+
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(target) {
+    if (target instanceof Herbivore && !target.hidden) {
+      target.health -= 50;
+    }
+
+    if (target.health === 0) {
+      const index = Animal.alive.indexOf(target);
+
+      Animal.alive.splice(index, 1);
+    }
+  }
 }
 
 module.exports = {
