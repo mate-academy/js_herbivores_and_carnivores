5977eb69a043fb91895867eacea0a75cd5052573
Alexey Taranenko <alloha.t.99@gmail.com>
2023-06-26 19:48:07 +0300

    chore/TheHunt

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,43 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    if (!Animal.alive) {
+      Animal.alive = [];
+    }
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.name = name;
+  }
+
+  bite(target) {
+    if (!(target instanceof Carnivore) && target.hidden !== true) {
+      target.health -= 50;
+    }
+
+    if (target.health === 0) {
+      Animal.alive = Animal.alive.filter((element) => element.health > 0);
+    }
+  }
 }
 
 module.exports = {
