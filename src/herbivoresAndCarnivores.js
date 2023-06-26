2380ae4699380e9caca9873116a11ebdaa251efd
Nazar Baraban <barabannazar44@gmail.com>
2023-06-26 19:13:18 +0300

    add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,54 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
+
+  die() {
+    const position = Animal.alive.indexOf(this);
+
+    if (position !== -1) {
+      Animal.alive.splice(position, 1);
+    }
+  }
+
+  decreaseHp(amount) {
+    this.health -= amount;
+
+    if (this.health <= 0) {
+      this.die();
+    }
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+  }
+
+  bite(beast) {
+    if (beast instanceof Carnivore || beast.hidden) {
+      return;
+    }
+
+    beast.decreaseHp(50);
+  }
 }
 
 module.exports = {
