a66c7e22d03bc106520cabdc44e44980ce4921bd
Anatolii Lykhohodenko <anatollykh10@gmail.com>
2023-06-27 20:51:34 +0300

    fix

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,17 +1,13 @@
 'use strict';
 class Animal {
-  static alive(animals) {
-    const alive = animals.filter(animal => animal.health > 0);
-
-    return alive;
-  }
-
   constructor(name, health = 100) {
     this.name = name;
     this.health = health;
     Animal.alive.push(this);
   }
 }
+
+Animal.alive = [];
 
 class Herbivore extends Animal {
   constructor(name, health = 100) {
@@ -25,18 +21,16 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
-  bite(bited) {
-    if (!bited.hidden && bited.hasOwnProperty('hidden')) {
-      bited.health -= 50;
+  bite(target) {
+    if (!target.hidden && target.hasOwnProperty('hidden')) {
+      target.health -= 50;
     }
 
-    if (bited.health === 0) {
-      Animal.alive = Animal.alive.filter(animal => animal !== bited);
+    if (target.health <= 0) {
+      Animal.alive = Animal.alive.filter(animal => animal !== target);
     }
   }
 }
-
-Animal.alive = [];
 
 module.exports = {
   Animal,
