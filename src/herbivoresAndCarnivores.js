3aa496f953327a911f18c681e42974ba9df3727d
Valeriy Lopatko <vlopatko1999@gmail.com>
2023-06-28 21:41:00 +0300

    fixing by comments

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -6,28 +6,33 @@ class Animal {
     this.name = name;
     this.health = health;
     Animal.alive.push(this);
-  }
-}
+  };
+};
+
 Animal.alive = [];
 
 class Herbivore extends Animal {
   constructor(name, health = 100, hidden = false) {
     super(name, health);
     this.hidden = hidden;
-  }
+  };
+
   hide() {
     this.hidden = true;
-  }
-}
+  };
+};
 
 class Carnivore extends Animal {
   bite(animal) {
-    if (animal.hidden === false) {
+    if (animal instanceof Herbivore && !animal.hidden) {
       animal.health -= 50;
     };
-    Animal.alive = Animal.alive.filter(el => el.health > 0);
+
+    if (animal.health < 1) {
+      Animal.alive = Animal.alive.filter(el => el.health > 0);
+    };
   }
-}
+};
 
 module.exports = {
   Animal,
