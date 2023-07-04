47948c366cbc7de02e212bbdc1dceabcb71e57d1
Yuliia Dub <yuliia.kasaraba@gmail.com>
2023-07-04 16:42:11 +0300

    add alive animal

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,36 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
+
+  static updateAliveAnimals() {
+    this.alive = this.alive.filter(animal => animal.health > 0);
+  };
 }
+
+Animal.alive = [];
 
 class Herbivore extends Animal {
-  // write your code here
-}
+  constructor() {
+    super();
+    this.hidden = false;
+  }
+  hide() {
+    this.hidden = true;
+  }
+};
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(someone) {
+    if (someone instanceof Herbivore && !someone.hidden) {
+      someone.health -= 50;
+    }
+    Animal.updateAliveAnimals();
+  }
 }
 
 module.exports = {
