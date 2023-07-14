e08e8a66feed227ad3069d78ca7c3db2eea01276
Alina <kozlovska.a.mail@gmail.com>
2023-07-14 11:23:41 +0200

    improve bite method logic

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -30,10 +30,7 @@ class Carnivore extends Animal {
   bite(animal) {
     if (animal instanceof Herbivore && !animal.hidden) {
       animal.health -= 50;
-
-      if (animal.health <= 0) {
-        Animal.alive.splice(Animal.alive.indexOf(animal), 1);
-      }
+      Animal.alive = Animal.alive.filter(a => a.health > 0);
     }
   }
 }
