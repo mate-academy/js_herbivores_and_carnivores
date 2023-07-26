eea3734494da83bb70e4a8cc0adea5af78622796
Andrey Bondar <andrey_bondar@outlook.com>
2023-07-26 17:23:17 +0300

    updated code

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -30,27 +30,18 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(target) {
-    const findTarget = Animal.alive.find(animal => animal.name === target.name);
-    const targetIndex = Animal.alive.indexOf(target);
-
     if (target instanceof Carnivore) {
       return;
     };
 
     if (target instanceof Herbivore && target.health === 0) {
       return;
-    }
+    };
 
     if (target instanceof Herbivore && target.hidden !== true) {
       target.health -= 50;
-      findTarget.health -= 50;
-
-      if (findTarget.health === 0) {
-        Animal.alive.splice(targetIndex, 1);
-      }
-    } else {
-
-    }
+      Animal.alive = Animal.alive.filter(animal => animal.health > 0);
+    };
   };
 }
 
