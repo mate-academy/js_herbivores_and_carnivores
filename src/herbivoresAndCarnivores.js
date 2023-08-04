c3e6bda06047d33b35a77effd7d3ffb390ec8041
Nataliia Nikolaienko <tasinka.nn@gmail.com>
2023-08-04 19:34:42 +0200

    herbivores And Carnivores

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,41 @@
 'use strict';
 
 class Animal {
-  // write your code here
-}
+  constructor(name, health) {
+    this.name = name;
+    this.health = health || 100;
 
+    Animal.alive.push(this);
+  }
+  die() {
+    if (this.health === 0) {
+      const index = Animal.alive.indexOf(this);
+
+      if (index !== -1) {
+        Animal.alive.splice(index, 1);
+      }
+    }
+  }
+}
+Animal.alive = [];
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(target) {
+    if (target instanceof Herbivore && !target.hidden) {
+      target.health -= 50;
+    }
+    target.die();
+  }
 }
 
 module.exports = {
