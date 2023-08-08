30986267f75c581ef71b76e6af1c6a1dd9a10931
Hania Nassalska <hanna.nassalska@gmail.com>
2023-08-08 15:16:31 +0200

    eslint problem fixed

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,13 +1,13 @@
 'use strict';
 
 class Animal {
-  static alive = [];
   constructor(name, health = 100) {
     this.name = name;
     this.health = health;
     Animal.alive.push(this);
   }
 }
+Animal.alive = [];
 
 class Herbivore extends Animal {
   constructor(name, health = 100, hidden = false) {
