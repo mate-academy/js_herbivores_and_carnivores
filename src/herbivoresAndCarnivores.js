824b3c4cfb73a3e52ed2e9650ff17cd5178b082a
Klaudia <kklis@interia.pl>
2023-08-10 12:41:24 +0200

    Fix lint error

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,14 +1,14 @@
 'use strict';
 
 class Animal {
-  static alive = [];
-
   constructor(name, health) {
     this.name = name;
     this.health = health || 100;
     Animal.alive.push(this);
   }
 }
+
+Animal.alive = [];
 
 class Herbivore extends Animal {
   constructor(name, health, hidden) {
