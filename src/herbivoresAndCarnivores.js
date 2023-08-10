aab397403a932261b07168725ed0b3dd4c3d2391
Wave <www.dandelion.ru@gmail.com>
2023-08-10 15:11:02 +0300

    add task animals solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,45 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.name = name;
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.name = name;
+  }
+
+  bite(obj) {
+    if (!obj.hidden && obj instanceof Herbivore) {
+      obj.health -= 50;
+    }
+
+    Animal.alive.map((animal, index) => {
+      if(animal.health <= 0) {
+          Animal.alive.splice(index, 1);
+        }
+      return animal;
+    })
+  }
 }
 
 module.exports = {
