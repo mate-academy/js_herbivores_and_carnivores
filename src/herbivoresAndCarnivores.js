996ccc08f853b121c4d896315878948c564db9a4
Palina Apanovich <palina.apanovich@gmail.com>
2023-08-10 23:24:42 +0200

    add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,56 @@
 
 class Animal {
   // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
+
+  die() {
+    Animal.alive = Animal.alive.filter(animal => animal !== this);
+  }
+
+  afterBite(decreaseInHealth) {
+    this.health -= decreaseInHealth;
+
+    if (this.health <= 0) {
+      this.health = 0;
+      this.die();
+    }
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    Animal.alive.push(this);
+  }
+
+  bite(target) {
+    if (!(target instanceof Herbivore) || target.hidden) {
+      return `${this.name} can't bite ${target.name}`;
+    }
+
+    target.afterBite(50);
+
+    return `${this.name} bit ${target.name}, `
+    + `health decreased to ${target.health}`;
+  }
 }
 
 module.exports = {
