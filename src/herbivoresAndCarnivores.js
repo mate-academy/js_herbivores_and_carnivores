8a8ef9104ab048f944ff72979a2219e473c8a23d
Aleksandr <kartanovaleks@gmail.com>
2023-08-11 21:11:40 +0300

    Sort out last solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -23,12 +23,10 @@ class Carnivore extends Animal {
   bite(target) {
     if (target instanceof Herbivore && !target.hidden) {
       target.health -= 50;
-    }
 
-    if (target.health <= 0) {
-      const index = Animal.alive.indexOf(target);
-
-      Animal.alive.splice(index, 1);
+      if (target.health <= 0) {
+        Animal.alive = Animal.alive.filter((animal) => animal !== target);
+      }
     }
   }
 }
