f7b83f2cdd118792ad67de3a4ab4bf85107c8893
davinnchii <zaikoivan99@gmail.com>
2023-08-15 18:47:36 +0200

    Add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,43 @@
 
 class Animal {
   // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+  }
 }
+
+const animals = Animal.alive;
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(name, health, hidden = false) {
+    super(name, health);
+    this.hidden = hidden;
+    animals.push(this);
+  }
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  constructor(name, health) {
+    super(name, health);
+    animals.push(this);
+  }
+  bite(target) {
+    if (!(target instanceof Carnivore) && target.hidden === false) {
+      target.health -= 50;
+    }
+
+    if (target.health === 0) {
+      animals.splice(animals.indexOf(target), 1);
+    }
+  }
 }
 
 module.exports = {
