b98e65465df5e4872eceb96bafec61e12caefe1f
Ivan Khutorovyi <ivanhutorovij@gmail.com>
2023-08-16 18:53:06 +0300

    add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,39 @@
 'use strict';
 
+const DEFOLT_HEALTH = 100;
+const DAMAGE_OF_BIT = 50;
+
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = DEFOLT_HEALTH;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(victim) {
+    if (!victim.hidden && victim.hasOwnProperty('hidden')) {
+      victim.health -= DAMAGE_OF_BIT;
+    }
+
+    if (victim.health === 0) {
+      Animal.alive = Animal.alive.filter(pet => pet.health !== 0);
+    }
+  }
 }
 
 module.exports = {
