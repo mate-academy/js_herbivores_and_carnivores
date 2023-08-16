1395656799d0e0590db8958cd47f3fec757bb30e
Paweł Przybylak <pawel.przybylak21@gmail.com>
2023-08-16 13:22:50 +0200

    another changes applied

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -8,24 +8,6 @@ class Animal {
     Animal.alive.push(this);
   }
 
-  hide() {
-    this.hidden = true;
-  }
-
-  bite(target) {
-    if (target instanceof Herbivore && !target.hidden) {
-      target.health -= 50;
-
-      Animal.alive = Animal.alive.filter((animal) => {
-        if (animal === target) {
-          return animal.health > 0;
-        }
-
-        return true;
-      });
-    }
-  }
-
   die() {
     if (this.health <= 0) {
       Animal.alive = Animal.alive.filter((animal) => animal !== this);
@@ -41,12 +23,30 @@ class Herbivore extends Animal {
     super(name);
     this.hidden = false;
   }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
   constructor(name, health = 100) {
     super(name, health);
+  }
+
+  bite(target) {
+    if (target instanceof Herbivore && !target.hidden) {
+      target.health -= 50;
+
+      Animal.alive = Animal.alive.filter((animal) => {
+        if (animal === target) {
+          return animal.health > 0;
+        }
+
+        return true;
+      });
+    }
   }
 }
 
