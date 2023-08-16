c8f0bb34bfb28dd3045be6e8cf9c0f392fca37c4
davinnchii <zaikoivan99@gmail.com>
2023-08-16 11:11:03 +0200

    Refactored code

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -4,37 +4,30 @@ class Animal {
   constructor(name, health = 100) {
     this.name = name;
     this.health = health;
+    Animal.alive.push(this);
   }
 }
 Animal.alive = [];
-
-const animals = Animal.alive;
 
 class Herbivore extends Animal {
   constructor(name, health, hidden = false) {
     super(name, health);
     this.hidden = hidden;
-    animals.push(this);
   }
+
   hide() {
     this.hidden = true;
   }
 }
 
 class Carnivore extends Animal {
-  constructor(name, health) {
-    super(name, health);
-    animals.push(this);
-  }
   bite(target) {
     if (!(target instanceof Carnivore) && target.hidden === false) {
       target.health -= 50;
     }
 
     if (target.health === 0) {
-      const index = animals.indexOf(target);
-
-      animals.splice(index, 1);
+      Animal.alive = Animal.alive.filter(value => value.health !== 0);
     }
   }
 }
