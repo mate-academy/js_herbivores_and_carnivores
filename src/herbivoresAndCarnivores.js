bb6ee2fa795e59eed46ab0ab581b46dbb02525d9
Anton Tsurkan <wesses1999@gmail.com>
2023-08-16 11:59:15 +0300

    fix

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -23,18 +23,11 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
-  bite(herbivore) {
-    if (!herbivore.hidden && !(herbivore instanceof Carnivore)) {
-      herbivore.health -= 50;
+  bite(animal) {
+    if (animal instanceof Herbivore && !animal.hidden) {
+      animal.health -= 50;
     }
-
-    if (herbivore.health <= 0) {
-      const index = Animal.alive.findIndex(animal => animal === herbivore);
-
-      if (index !== -1) {
-        Animal.alive.splice(index, 1);
-      }
-    }
+    Animal.alive = Animal.alive.filter(el => el.health > 0);
   }
 }
 
