d12b8eac038775bdfbafb80436c27c6c37661f91
Viktoriia Roik <viktoriaroik2103@gmail.com>
2023-08-17 21:19:03 +0300

    add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,45 @@
 'use strict';
 
+const MAX_HEALTH = 100;
+const HEALTH_DAMAGE = 50;
+const DEAD = 0;
+
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = MAX_HEALTH;
+
+    Animal.alive.push(this);
+  }
+
+  die() {
+    Animal.alive = Animal.alive.filter(animal => animal.health > DEAD);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(victim) {
+    if (!victim.hidden && victim instanceof Herbivore) {
+      victim.health -= HEALTH_DAMAGE;
+    }
+
+    if (victim.health <= DEAD) {
+      victim.die();
+    }
+  }
 }
 
 module.exports = {
