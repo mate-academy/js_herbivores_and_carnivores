cacdf7d81110e92bc605106bc7993280b817f751
Denys Poltorak <itwfrontend@gmail.com>
2023-08-17 16:33:58 +0300

    feat: add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,50 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+  }
 }
 
+// ? why i cannot use static in a class
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    Animal.alive.push(this);
+  }
+
+  bite(herbivore) {
+    const isCarnivore = herbivore instanceof Carnivore;
+    const isHidden = herbivore.hidden || false;
+
+    if (isCarnivore || isHidden) {
+      return;
+    }
+
+    herbivore.health -= 50;
+
+    if (herbivore.health > 0) {
+      return;
+    }
+
+    Animal.alive = Animal.alive
+      .filter((animal) => animal.name !== herbivore.name);
+  };
 }
 
 module.exports = {
