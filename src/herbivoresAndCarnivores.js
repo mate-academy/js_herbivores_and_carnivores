e464a705a74085a30bd4eec34f6b1c19401644ca
maksym-mishchanchuk <mishchanchuk.maksym@gmail.com>
2023-08-17 23:31:08 +0300

    initial commit

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,45 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push((this));
+  };
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  };
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  };
+  bite(animal) {
+    const isHerbivore = animal instanceof Herbivore;
+
+    if (animal.hidden === false
+      && isHerbivore === true) {
+      animal.health -= 50;
+    }
+
+    if (animal.health > 0) {
+      return;
+    }
+
+    Animal.alive = Animal.alive
+      .filter((oneAnimal) => oneAnimal.health > 0);
+  }
 }
 
 module.exports = {
