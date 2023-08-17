fc4b6cb1426af75850a792a3e5d2aca0097a19f0
AndrMar1939 <advo737@gmail.com>
2023-08-17 15:53:39 +0300

    the best solution, maybe

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,50 @@
 'use strict';
 
+const aliveAnimalsList = [];
+const HEALTH = 100;
+const BITE_POWER = 50;
+
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = HEALTH;
+    aliveAnimalsList.push(this);
+  }
+
+  static get alive() {
+    return aliveAnimalsList.filter(animal => animal.health > 0);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+
+    return this;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.bitePower = BITE_POWER;
+  }
+
+  bite(victim) {
+    if (victim.hidden || victim instanceof Carnivore) {
+      return this;
+    }
+
+    victim.health -= this.bitePower;
+
+    return this;
+  }
 }
 
 module.exports = {
