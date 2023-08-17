4ef8363e03a67417c82db1387375dcc27bc62d78
Kava Bohdan <bogdan.kava.99@gmail.com>
2023-08-17 19:54:51 +0300

    add task colution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,41 @@
 'use strict';
 
+const DEFAULT_HEALTH_VALUE = 100;
+const BITE_DAMAGE_VALUE = 50;
+
 class Animal {
-  // write your code here
+  constructor(name, health = DEFAULT_HEALTH_VALUE) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(victim) {
+    if (victim.hidden === true || !(victim instanceof Herbivore)) {
+      return;
+    }
+
+    victim.health -= BITE_DAMAGE_VALUE;
+
+    if (victim.health <= 0) {
+      Animal.alive = Animal.alive.filter(animal => animal.health > 0);
+    }
+  }
 }
 
 module.exports = {
