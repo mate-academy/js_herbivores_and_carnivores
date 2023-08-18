c77860d642daed665d38ce2bf1387c5db1e7b894
Nazar Votkanych <nazarvotkanych@gmail.com>
2023-08-18 11:18:20 +0300

    add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -4,27 +4,23 @@ class Animal {
   constructor(name, health = 100) {
     this.name = name;
     this.health = health;
-    this.hidden = false;
     Animal.alive.push(this);
+  }
+}
+Animal.alive = [];
+
+class Herbivore extends Animal {
+  constructor(name, health, hidden = false) {
+    super(name, health);
+    this.hidden = hidden;
   }
 
   hide() {
     this.hidden = true;
   }
 }
-Animal.alive = [];
-
-class Herbivore extends Animal {
-  constructor(name, health) {
-    super(name);
-  }
-}
 
 class Carnivore extends Animal {
-  constructor(name, health) {
-    super(name);
-  }
-
   bite(target) {
     if (target instanceof Herbivore && !target.hidden) {
       target.health -= 50;
