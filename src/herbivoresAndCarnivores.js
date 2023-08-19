a94608241ce8aeede18d0f2044136faf8097e615
Roman Yatsykovych <55942779+yar0man@users.noreply.github.com>
2023-08-19 17:32:37 +0300

    add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,41 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.health = 100;
+    this.name = name;
+
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(health, name, hidden = false) {
+    super(health, name, hidden);
+  }
+
+  bite(animal) {
+    if (!(animal instanceof Carnivore) && animal.hidden !== true) {
+      animal.health -= 50;
+    }
+
+    if (animal.health <= 0) {
+      Animal.alive = Animal.alive.filter(element => element.health > 0);
+    }
+  }
 }
 
 module.exports = {
