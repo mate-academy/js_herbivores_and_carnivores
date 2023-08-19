a24fe8ef4fe087ff030cd93f53a639170b35d33a
Rostyslav Kravchuk <mr.shadov.05@gmail.com>
2023-08-19 22:04:49 +0300

    creade classes for animals

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,45 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  static START_HEALTH = 100;
+
+  health = Animal.START_HEALTH;
+
+  constructor(name) {
+    this.name = name;
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  };
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  static BITE_DAMAGE = 50;
+
+  bite(animalToBite) {
+    if (animalToBite instanceof Carnivore || animalToBite.hidden) {
+      return;
+    }
+
+    animalToBite.health -= Carnivore.BITE_DAMAGE;
+
+    if (animalToBite.health <= 0) {
+      const animalToDeleteIndex = Animal.alive.indexOf(animalToBite);
+
+      Animal.alive.splice(animalToDeleteIndex, 1);
+    }
+  }
 }
 
 module.exports = {
