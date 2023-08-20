0879d597c30e0dfb44d867f080762d3753426359
Vitalii Serhiichuk <vitalya.sergiychuk19999@gmail.com>
2023-08-21 01:46:00 +0300

    add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,47 @@
 'use strict';
 
+const MAX_HEALTH = 100;
+const DEATH_HEALTH = 0;
+const DAMAGE_BITE = 50;
+
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = MAX_HEALTH;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super();
+    this.name = name;
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super();
+    this.name = name;
+  }
+
+  bite(animal) {
+    if (animal instanceof Herbivore && !animal.hidden) {
+      animal.health -= DAMAGE_BITE;
+    }
+
+    if (animal) {
+      Animal.alive = Animal.alive
+        .filter(animals => animals.health !== DEATH_HEALTH);
+    }
+  }
 }
 
 module.exports = {
