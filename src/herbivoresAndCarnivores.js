b888ba7abc920469140fbd0372d30f0df4fd635c
KlymSudakov <sudakov.klym@gmail.com>
2023-08-22 01:43:13 +0300

    add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,44 @@
 'use strict';
 
+const HEALTH_FULL = 100;
+const HEALTH_AFTER_BITE = 50;
+const HEALTH_OF_DEAD_ANIMAL = 0;
+
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = HEALTH_FULL;
+    Animal.alive.push(this);
+  }
+
+  die() {
+    Animal.alive = Animal.alive.filter((animal) => animal !== this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(target) {
+    if (target.hidden === false) {
+      target.health -= HEALTH_AFTER_BITE;
+    }
+
+    if (target.health <= HEALTH_OF_DEAD_ANIMAL) {
+      target.die();
+    }
+  }
 }
 
 module.exports = {
