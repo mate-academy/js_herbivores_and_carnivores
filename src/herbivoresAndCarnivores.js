e0d3a14d6264bd7e4976a073176cdc87c2ea6bd5
Oleksandr Shevchenko <aleksandrtarasovi44@gmail.com>
2023-08-21 13:22:03 +0300

    add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,41 @@
 'use strict';
 
+const ANIMAL_START_HEALTH = 100;
+const CARNIVORE_DAMAGE = 50;
+const DEFAULT_NAME = 'Unknown  animal';
+
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name || DEFAULT_NAME;
+    this.health = ANIMAL_START_HEALTH;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hidden = false;
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(beast) {
+    if (beast instanceof Carnivore || beast.hidden) {
+      return null;
+    }
+
+    beast.health -= CARNIVORE_DAMAGE;
+
+    if (beast.health <= 0) {
+      const index = Animal.alive.indexOf(beast);
+
+      Animal.alive.splice(index, 1);
+    }
+  }
 }
 
 module.exports = {
