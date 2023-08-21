1e422364474dca7fbc80efc7d1d94ccf9d35043e
Yevheniia Sidanchenko <sidanchenkoe@gmail.com>
2023-08-21 15:46:18 +0300

    fix

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -32,9 +32,7 @@ class Carnivore extends Animal {
       animal.health -= BITE_STRENGTH;
 
       if (animal.health <= 0) {
-        const index = Animal.alive.indexOf(animal);
-
-        Animal.alive.splice(index, 1);
+        Animal.alive = Animal.alive.filter(({ health }) => health > 0);
       }
     }
   }
