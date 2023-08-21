9dbc033102fa564c521e43e8a9e6a00129ef68f3
valet2102 <valikyanyk@gmail.com>
2023-08-21 15:28:51 +0300

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,43 @@
 'use strict';
 
+const ANIMAL_START_HEALTH = 100;
+const CARNIVORE_DAMAGE = 50;
+const DEFAULT_NAME = 'Unknown  animal';
+
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name || DEFAULT_NAME;
+    this.health = ANIMAL_START_HEALTH;
+
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (animal instanceof Carnivore || animal.hidden) {
+      return;
+    }
+
+    animal.health -= CARNIVORE_DAMAGE;
+
+    if (animal.health === 0) {
+      Animal.alive = Animal.alive.filter(({ name }) => name !== animal.name);
+    }
+  }
 }
 
 module.exports = {
