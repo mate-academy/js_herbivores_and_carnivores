84a5356d3893baabd5e0f4a4afc4beccd5b534b1
Yevhenii Voronin <voronine@ukr.net>
2023-08-21 13:56:20 +0300

    Solution instanceOf & Magic value

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,18 +1,23 @@
 'use strict';
 
+const FULL_HEALTH = 100;
+const BITE_HEALTH = 50;
+
 class Animal {
-  constructor(name, health = 100) {
+  constructor(name, health = FULL_HEALTH) {
     this.name = name;
     this.health = health;
 
     Animal.alive.push(this);
   }
+
   die() {
     Animal.alive = Animal.alive.filter(animal => animal.health > 0);
   }
 }
 
 Animal.alive = [];
+
 class Herbivore extends Animal {
   constructor(name) {
     super(name);
@@ -26,8 +31,8 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(animal) {
-    if (!animal.hidden && Herbivore.prototype.isPrototypeOf(animal)) {
-      animal.health -= 50;
+    if (!animal.hidden && animal instanceof Herbivore) {
+      animal.health -= BITE_HEALTH;
     }
 
     if (animal.health <= 0) {
