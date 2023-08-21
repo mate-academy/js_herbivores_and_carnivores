fe611e3bcdd8c45d0fb5e558f9c297d371136fd9
Andrii Nakonechnyi <andreynakonechnuy11@mail.com>
2023-08-21 20:04:01 +0300

    js_herbivores_and_carnivores-2

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,26 +1,25 @@
 'use strict';
 
+const FULL_HEALTH = 100;
+const BITE_DAMAGE = 50;
+
 class Animal {
   static alive = [];
 
-  constructor(name, health = 100) {
+  constructor(name, health = FULL_HEALTH) {
     this.name = name;
     this.health = health;
     Animal.alive.push(this);
   }
 
   die() {
-    const index = Animal.alive.indexOf(this);
-
-    if (index !== -1) {
-      Animal.alive.splice(index, 1);
-    }
+    Animal.alive = Animal.alive.filter(animal => animal !== this);
   }
 }
 
 class Herbivore extends Animal {
-  constructor(name, health = 100) {
-    super(name, health);
+  constructor(name) {
+    super(name);
     this.hidden = false;
   }
 
@@ -30,17 +29,19 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
-  constructor(name, health = 100) {
+  constructor(name, health = FULL_HEALTH) {
     super(name, health);
   }
 
   bite(target) {
-    if (target instanceof Herbivore && !target.hidden) {
-      target.health -= 50;
+    if (!(target instanceof Herbivore) || target.hidden) {
+      return;
+    }
 
-      if (target.health <= 0) {
-        target.die();
-      }
+    target.health -= BITE_DAMAGE;
+
+    if (target.health <= 0) {
+      target.die();
     }
   }
 }
