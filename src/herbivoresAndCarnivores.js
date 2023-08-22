851e412514a1e3c24c7b199b30e36161040f0316
Vitalii-Petruk <vitalik3807083@gmail.com>
2023-08-22 13:33:20 +0300

    delete magic value

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,13 +1,16 @@
 'use strict';
 
-class Animal {
-   static alive = [];
+const FULL_HEALTH = 100;
+const BITING_PRICE = 50;
 
-   constructor(name, health = 100) {
-     this.name = name;
-     this.health = health;
-     Animal.alive.push(this);
-   };
+class Animal {
+  static alive = [];
+
+  constructor(name, health = FULL_HEALTH) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
@@ -22,15 +25,13 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
-  bite(herbivoreVictim) {
-    if (herbivoreVictim instanceof Herbivore && !herbivoreVictim.hidden) {
-      herbivoreVictim.health -= 50;
+  bite(victim) {
+    if (victim instanceof Herbivore && !victim.hidden) {
+      victim.health -= BITING_PRICE;
     }
 
-    if (herbivoreVictim.health <= 0) {
-      Animal.alive = Animal.alive.filter(
-        (animal) => animal !== herbivoreVictim
-      );
+    if (victim.health <= 0) {
+      Animal.alive = Animal.alive.filter((animal) => animal !== victim);
     }
   }
 }
