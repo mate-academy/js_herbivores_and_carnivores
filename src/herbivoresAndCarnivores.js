7af6f4a770e1f9a9542d6579f5b8f61ecb1f5ca4
Oleg <gtheplayer2@gmail.com>
2023-08-23 15:45:05 +0300

    Fix problems

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -4,7 +4,6 @@ class Animal {
   constructor(name, health = 100) {
     this.name = name;
     this.health = health;
-    this.hidden = false;
 
     if (!Animal.alive) {
       Animal.alive = [];
@@ -16,12 +15,23 @@ class Animal {
     Animal.alive = Animal.alive.filter(animal => animal.health > 0);
   }
 }
+
 class Herbivore extends Animal {
+  constructor(name, health = 100) {
+    super(name, health);
+    this.hidden = false;
+  }
+
   hide() {
     this.hidden = true;
   }
 }
+
 class Carnivore extends Animal {
+  constructor(name, health = 100) {
+    super(name, health);
+  }
+
   bite(target) {
     if (target instanceof Herbivore && !target.hidden) {
       target.health -= 50;
