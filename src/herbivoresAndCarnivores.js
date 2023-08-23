b83f7c03770e91c03784fdba991cfa31cb99b77d
dima <dima98doroshenko@gmail.com>
2023-08-23 10:28:54 +0300

    herbivoresAndCarnivores

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,42 @@
 'use strict';
 
+const FULL_HEALTH = 100;
+const DAMAGE = 50;
+const DIED = 0;
+
 class Animal {
-  // write your code here
+  constructor(name, health = FULL_HEALTH) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(prey) {
+    if (prey instanceof Herbivore && !prey.hidden) {
+      prey.health -= DAMAGE;
+    }
+
+    if (prey.health === DIED) {
+      const diedAnimal = Animal.alive.indexOf(prey);
+
+      Animal.alive.splice(diedAnimal, 1);
+    }
+  }
 }
 
 module.exports = {
