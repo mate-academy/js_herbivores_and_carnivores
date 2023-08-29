c55056e7a378ad4ce732abbb4c746cc1115eef92
FurArt <iiionerart@gmail.com>
2023-08-29 23:22:45 +0300

    added solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,43 @@
 'use strict';
 
 class Animal {
-  // write your code here
-}
+  static removeDeadAnimals(animal) {
+    Animal.alive = Animal.alive.filter((deadAnimal) => {
+      return deadAnimal.name !== animal.name;
+    });
+  }
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
 
+    Animal.alive.push(this);
+  }
+
+  thisClassName() {
+    return this.constructor.name;
+  }
+}
+Animal.alive = [];
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (!animal.hidden && !(animal.thisClassName() === 'Carnivore')) {
+      animal.health -= 50;
+    }
+
+    if (animal.health <= 0) {
+      Animal.removeDeadAnimals(animal);
+    }
+  }
 }
 
 module.exports = {
