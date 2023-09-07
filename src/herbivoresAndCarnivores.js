0085169066486761d508ef3cf8a33a55099ccdf8
Krzysztof Janiewicz <spojrzenie@gmail.com>
2023-09-07 23:21:10 +0200

    Add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,36 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.health = 100;
+    this.name = name;
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herb) {
+    if (herb instanceof Herbivore) {
+      herb.health = herb.hidden === false ? herb.health - 50 : herb.health;
+    }
+
+    if (herb.health <= 0) {
+      const index = Animal.alive.indexOf(herb);
+
+      if (index !== -1) {
+        Animal.alive.splice(index, 1);
+      }
+    }
+  }
 }
 
 module.exports = {
