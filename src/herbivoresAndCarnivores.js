29245994af5767ebd4dbcde462cddeba919f45c9
Astra09 <sichkarstepan09@gmail.com>
2023-09-14 02:05:57 +0300

    class developed

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,50 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.health = 100;
+    this.name = name;
+    Animal.addAlive(this);
+  }
+
+  static addAlive(object) {
+    Animal.alive.push(object);
+  }
+
+  static removeAlive(object) {
+    Animal.alive.splice(Animal.alive.indexOf(object), 1);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor() {
+    super();
+    this.hidden = false;
+  }
+
+  hide() {
+    if (this.hidden) {
+      this.hidden = false;
+
+      return;
+    }
+
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herbivore) {
+    if (herbivore instanceof Herbivore && herbivore.hidden === false) {
+      herbivore.health -= 50;
+
+      if (herbivore.health <= 0) {
+        Animal.removeAlive(herbivore);
+      }
+    }
+  }
 }
 
 module.exports = {
