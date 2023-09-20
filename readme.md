a030458bf5356503c86e58f317231552e5c258b3
Lubomur Chuchvara <lubomircucvara@gmail.com>
2023-09-20 15:54:14 +0300

    solution

--- readme.md
+++ readme.md
@@ -48,4 +48,4 @@ Animal.alive === [
 ];
 ```
 
-**Read [the guideline](https://github.com/mate-academy/js_task-guideline/blob/master/README.md) before start**
+**Read [the guideline](https://github.com/Liubomyr19/js_task-guideline/blob/master/README.md) before start**
--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,36 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
+
+  static removeDeadAnimals() {
+    Animal.alive = Animal.alive.filter(animal => animal.health > 0);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(target) {
+    if (target instanceof Herbivore && !target.hidden) {
+      target.health -= 50;
+
+      if (target.health <= 0) {
+        Animal.removeDeadAnimals();
+      }
+    }
+  }
 }
 
 module.exports = {
