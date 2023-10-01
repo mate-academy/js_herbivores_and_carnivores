3a6a0d9ee7aa4d24169c144a7514e20417f95f1e
Serhii Mykhailivskyi <sergeymih098123@gmail.com>
2023-10-01 16:09:47 +0300

    add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,45 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.health = 100;
+    this.name = name;
+
+    // Linter is not allowing us to use static =(
+
+    if (!Animal.alive) {
+      Animal.alive = [];
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    Animal.alive.push(this);
+  }
+
+  bite(animal) {
+    if (!(animal instanceof Carnivore) && !animal.hidden) {
+      animal.health -= 50;
+
+      if (animal.health === 0) {
+        Animal.alive = Animal.alive.filter(beast => beast.name !== animal.name);
+      }
+    }
+  }
 }
 
 module.exports = {
