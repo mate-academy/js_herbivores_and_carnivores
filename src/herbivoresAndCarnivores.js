a7ad720f8150b9e7ee4a08d87a40a2f18d7d2ac5
ElenaGrasovskaya <elenagrasovskaya@gmail.com>
2023-10-02 12:25:57 +0300

    Classes and inheritance task

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,52 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.health = 100;
+    this.name = name;
+  };
+
+  static addAnimal(animal) {
+    this.alive.push(animal);
+  }
+
+  static removeAnimal(animal) {
+    const index = this.alive.findIndex(el => el === animal);
+
+    this.alive.splice(index, 1);
+  }
 }
+Animal.alive = [];
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+    this.type = 'herbivore';
+    Animal.addAnimal(this);
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.type = 'carnivore';
+    Animal.addAnimal(this);
+  }
+
+  bite(victim) {
+    if (victim.type === 'herbivore' && victim.hidden === false) {
+      victim.health -= 50;
+    }
+
+    if (victim.health <= 0) {
+      Animal.removeAnimal(victim);
+    }
+  }
 }
 
 module.exports = {
