41bc2b5485958dd71da35913b409ee87dcfda8d4
Oleh <olezhkapopovych@gmail.com>
2023-10-02 16:47:52 +0300

    cut some lines

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -23,11 +23,7 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(animal) {
-    if (animal instanceof Carnivore) {
-      return;
-    }
-
-    if (!animal.hidden) {
+    if (!animal.hidden && animal instanceof Herbivore) {
       animal.health -= 50;
     }
 
