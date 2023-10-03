60e1b0e1a80a2bb3c198bc0486fe0e1760193788
Denys Vasylenkoe <kapodenim@gmail.com>
2023-10-03 18:58:16 +0300

    v1

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,44 @@
 
 class Animal {
   // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(name, health, hidden = false) {
+    super(name, health);
+    this.hidden = hidden;
+    this.type = 'Herbivore';
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+
+  constructor(name, health) {
+    super(name, health);
+    this.type = 'Carnivore';
+  }
+
+  bite(animal) {
+    if (animal.type === 'Herbivore') {
+      if (animal.hidden !== true && animal.health > 0) {
+        animal.health -= 50;
+        Animal.alive = Animal.alive.filter(animals => animals.health > 50);
+      }
+    }
+  }
 }
 
 module.exports = {
