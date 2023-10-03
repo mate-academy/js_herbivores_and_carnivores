95b7ea456a618dc10dba519dd4eff6db115b6172
Denys Shurubura <denys.shurubura@gmail.com>
2023-10-03 17:26:19 +0300

    task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,48 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health) {
+    this.name = name;
+    this.health = health || 100;
+    Animal.addToAlive(this);
+  }
+
+  static addToAlive(animal) {
+    Animal.alive.push(animal);
+  }
+
+  static removeAlive(animal) {
+    Animal.alive = Animal.alive.filter(aliveAnimal => aliveAnimal !== animal);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health, hidden) {
+    super(name, health);
+    this.hidden = hidden || false;
+  }
+
+  hide() {
+    if (this.hidden) {
+      this.hidden = false;
+    }
+
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(target) {
+    if (!target.hidden && target instanceof Herbivore) {
+      target.health -= 50;
+    }
+
+    if (target.health === 0) {
+      Animal.removeAlive(target);
+    }
+  }
 }
 
 module.exports = {
