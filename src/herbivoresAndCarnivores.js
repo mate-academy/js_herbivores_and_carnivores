fc6c35be2b210164a0734537df2c1c2a4e6565bf
Ivanna Zaiets <iva.zaiets@gmail.com>
2023-10-03 17:34:23 +0300

    solution herbivore and carnivores task

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,55 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(health, name) {
+    this.health = 100;
+    this.name = name;
+    Animal.addToAlive(this);
+  }
+
+  static addToAlive(animal) {
+    Animal.alive.push(animal);
+  }
+
+  static removeFromAlive(animal) {
+    Animal.alive = Animal.alive.filter(aliveAnimal => aliveAnimal !== animal);
+  }
 }
+
+Animal.alive = [];
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(health, name) {
+    super(health, name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  constructor(health, name) {
+    super(health, name);
+
+    // I should duplicate this code again,
+    // because without this code I see error like 'Useless constructor.'
+    this.health = 100;
+    this.name = name;
+  }
+
+  bite(animal) {
+    if (animal instanceof Herbivore && animal.hidden === false) {
+      animal.health -= 50;
+    }
+
+    if (animal.health === 0) {
+      Animal.removeFromAlive(animal);
+    }
+  }
 }
 
 module.exports = {
