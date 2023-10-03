16ed3e911c3b8de79c4d32966771512cd8373e80
Nazar Hnatyuk <108331415+Stim1@users.noreply.github.com>
2023-10-03 14:51:10 +0300

    add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,37 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.health = 100;
+    this.name = name;
+    Animal.alive.push(this);
+  }
+
+  removeFromAlive() {
+    Animal.alive = Animal.alive.filter(animal => animal.health !== 0);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(health, name, hidden = false) {
+    super(health, name);
+    this.hidden = hidden;
+  }
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(victim) {
+    if (victim instanceof Herbivore
+      && !victim.hidden) {
+      victim.health -= 50;
+      this.removeFromAlive();
+    }
+  }
 }
 
 module.exports = {
