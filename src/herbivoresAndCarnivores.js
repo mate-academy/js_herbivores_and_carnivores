6aa01cce4ed16cd5d4ccbf64829f4c5a2ed81393
Anastasiia Karkachova <a.karkachova0512@gmail.com>
2023-10-04 12:09:52 +0300

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -3,8 +3,7 @@
 class Animal {
   constructor(name, health) {
     this.name = name;
-    this.health = health || 100;
-    this.hidden = false;
+    this.health = 100;
     Animal.alive.push(this);
   }
 }
@@ -12,6 +11,11 @@ class Animal {
 Animal.alive = [];
 
 class Herbivore extends Animal {
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
   hide() {
     this.hidden = true;
   }
