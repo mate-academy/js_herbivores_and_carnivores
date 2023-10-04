c6b0dd296d809d8e237602aceb50640d89e9e480
Nazar Hnatyuk <108331415+Stim1@users.noreply.github.com>
2023-10-04 16:37:17 +0300

    implement all recomendations

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -4,11 +4,15 @@ class Animal {
   constructor(name) {
     this.health = 100;
     this.name = name;
-    Animal.alive.push(this);
+    this.addToAlive(this);
   }
 
-  removeFromAlive() {
-    Animal.alive = Animal.alive.filter(animal => animal.health !== 0);
+  addToAlive(animal) {
+    Animal.alive.push(animal);
+  }
+
+  removeFromAlive(animal) {
+    Animal.alive = Animal.alive.filter(aliveAnimal => aliveAnimal !== animal);
   }
 }
 
@@ -30,7 +34,7 @@ class Carnivore extends Animal {
       victim.health -= 50;
 
       if (victim.health <= 0) {
-        this.removeFromAlive();
+        this.removeFromAlive(victim);
       }
     }
   }
