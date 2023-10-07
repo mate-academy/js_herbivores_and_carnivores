0da8f00bdbc888a900d3005920a241a5e8ecfc47
Ivan Orinchak <124816409+IvanOrinchak@users.noreply.github.com>
2023-10-07 15:54:07 +0300

    js_herbivores_and_carnivores_2

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -14,31 +14,14 @@ Animal.alive = [];
 
 class Herbivore extends Animal {
   hide() {
-    Animal.alive = Animal.alive.map(element => {
-      if (element.name === this.name) {
-        element.hidden = true;
-        this.hidden = true;
-
-        return element;
-      }
-
-      return element;
-    });
+    this.hidden = true;
   }
 }
 
 class Carnivore extends Animal {
   bite(canBite) {
     if (!(canBite instanceof Carnivore) && !canBite.hidden) {
-      Animal.alive = Animal.alive.map(element => {
-        if (element.name === canBite.name) {
-          element.health = element.health - 50;
-
-          return element;
-        }
-
-        return element;
-      });
+      canBite.health -= 50;
 
       Animal.alive = Animal.alive.filter(element => element.health > 0);
     }
