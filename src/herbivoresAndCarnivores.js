648272f0d5cc5214d48a94cf63d79345fc7801b4
Viktoriia Soloviova <ivicki.vs@gmail.com>
2023-10-07 23:05:39 +0300

    problem with static property

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,51 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  // static alive = [];
+
+  constructor(health = 100, name) {
+    this.health = health;
+    this.name = name;
+    Animal.alive.push(this);
+  }
+
+  // static deleteDeadAnimal() {
+  //   Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
+  // }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(health, name);
+    this.hidden = false;
+  }
+
+  hide() {
+    if (this.hidden === false) {
+      this.hidden = true;
+    } else {
+      this.hidden = false;
+    }
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(health, name) {
+    super(name, health);
+  }
+
+  bite(victum) {
+    if (victum.hidden === false
+      && victum instanceof Herbivore) {
+      victum.health -= 50;
+    }
+
+    if (victum.health === 0) {
+      Animal.alive = Animal.alive.filter(aliveAnimal => aliveAnimal !== victum);
+    }
+  }
 }
 
 module.exports = {
