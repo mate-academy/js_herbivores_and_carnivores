6fdeda649a6ac481f83cb7cd89ced8ea40d5cb91
Nazariy Dovgyi <nazar.dovgyi@gmail.com>
2023-10-10 08:10:03 +0200

    add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,36 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.health = 100;
+    this.name = name;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(target) {
+    if (!(target instanceof Carnivore) && !target.hidden) {
+      target.health -= 50;
+
+      if (target.health < 1) {
+        Animal.alive = Animal.alive.filter(animal => animal !== target);
+      }
+    }
+  }
 }
 
 module.exports = {
