f84a99a7ffd59e35bfd4fa17d2270842424efdaf
Sireus <thesireus@gmail.com>
2023-10-16 13:15:54 +0300

    Trying to refactor

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -4,6 +4,15 @@ class Animal {
   constructor(name) {
     this.name = name;
     this.health = 100;
+    Animal.alive.push(this);
+  }
+
+  static checkHealth() {
+    const dead = Animal.alive.findIndex(beast => !beast.health);
+
+    if (dead !== -1) {
+      Animal.alive.splice(dead, 1);
+    }
   }
 }
 
@@ -13,7 +22,6 @@ class Herbivore extends Animal {
   constructor(name) {
     super(name);
     this.hidden = false;
-    Animal.alive.push(this);
   }
 
   hide() {
@@ -22,20 +30,10 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
-  constructor(name) {
-    super(name);
-    Animal.alive.push(this);
-  }
-
   bite(obj) {
     if (obj instanceof Herbivore && !obj.hidden) {
       obj.health -= 50;
-    }
-
-    const dead = Animal.alive.findIndex(beast => !beast.health);
-
-    if (dead !== -1) {
-      Animal.alive.splice(dead, 1);
+      Animal.checkHealth();
     }
   }
 }
