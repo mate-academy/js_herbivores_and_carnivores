cd34d8227573aa210bb4ae35e94f6e86bffbb9b9
Markiyan Dmyterko <marki.dmyterko@gmail.com>
2023-10-16 14:52:42 +0300

    done

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,35 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.health = 100;
+    this.name = name;
+    Animal.alive.push(this);
+  }
 }
+Animal.alive = [];
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(health, name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herb) {
+    if (herb.hidden === false) {
+      herb.health -= 50;
+    }
+
+    if(herb.health === 0) {
+      Animal.alive = Animal.alive.filter(animal => animal !== herb)
+    }
+  }
 }
 
 module.exports = {
