0a34db885533571d3a50786a7c08fa2baae58d95
newqou <137334905+newqou@users.noreply.github.com>
2023-10-19 20:47:57 +0300

    develop

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,45 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name = '') {
+    this.health = 100;
+    this.name = name;
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    Animal.alive.push(this);
+  }
+
+  bite(herbivore) {
+    if (!(herbivore instanceof Herbivore) || herbivore.hidden) {
+      return;
+    }
+
+    herbivore.health -= 50;
+
+    if (herbivore.health === 0) {
+      Animal.alive = Animal.alive.filter(animal => (
+        animal.name !== herbivore.name
+      ));
+    }
+  }
 }
 
 module.exports = {
