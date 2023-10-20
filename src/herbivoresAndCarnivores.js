023b5555dde331ac7398b0d82f2932cc0c876e50
Evhen Teneta <tenetaevgen@gmail.com>
2023-10-20 16:53:18 +0300

    edit comment from VitaliyBodnar

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -37,11 +37,11 @@ class Carnivore extends Animal {
     animal.health -= 50;
 
     if (animal.health <= 0) {
-      const index = Animal.alive.indexOf(animal);
-
-      if (index > -1) {
-        Animal.alive.splice(index, 1);
-      }
+      Animal.alive = Animal.alive.filter(item => {
+        if(item.health !== 0) {
+          return item;
+        }
+      })
     }
   }
 }
