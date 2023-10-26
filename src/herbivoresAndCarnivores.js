652b2652b7ba412625ad5c22fa14e693b29beaac
Viktor Bykh <bykhviktor21@gmail.com>
2023-10-26 18:22:41 +0300

    first version of hw js_herbivores_and_carnivores

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,19 +1,46 @@
 'use strict';
 
+const HEALTH_BY_DEFAULT = 100;
+const DAMAGE_AMOUNT = 50;
+const MINIMAL_AMOUNT_OF_HEALTH = 0;
+
 class Animal {
-  // write your code here
+  constructor(name, health = HEALTH_BY_DEFAULT, hidden = false) {
+    this.name = name;
+    this.health = health;
+    this.hidden = hidden;
+    Animal.alive.push(this);
+  }
+
+  die() {
+    Animal.alive.splice(Animal.alive.indexOf(this), 1);
+  }
+
+  getDamage(amount) {
+    this.health -= amount;
+
+    if (this.health <= MINIMAL_AMOUNT_OF_HEALTH) {
+      this.die();
+    }
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (animal.hidden === false && animal instanceof Herbivore) {
+      animal.getDamage(DAMAGE_AMOUNT);
+    }
+  }
 }
 
 module.exports = {
-  Animal,
-  Herbivore,
-  Carnivore,
+  Animal, Herbivore, Carnivore,
 };
