f88e1a4b91f571fb53a84ffa5458f27a23f8aa4e
Viktoria-Moroz <viktoriaandriivna25@gmail.com>
2023-10-28 10:16:40 +0300

    the task was completed

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,39 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health = 100, hidden = false) {
+    super(name, health);
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+  }
+
+  bite(animal) {
+    if (animal && animal.hidden === false) {
+      animal.health -= 50;
+    }
+
+    Animal.alive = Animal.alive.filter((beast) => beast.health > 0);
+  }
 }
 
 module.exports = {
