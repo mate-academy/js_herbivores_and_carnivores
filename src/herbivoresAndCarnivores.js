7baad1e30c6dfa718cc9c2546438cade1ceb92e1
Svitlana Ramanauskas <ramanauskassvs@gmail.com>
2023-11-03 17:18:55 +0200

    fixed solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -4,6 +4,7 @@ class Animal {
   constructor(name, health = 100) {
     this.name = name;
     this.health = health;
+    Animal.alive.push(this);
   }
 }
 
@@ -13,22 +14,16 @@ class Herbivore extends Animal {
   constructor(name, health, hidden = false) {
     super(name, health);
     this.hidden = hidden;
-    Animal.alive.push(this);
   }
 
   hide() {
-    if (this.hidden === false) {
+    if (!this.hidden) {
       this.hidden = true;
     }
   }
 }
 
 class Carnivore extends Animal {
-  constructor(name, health) {
-    super(name, health);
-
-    Animal.alive.push(this);
-  }
   bite(beast) {
     if (beast.hidden === false) {
       beast.health -= 50;
