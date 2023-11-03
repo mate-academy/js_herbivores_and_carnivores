a7207a69be7ec96547f3b629155ee6b2e7a516d0
Mikhailo Koshilka <mkosilka@gmail.com>
2023-11-03 18:28:12 +0200

    add fix

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -11,8 +11,8 @@ class Animal {
 Animal.alive = [];
 
 class Herbivore extends Animal {
-  constructor() {
-    super();
+  constructor(name, health) {
+    super(name, health);
     this.hidden = false;
   }
 
