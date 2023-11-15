9c8cc1f295901633f97087a4dd84441e3eee6edb
Mirosław Wandyk <barosz1111@gmail.com>
2023-11-15 15:39:45 +0100

    initial commit

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,43 @@
 
 class Animal {
   // write your code here
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
 }
+
+Animal.alive = [];
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    if (this.hidden === false) {
+      this.hidden = true;
+    } else {
+      this.hidden = false;
+    }
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  bite(enemy) {
+    if (enemy.hidden === false && enemy instanceof Herbivore) {
+      enemy.health -= 50;
+      if (enemy.health === 0) {
+        const index = Animal.alive.indexOf(enemy);
+        Animal.alive.splice(index, 1);
+      }
+    }
+  }
 }
 
 module.exports = {
