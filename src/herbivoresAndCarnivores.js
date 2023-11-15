a4b0b25043e3ef0485d081c319d75b767fe5a011
Emiliia Kaminska <milja2705@gmail.com>
2023-11-15 17:31:08 +0200

    made health value by default in constructor

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,8 +1,8 @@
 'use strict';
 
 class Animal {
-  constructor(name) {
-    this.health = 100;
+  constructor(name, health = 100) {
+    this.health = health;
     this.name = name;
     Animal.alive.push(this);
   }
