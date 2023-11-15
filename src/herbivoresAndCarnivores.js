8173a7ae6bb0128d2af47ac62c43df5a4b8d91ee
Sergey Emelyanov <mrleondono@gmail.com>
2023-11-15 23:55:11 +0200

    herbivoresAndCarnivores task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,50 @@
 'use strict';
 
+const HEALTH = 100;
+const DAMAGE_PER_BITE = 50;
+
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = HEALTH;
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    Animal.alive.push(this);
+  }
+
+  bite(target) {
+    if (
+      target.health > 0
+      && target instanceof Herbivore
+      && !target.hidden
+    ) {
+      target.health -= DAMAGE_PER_BITE;
+
+      if (target.health === 0 || target.health < 0) {
+        Animal.alive = Animal.alive.filter((currentAnimal) => (
+          currentAnimal !== target
+        ));
+      }
+    }
+  }
 }
 
 module.exports = {
