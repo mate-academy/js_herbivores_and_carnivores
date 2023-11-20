8413fece1224ad513e4fb0ccb95d57193a6b6e16
Svitlana <svitlana.ponomarchuk@gmail.com>
2023-11-20 02:27:07 +0200

    added herbivore and carnivores

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,47 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100, hidden = false) {
+    this.name = name;
+    this.health = health;
+    this.hidden = hidden;
+
+    if (!Animal.alive) {
+      Animal.alive = [];
+    }
+
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    this.hidden = true;
+  }
+
+  die() {
+    if (Animal.alive) {
+      const index = Animal.alive.indexOf(this);
+
+      if (index !== -1) {
+        Animal.alive.splice(index, 1);
+      }
+    }
+  }
 }
 
-class Herbivore extends Animal {
-  // write your code here
-}
+class Herbivore extends Animal { }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herbivore) {
+    if (herbivore.constructor.name === 'Herbivore') {
+      if (!herbivore.hidden) {
+        herbivore.health -= 50;
+      }
+
+      if (herbivore.health === 0) {
+        herbivore.die();
+      }
+    }
+  }
 }
 
 module.exports = {
