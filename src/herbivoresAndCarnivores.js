3fb98989b377f65c88d8e034444e7937227dff10
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2023-11-23 00:14:41 +0100

    add task soulution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,40 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  };
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(name) {
+    if (name instanceof Herbivore) {
+      if (name.hidden === false) {
+        name.health -= 50;
+      }
+    }
+
+    if (name.health <= 0) {
+      const tmp = Animal.alive.indexOf(name);
+
+      Animal.alive.splice(tmp, 1);
+    }
+  }
 }
 
 module.exports = {
