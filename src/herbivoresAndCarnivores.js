18ab8f292cf07f396cfdca1e9bdb820c3db1f45c
Roman Tarasov <rtarasov1k90@gmail.com>
2023-11-23 11:20:03 +0200

    Filtered

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -28,7 +28,7 @@ class Carnivore extends Animal {
     }
 
     if (target.health <= 0) {
-      Animal.alive = Animal.alive.filter((animal) => animal.health <= 0);
+      Animal.alive = Animal.alive.filter((animal) => animal.health);
     }
   }
 }
