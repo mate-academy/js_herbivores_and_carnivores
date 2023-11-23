5149e266295c2be809e2f6bf3bf8ba264ba5a9c3
Roman Tarasov <rtarasov1k90@gmail.com>
2023-11-23 09:27:43 +0200

    Herbivores and Сarnivores

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,40 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health, hidden = false) {
+    super(name, health);
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super();
+  }
+
+  bite(target) {
+    if (target.hidden === false) {
+      target.health = target.health - 50;
+    }
+
+    if (target.health === 0) {
+      Animal.alive.splice(Animal.alive.indexOf(target), 1)
+    }
+  }
 }
 
 module.exports = {
