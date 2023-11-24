9b4308c07a0ecc9556c715e08cdde2c976b22873
Ivan Vashchuk <vaniavaschuk@gmail.com>
2023-11-24 10:26:24 +0800

    Nature

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,38 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health, hidden = false) {
+    super(name, health);
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(victim) {
+    if (victim instanceof Herbivore && !victim.hidden) {
+      victim.health -= 50;
+    }
+
+    if (victim.health <= 0) {
+      const indexOfVictim = Animal.alive.indexOf(victim);
+
+      Animal.alive.splice(indexOfVictim, 1);
+    }
+  }
 }
 
 module.exports = {
