66233bc1f213271177a7d541e81b647302abb081
NataSerg <na.s@ukr.net>
2023-11-24 17:11:30 +0200

    add animal classes

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,47 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  health = 100;
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hidden = false;
+
+  constructor(name) {
+    super(name);
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    if (this.hidden) {
+      this.hidden = false;
+    } else {
+      this.hidden = true;
+    }
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    Animal.alive.push(this);
+  }
+
+  bite(animal) {
+    if (animal.hasOwnProperty('hidden') && animal.hidden === false) {
+      animal.health -= 50;
+
+      if (animal.health <= 0) {
+        const index = Animal.alive
+          .findIndex((currentAnimal) => currentAnimal === animal);
+
+        if (index >= 0) {
+          Animal.alive.splice(index, 1);
+        }
+      }
+    }
+  }
 }
 
 module.exports = {
