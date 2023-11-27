b5d30a8fa500e5df193c2b3190b6b0f43028b551
Hlib Vakhrameev@icloud.com <g.vakhrameev@icloud.com>
2023-11-27 11:43:18 +0200

    Write solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,39 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health, hidden = false) {
+    super(name, health);
+
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(target) {
+    if (target.hasOwnProperty('hidden')) {
+      if (!target.hidden) {
+        target.health -= 50;
+      }
+    }
+
+    Animal.alive = Animal.alive
+      .filter(animal => animal.health > 0);
+  }
 }
 
 module.exports = {
