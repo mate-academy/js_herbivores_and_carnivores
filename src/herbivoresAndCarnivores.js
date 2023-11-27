75b7a5932a6660ed34bff4b1dbbbb29ba5a8da02
Krot Nikita <krotnikitaasd@gmail.com>
2023-11-27 17:06:49 +0200

    Animal world

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,45 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    this.addToAliveList();
+  }
+
+  addToAliveList() {
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(victim) {
+    if (victim.hidden === false && this.hidden === undefined) {
+      victim.health -= 50;
+
+      if (victim.health === 0) {
+        for (let i = 0; i < Animal.alive.length; i++) {
+          if (Animal.alive[i].name === victim.name) {
+            delete Animal.alive[i];
+          }
+        }
+      }
+    }
+  }
 }
 
 module.exports = {
