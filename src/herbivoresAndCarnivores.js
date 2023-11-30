1e2dcecb6cdbecf878cbb609fefa59e354fbaba4
Maria Berezhna <marua545@gmail.com>
2023-11-30 16:39:43 +0200

    js_herbivores_and_carnivores

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,42 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.health = health;
+    this.name = name;
+    Animal.alive.push(this);
+  }
+
+  damshAnimal() {
+    Animal.alive = Animal.alive.filter(animal => animal.name !== this.name);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health = 100, hidden = false) {
+    super(name, health);
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+  }
+
+  bite(animal) {
+    if (animal instanceof Herbivore && !animal.hidden) {
+      animal.health -= 50;
+      animal.damshAnimal();
+    }
+  }
 }
+
+Animal.alive = [];
 
 module.exports = {
   Animal,
