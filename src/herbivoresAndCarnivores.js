01df7541815537a6dfbaf7c8b8d6435f31e043fe
Oleh Khrystofor <hristoforolegivanovich@gmail.com>
2023-12-02 12:21:32 +0200

    first

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,49 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.health = health;
+    this.name = name;
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health, hidden = false) {
+    super(name, health);
+    this.hidden = hidden;
+    Animal.alive.push(this);
+  }
+  hide(val) {
+    if (val === undefined) {
+      this.hidden = true;
+    } else if (val === true || false) {
+      this.hidden = val;
+    }
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    Animal.alive.push(this);
+  }
+  bite(val) {
+    if (val instanceof Herbivore) {
+      if (val.health === 50 && !val.hidden === true) {
+        val.health = 0;
+
+        const herbDelIndex = Animal.alive.indexOf(val);
+
+        Animal.alive.splice(herbDelIndex, 1);
+      }
+
+      if (val.health === 100 && !val.hidden === true) {
+        val.health = 50;
+      }
+    }
+  }
 }
 
 module.exports = {
