4893a39ab51af94ac8d6f3832f492f4cc4ba4c01
Mykhailo Hubko <mixelios@gmail.com>
2023-12-05 11:03:24 +0100

    tasck completed

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,37 @@
 'use strict';
 
 class Animal {
-  // write your code here
-}
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
+};
+
+Animal.alive = [];
 
 class Herbivore extends Animal {
-  // write your code here
-}
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
+};
 
 class Carnivore extends Animal {
-  // write your code here
-}
+  bite(animal) {
+    if ((animal instanceof Herbivore) && animal.hidden === false) {
+      animal.health -= 50;
+    };
+
+    const checkAnimal = Animal.alive.filter(item => item.health > 0);
+
+    Animal.alive = checkAnimal;
+  }
+};
 
 module.exports = {
   Animal,
