3a4959d5c06db33bc4bbd5b91af0b3e3cd1a4f91
Anastasia Granovskaya <iamgranovskaya@gmail.com>
2023-12-13 13:59:55 +0200

    fixes

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -4,6 +4,8 @@ class Animal {
   constructor(name, health = 100) {
     this.name = name;
     this.health = health;
+
+    Animal.alive.push(this);
   }
 }
 
@@ -13,8 +15,6 @@ class Herbivore extends Animal {
   constructor(name, health) {
     super(name, health);
     this.hidden = false;
-
-    Animal.alive.push(this);
   }
 
   hide() {
@@ -23,12 +23,6 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
-  constructor(name, health) {
-    super(name, health);
-
-    Animal.alive.push(this);
-  }
-
   bite(animal) {
     if (animal instanceof Herbivore && animal.hidden !== true) {
       animal.health -= 50;
