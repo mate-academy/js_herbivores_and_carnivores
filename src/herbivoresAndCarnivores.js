5bbcb788f06f25651d7e5cd7e2d37a2a4c8b15e4
Vasil Liakhivskyi <vazilx@gmail.com>
2023-12-21 16:10:49 +0100

    add task colution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,47 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static dying() {
+    const dyIndex = this.alive.findIndex(animal => animal.health <= 0);
+
+    Animal.alive.splice(dyIndex, 1);
+  }
+
+  constructor(name, health = 100) {
+    this.health = health;
+    this.name = name;
+
+    Animal.alive.push(this);
+  }
 }
+Animal.alive = [];
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    if (this.hidden) {
+      this.hidden = false;
+    } else {
+      this.hidden = true;
+    }
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (animal.hasOwnProperty('hidden') && animal.hidden === false) {
+      animal.health -= 50;
+    }
+
+    if (animal.health <= 0) {
+      Animal.dying();
+    }
+  }
 }
 
 module.exports = {
