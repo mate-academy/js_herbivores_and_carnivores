2f44cf91db37481fa6ee03085eff89ade11f9d5c
Serhii Kot <serhiikot.personal@outlook.com>
2023-12-21 15:43:19 +0200

    initial commit

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,52 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.health = health;
+    this.name = name;
+    Animal.addAnimal(this);
+  }
+
+  static addAnimal(animal) {
+    this.alive.push(animal);
+  }
+
+  static removeAnimal(animal) {
+    this.alive.splice(this.alive.indexOf(animal), 1);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health = 100, hidden = false) {
+    super(name, health);
+    this.hidden = hidden;
+  }
+
+  hide() {
+    if (this.hidden === false) {
+      this.hidden = true;
+    } else {
+      this.hidden = false;
+    }
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+  }
+
+  bite(prey) {
+    if (prey instanceof Herbivore && prey.hidden !== true && prey.health > 0) {
+      prey.health -= 50;
+    }
+
+    if (prey.health === 0) {
+      Animal.removeAnimal(prey);
+    }
+  }
 }
 
 module.exports = {
