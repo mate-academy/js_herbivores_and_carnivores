d5a574201a696bcb71adbb9c95bd4a5604195867
Anton Duchenko <deragandi0@gmail.com>
2023-12-27 22:41:55 +0200

    Solution after finding checklist

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -35,7 +35,7 @@ class Carnivore extends Animal {
       animal.health -= 50;
 
       if (animal.health === 0) {
-        Animal.alive.splice(Animal.alive.indexOf(animal), 1);
+        Animal.alive = Animal.alive.filter(anim => anim !== animal);
       }
     }
   }
