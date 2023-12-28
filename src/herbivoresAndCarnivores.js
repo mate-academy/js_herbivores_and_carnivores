a95e39ae6b7c50ec5307a2f003693e18a026fd53
White EdenI <danil.gayvoronsky2201@gmail.com>
2023-12-29 01:17:54 +0200

    Done! (try1)

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,52 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
+Animal.removeFromAlive = function(target) {
+  const index = this.alive.indexOf(target);
+
+  if (index !== -1) {
+    this.alive.splice(index, 1);
+  }
+};
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
+
+  decreaseHealth(amount) {
+    this.health -= amount;
+
+    if (this.health <= 0) {
+      Animal.removeFromAlive(this);
+    }
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+  }
+
+  bite(target) {
+    if (target instanceof Herbivore && !target.hidden) {
+      target.decreaseHealth(50);
+    }
+  }
 }
 
 module.exports = {
