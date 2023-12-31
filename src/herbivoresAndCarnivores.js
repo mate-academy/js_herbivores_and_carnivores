50dd738a717a0d98f42dcd26f5e26f36509686e0
Artem <artemmalikov742@gmail.com>
2023-12-31 18:19:21 +0200

    add constants

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,20 +1,27 @@
 'use strict';
 
 class Animal {
-  constructor(name, health = 100) {
+  static FULL_HEALTH_ANIMAL = 100;
+  static DEATH_HEALTH = 0;
+
+  static alive = [];
+
+  constructor(name, health = Animal.FULL_HEALTH_ANIMAL) {
     this.name = name;
     this.health = health;
   }
-}
 
-Animal.alive = [];
+  static addAliveAnimal(animal) {
+    this.alive.push(animal);
+  }
+}
 
 class Herbivore extends Animal {
   constructor(name, health) {
     super(name, health);
     this.hidden = false;
 
-    Animal.alive.push(this);
+    Animal.addAliveAnimal(this);
   }
 
   hide() {
@@ -23,17 +30,19 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
+  static CARNIVORE_DAMAGE = 50;
+
   constructor(name, health) {
     super(name, health);
 
-    Animal.alive.push(this);
+    Animal.addAliveAnimal(this);
   }
 
   bite(herbivore) {
     if (herbivore instanceof Herbivore && herbivore.hidden === false) {
-      herbivore.health -= 50;
+      herbivore.health -= Carnivore.CARNIVORE_DAMAGE;
 
-      if (herbivore.health === 0) {
+      if (herbivore.health === Animal.DEATH_HEALTH) {
         Animal.alive = Animal.alive.filter(animal => animal !== herbivore);
       }
     }
