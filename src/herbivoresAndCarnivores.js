342e7153c82c6aaf92de1e5345e65066095d4c9d
Andrew Rich <andy.rich3a@gmail.com>
2024-01-07 10:37:52 +0200

    add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,40 @@
 
 class Animal {
   // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
+
+  static removeDeadAnimals() {
+    Animal.alive = Animal.alive.filter(animal => animal.health > 0);
+  }
 }
+
+Animal.alive = [];
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  bite(animal) {
+    if (!(animal instanceof Carnivore)
+      && !(animal instanceof Herbivore && animal.hidden)) {
+      animal.health -= 50;
+      Animal.removeDeadAnimals();
+    }
+  }
 }
 
 module.exports = {
