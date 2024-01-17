cf7a7c25a946461d3927113fcf4c59e0ccc0a7b9
Artem Vlasiuk <artemvlasiuk@93gmail.com>
2024-01-17 17:50:03 +0200

    Soilution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,39 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.health = 100;
+    this.name = name;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (animal.hasOwnProperty('hidden') && !animal.hidden) {
+      animal.health -= 50;
+    }
+
+    Animal.alive.forEach((element, index) => {
+      if (element.health === 0) {
+        Animal.alive.splice(index, 1);
+      }
+    });
+  }
 }
+
+Animal.alive = [];
 
 module.exports = {
   Animal,
