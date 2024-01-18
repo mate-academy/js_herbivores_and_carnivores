15df9ef458b0bbd2035e5d2a18b14272816c8c02
Stanislav Kukharuk <kukharuk.stan1slav@gmail.com>
2024-01-18 19:40:19 +0000

    small changes

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -25,7 +25,7 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(animal) {
-    if (!animal.hidden && animal.hasOwnProperty('hidden')) {
+    if (!animal.hidden && animal instanceof Herbivore) {
       animal.health -= 50;
     }
 
