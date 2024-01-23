56c615f7293d81728f7e47f77f37366d11eb22d6
Volodymyr Sarabun <vov4ukcool25@gmail.com>
2024-01-23 17:42:03 +0200

    task animal

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,50 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.health = 100;
+    this.name = name;
+    Animal.alive.push(this);
+  }
+
+  static removeFromAlive(animal) {
+    Animal.alive = Animal.alive.filter(a => a !== animal);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  // eslint-disable-next-line no-useless-constructor
+  constructor(name) {
+    super(name);
+  }
+
+  bite(animal) {
+    if (animal instanceof Carnivore
+      || (animal instanceof Herbivore
+      && animal.hidden)) {
+      return `${this.name} cannot bite ${animal.name}`;
+    } else {
+      animal.health -= 50;
+
+      if (animal.health <= 0) {
+        Animal.removeFromAlive(animal);
+      }
+    }
+  }
 }
 
 module.exports = {
