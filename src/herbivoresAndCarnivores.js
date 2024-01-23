92c28765350b35df0236d961a5063cd3a81e7cd3
Tetiana Bukhenko <tania04bukhenko@gmail.com>
2024-01-23 20:02:39 +0200

    add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -4,6 +4,8 @@ class Animal {
   constructor(name) {
     this.name = name;
     this.health = 100;
+
+    Animal.alive.push(this);
   }
 }
 
@@ -13,7 +15,6 @@ class Herbivore extends Animal {
   constructor(name) {
     super(name);
     this.hidden = false;
-    Animal.alive.push(this);
   }
 
   hide() {
@@ -22,21 +23,14 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
-  constructor(name) {
-    super(name);
-    Animal.alive.push(this);
-  }
-
   bite(name) {
-    if (name instanceof Herbivore) {
-      if (name.hidden !== true) {
-        name.health -= 50;
+    if (name instanceof Herbivore && !name.hidden) {
+      name.health -= 50;
 
-        if (name.health <= 0) {
-          const index = Animal.alive.indexOf(name);
+      if (name.health <= 0) {
+        const index = Animal.alive.indexOf(name);
 
-          Animal.alive.splice(index, 1);
-        }
+        Animal.alive.splice(index, 1);
       }
     }
   }
