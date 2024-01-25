21bb4223f96e5fe81380f62e98e9dc769d1159c8
“Julia <j.shkorupatska@gmail.com>
2024-01-25 21:17:48 +0200

    developed herbivores and carnivores function

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,47 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health) {
+    this.name = name;
+    this.health = health || 100;
+    Animal.addAlive(this);
+  }
+
+  static addAlive(animal) {
+    Animal.alive.push(animal);
+  }
+
+  static removeDead() {
+    return Animal.alive.filter(animal => animal.health > 0);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health, hidden) {
+    super(name, health);
+    this.hidden = hidden || false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.demage = 50;
+  }
+
+  bite(herbivore) {
+    if (
+      herbivore instanceof Herbivore && !herbivore.hidden) {
+      herbivore.health -= this.demage;
+      Animal.alive = Animal.removeDead();
+    }
+  }
 }
 
 module.exports = {
