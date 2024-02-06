958320b2f766c43d0c350df5489c6e66082c03a2
Nikita <2004nnikita@gmail.com>
2024-02-06 22:40:01 +0100

    first attempt

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,56 @@
 'use strict';
 
 class Animal {
+  static alive = [];
   // write your code here
+  constructor (name, health) {
+    this.name = name;
+    this.health = health;
+
+    if (this.health === undefined) {
+      this.health = 100;
+    }
+    Animal.alive.push(this);
+  }
+
+  static removeDeadAnimal(animal) {
+    const index = Animal.alive.indexOf(animal);
+    if (index !== -1) {
+      Animal.alive.splice(index, 1);
+    }
+  }
 }
+
+
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide () {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  constructor(name, health) {
+    super(name, health);
+  }
+
+  bite (animal) {
+    if (animal instanceof Herbivore && !animal.hidden) {
+      animal.health -= 50;
+      if (animal.health <= 0) {
+        Animal.removeDeadAnimal(animal);
+      }
+    }
+  }
 }
+
 
 module.exports = {
   Animal,
