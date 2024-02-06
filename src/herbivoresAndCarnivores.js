f43ea7ded4c1df5d9beb7b9610a52a01f53d3a7f
Roman Babii <58149320+self51@users.noreply.github.com>
2024-02-06 15:50:59 +0200

    Fixed herbivoresAndCarnivores.js

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -23,7 +23,7 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(herbivore) {
-    const canBite = !herbivore.hidden
+    const canBite = herbivore.hidden === false
       && herbivore.prototype !== Carnivore.prototype;
 
     if (canBite) {
