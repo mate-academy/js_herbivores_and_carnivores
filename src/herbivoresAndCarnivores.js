c035356083b4ec22bccfd730f71570ea50818981
Yehor-f2006 <151622806+Yehor-f2006@users.noreply.github.com>
2024-02-12 12:11:09 +0200

    solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -4,49 +4,28 @@ class Animal {
   constructor(name) {
     this.name = name;
     this.health = 100;
+    this.hidden = false;
+    Animal.alive.push(this);
   }
 
   static alive = [];
 
-  static addToArray(animal) {
-    Animal.alive.push(animal);
-  }
-
-  static removeAnimal(animal) {
-    const index = Animal.alive.findIndex(one => one === animal);
-
-    if (animal.health > 0) {
-      Animal.alive.splice(index, 1, animal);
-    } else {
-      Animal.alive.splice(index, 1);
-    }
+  static removeAnimal() {
+    Animal.alive = Animal.alive.filter(one => one.health > 0);
   }
 }
 
 class Herbivore extends Animal {
-  constructor(name) {
-    super(name);
-    this.hidden = false;
-
-    Animal.addToArray(this);
-  }
-
   hide() {
     this.hidden = true;
   }
 }
 
 class Carnivore extends Animal {
-  constructor(name) {
-    super(name);
-
-    Animal.addToArray(this);
-  }
-
   bite(animal) {
     if (!animal.hidden && animal instanceof Herbivore) {
       animal.health -= 50;
-      Animal.removeAnimal(animal);
+      Animal.removeAnimal();
     }
   }
 }
