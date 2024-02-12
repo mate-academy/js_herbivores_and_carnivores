52e2c7ff1153fbf8c29551b13315ad980eb8d28b
Богдан <BoGoBooM@MacBook-Pro---Bohdan.local>
2024-02-13 00:52:57 +0200

    task solution:)

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,35 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herbAnimal) {
+    if (herbAnimal.hide && !herbAnimal.hidden === true) {
+      herbAnimal.health -= 50;
+    }
+
+    Animal.alive = Animal.alive.filter(beast => beast.health > 0);
+  }
 }
 
 module.exports = {
