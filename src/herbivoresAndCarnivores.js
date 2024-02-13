6ce4c8c580c55bdafd4163927c9f0fbd7eef4b09
Denys209818 <deniskravcuk63@gmail.com>
2024-02-13 19:24:32 +0200

    Load full realization

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,42 @@
 
 class Animal {
   // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    if (!Animal.alive) {
+      Animal.alive = [];
+    }
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  bite(animal) {
+    if ((animal instanceof Herbivore)
+      && !animal.hidden) {
+      animal.health -= 50;
+
+      if (animal.health <= 0) {
+        Animal.alive = [...Animal.alive.filter(x => x !== animal)];
+      }
+    }
+  }
 }
 
 module.exports = {
