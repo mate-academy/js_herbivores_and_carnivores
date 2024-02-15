8fe46ffa06d580fb3bb9c2a88287dd07667487d7
zozulia.kirill <kirillzozulya95@gmail.com>
2024-02-15 19:48:16 +0200

    resolve task

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -29,15 +29,15 @@ class Carnivore extends Animal {
 
   bite(animal) {
     if (
-        animal instanceof Herbivore
+      animal instanceof Herbivore
         && !animal.hidden
         && animal.health > 0
-      ) {
-        animal.health -= 50;
+    ) {
+      animal.health -= 50;
     }
 
     if (animal.health === 0) {
-      Animal.alive.splice(Animal.alive.indexOf(animal), 1)
+      Animal.alive.splice(Animal.alive.indexOf(animal), 1);
     }
   }
 }
