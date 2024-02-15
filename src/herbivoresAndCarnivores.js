1bf666042fe30f6b4aec1f57d735f678c0afb162
zozulia.kirill <kirillzozulya95@gmail.com>
2024-02-15 19:59:26 +0200

    resolve comments

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -4,29 +4,21 @@ class Animal {
   constructor(name, health = 100) {
     this.name = name;
     this.health = health;
+    this.hidden = false;
+
+    Animal.alive.push(this);
   }
 }
 
 Animal.alive = [];
 
 class Herbivore extends Animal {
-  constructor(name) {
-    super(name);
-    this.hidden = false;
-    Animal.alive.push(this);
-  }
-
   hide() {
     this.hidden = true;
   }
 }
 
 class Carnivore extends Animal {
-  constructor(name) {
-    super(name);
-    Animal.alive.push(this);
-  }
-
   bite(animal) {
     if (
       animal instanceof Herbivore
@@ -36,9 +28,7 @@ class Carnivore extends Animal {
       animal.health -= 50;
     }
 
-    if (animal.health === 0) {
-      Animal.alive.splice(Animal.alive.indexOf(animal), 1);
-    }
+    Animal.alive = Animal.alive.filter(herb => herb.health !== 0);
   }
 }
 
