fadab44e2e7697f11ebdc9800456879884027a5c
Denys Stronin <den.stronin@gmail.com>
2024-02-23 11:32:53 +0200

    The Herbivores_and_Сarnivores class implemented

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,40 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(unit) {
+    const isHerb = unit instanceof Herbivore;
+    const invisible = unit.hidden === false;
+
+    if (isHerb && invisible) {
+      const POINT = 50;
+
+      unit.health -= POINT;
+    }
+
+    Animal.alive = Animal.alive.filter(elem => elem.health >= 50);
+  }
 }
 
 module.exports = {
