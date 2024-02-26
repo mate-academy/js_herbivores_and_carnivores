f7cceed15e046d406424f83597e48fc8268fbdbc
Nirvacsh <nir.vlad.zu@gmail.com>
2024-02-26 15:36:26 +0200

    first try

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,59 @@
 'use strict';
 
+const BASE_HEALTH = 100;
+const DECREASE_HEALTH = 50;
+
 class Animal {
-  // write your code here
+  constructor(name, health = BASE_HEALTH) {
+    this.name = name;
+    this._health = health;
+    Animal.alive.push(this);
+  }
+
+  get health() {
+    return this._health;
+  }
+
+  set health(value) {
+    this._health = value;
+
+    if (this._health <= 0) {
+      this.die();
+    }
+  }
+
+  decreaseHealth(amount) {
+    this.health -= amount;
+  }
+
+  die() {
+    Animal.alive.splice(Animal.alive.indexOf(this), 1);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+
+    return this;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(target) {
+    if (target.hidden || target instanceof Carnivore) {
+      return;
+    }
+
+    target.decreaseHealth(DECREASE_HEALTH);
+  }
 }
 
 module.exports = {
