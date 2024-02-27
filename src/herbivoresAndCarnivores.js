da4fe8c63133b9abad12da1decb0111f5450f30b
Nirvacsh <nir.vlad.zu@gmail.com>
2024-02-27 08:59:25 +0200

    fix

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -27,7 +27,7 @@ class Animal {
   }
 
   die() {
-    Animal.alive = Animal.alive.filter(animal => animal.health <= 0);
+    Animal.alive = Animal.alive.filter(animal => animal.health > 0);
   }
 }
 
