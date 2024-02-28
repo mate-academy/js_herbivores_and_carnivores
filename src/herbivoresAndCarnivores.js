1588a1bf4b55cc35dfef9d4e661f79a6911a001a
Andrey Bikir <bikir.a.v@gmail.com>
2024-02-28 03:00:21 +0200

    Animal

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,37 @@
 'use strict';
 
+const baseHealth = 100;
+const damage = -50;
+
 class Animal {
-  // write your code here
+  constructor(name, health = baseHealth) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
+Animal.alive = [];
+
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, hidden = false) {
+    super(name);
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (animal instanceof Herbivore && animal.hidden === false) {
+      animal.health += damage;
+    }
+
+    Animal.alive = Animal.alive.filter(el => el.health > 0);
+  }
 }
 
 module.exports = {
