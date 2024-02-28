c4c8e606bff026e5d552759122aeb7c7229c5df3
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2024-02-28 03:51:03 +0200

    feature [js-advanced]: implement Animal class & its children

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,34 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.health = health;
+    this.name = name;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hidden = false;
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (animal instanceof Herbivore && !animal.hidden) {
+      animal.health -= 50;
+    }
+
+    if (!animal.health) {
+      Animal.alive = Animal.alive
+        .filter(alive => alive.name !== animal.name);
+    }
+  }
 }
 
 module.exports = {
