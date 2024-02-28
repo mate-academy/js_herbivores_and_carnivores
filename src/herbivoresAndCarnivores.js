5e3c07b1670a52d907da418e42bf8b9edfed57cf
DESKTOP-EHDJ2FA\yarik <yarik.zubyk@gmail.com>
2024-02-28 22:06:49 +0200

    created Animals project

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,46 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.health = health;
+    this.name = name;
+
+    Animal.alive.push(this);
+  }
 }
+Animal.alive = [];
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health = 100, hidden = false) {
+    super(name, health);
+    this.name = name;
+    this.hidden = hidden;
+    this.health = health;
+
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+
+    Animal.alive.push(this);
+  }
+
+  bite(animal) {
+    if (animal.hidden === true || animal instanceof Carnivore) {
+      return;
+    }
+
+    animal.health -= 50;
+
+    Animal.alive = Animal.alive.filter(item => item.health > 0);
+  }
 }
 
 module.exports = {
