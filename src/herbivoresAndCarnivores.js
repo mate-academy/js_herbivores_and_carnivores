babae27d04fbc46514411b2bf2d92e619c241c33
Yevhen Vladarskyi <vladarskyy@gmail.com>
2024-03-01 13:29:47 +0200

    add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,49 @@
 'use strict';
 
+const AMOUNT_OF_HEALTH = 100;
+
 class Animal {
-  // write your code here
+  static alive = [];
+
+  health = AMOUNT_OF_HEALTH;
+
+  constructor(name) {
+    this.name = name;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+
+    this.hidden = false;
+
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    this.hidden = (this.hidden)
+      ? false
+      : true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+
+    Animal.alive.push(this);
+  }
+
+  bite(animal) {
+    if (animal instanceof Herbivore && !animal.hidden) {
+      animal.health -= 50;
+    }
+
+    if (animal.health === 0) {
+      Animal.alive = Animal.alive.filter(beast => beast !== animal);
+    }
+  }
 }
 
 module.exports = {
