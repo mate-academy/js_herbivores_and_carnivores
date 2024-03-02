aa60bcea70c69259d2e98c9aeeba8675d10c284d
Andrii Dudar <andrij.dudar@gmail.com>
2024-03-02 14:33:25 +0200

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -26,9 +26,7 @@ class Carnivore extends Animal {
       animal.health -= 50;
     }
 
-    if (animal.health <= 0) {
-      Animal.alive.splice(Animal.alive.indexOf(animal), 1);
-    }
+    Animal.alive = Animal.alive.filter((a) => a.health > 0);
   }
 }
 
