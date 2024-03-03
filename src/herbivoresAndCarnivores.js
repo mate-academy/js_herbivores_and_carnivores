62f18a0f1d499b6bda34bfc11ce4a3d087debfa6
Roman Romanchuk <roma.roman4uk.48@gmail.com>
2024-03-03 21:20:08 +0100

    did the task

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,53 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
+
+  static removeDeadAnimal(animal) {
+    const index = Animal.alive.indexOf(animal);
+
+    if (index !== -1) {
+      Animal.alive.splice(index, 1);
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(target) {
+    if (
+      target instanceof Carnivore ||
+      (target.hidden && target instanceof Herbivore)
+    ) {
+      return 'The bite failed.';
+    } else {
+      target.health -= 50;
+
+      if (target.health <= 0) {
+        Animal.removeDeadAnimal(target);
+
+        return `${target.name} has been killed.`;
+      } else {
+        return `${target.name} has been bitten.`;
+      }
+    }
+  }
 }
 
 module.exports = {
