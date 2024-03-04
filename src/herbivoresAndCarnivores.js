9551cc3dd9829dc404d11b0530cd55ea26d926d7
Max Petrashishin <rikogjdefolt@gmail.com>
2024-03-04 13:12:43 +0200

    done animal

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,40 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  static borned(anim) {
+    Animal.alive.push(anim);
+  }
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+
+    Animal.borned(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(anim) {
+    if (anim.hidden !== undefined && !anim.hidden) {
+      anim.health -= 50;
+
+      Animal.alive = Animal.alive.filter((an) => an.health > 0);
+    }
+  }
 }
 
 module.exports = {
