cea23c0944394098b634f24fac5986462b0d10e4
Iryna Liashko <ira.liashko1@gmail.com>
2024-03-07 20:53:45 +0100

    question

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,38 @@
 
 class Animal {
   // write your code here
+  static alive = [];
+
+  constructor(health = 100, name) {
+    this.health = health;
+    this.name = name;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(health = 100, name) {
+    super(health, name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  constructor(health = 100, name) {
+    super(health, name);
+  }
+
+  bite() {
+    if (!Herbivore && !this.hidden) {
+      this.health -= 50;
+    }
+  }
 }
 
 module.exports = {
