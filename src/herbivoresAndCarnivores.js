95b1ec30c23358c640234a89a17d56aa58a1b379
Vadym Bryvka <vadym.bryvkaa@gmail.com>
2024-03-07 20:24:01 -0600

    all done

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,45 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  constructor(health = 100, name) {
+    this.name = name;
+    this.health = health;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(health, name) {
+    super(health, name);
+    this.hidden = false;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(health, name) {
+    super(health, name);
+    this.health = 100;
+
+    if (this.health !== 0) {
+      Animal.alive.push(this);
+    }
+  }
+
+  bite(someAnimal) {
+    if (someAnimal.hidden !== true && someAnimal instanceof Herbivore) {
+      someAnimal.health -= 50;
+
+      if (someAnimal.health <= 0) {
+        Animal.alive.pop(someAnimal);
+      }
+    }
+  }
 }
 
 module.exports = {
