68562c9194147295e5d3b2b97707308ad7ba6322
Maksym-Atanasov <157310647+Maksym-Atanasov@users.noreply.github.com>
2024-03-09 19:35:11 +0200

    solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,45 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+  }
+
+  bite(animal) {
+    if (!(animal.hasOwnProperty(Carnivore)) && animal.hidden === false) {
+      animal.health -= 50;
+    }
+
+    if (animal.health <= 0) {
+      Animal.alive.forEach((element, index) => {
+        if (element.name === animal.name) {
+          delete Animal.alive[index];
+        }
+      });
+    }
+  }
 }
 
 module.exports = {
