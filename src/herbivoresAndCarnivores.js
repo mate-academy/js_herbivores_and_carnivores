55df0128b5a03d255bd9c433e006d20a1a3afdfa
Maryna Karpova <mari.artarea@gmail.com>
2024-03-12 09:37:35 +0100

    solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -10,7 +10,7 @@ class Animal {
     Animal.alive.push(this);
   }
 
-  static removedAnimals() {
+  static toRemovAnimals() {
     Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
   }
 }
@@ -30,7 +30,7 @@ class Carnivore extends Animal {
   bite(animal) {
     if (!animal.hidden && animal instanceof Herbivore) {
       animal.health -= 50;
-      Animal.removedAnimals();
+      Animal.toRemovAnimals();
     }
   }
 }
