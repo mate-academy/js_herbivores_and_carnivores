709ff4602f1352ae73dd07b40c36ce1ff79325aa
Valentyna Lytvynenko <valentina.vik10@gmail.com>
2024-03-13 21:05:09 +0200

    add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,48 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, role = 'herbivore') {
+    super(name);
+    this.role = role;
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, role = 'carnivore') {
+    super(name);
+    this.role = role;
+  }
+
+  bite(target) {
+    if (target instanceof Carnivore || target.hidden) {
+      return;
+    }
+
+    target.health -= 50;
+
+    if (target.health === 0) {
+      const index = Animal.alive.indexOf(target);
+
+      if (index !== -1) {
+        Animal.alive.splice(index, 1);
+      }
+    }
+  }
 }
 
 module.exports = {
