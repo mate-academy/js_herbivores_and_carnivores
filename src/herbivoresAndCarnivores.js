21ccd6bbc4fa091a9fb84f1a4c74ffe54c2b67bf
hyriix <shyrii11bohdan@gmail.com>
2024-03-15 16:54:04 +0100

    liquidated

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,45 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
+  static alive = [];
+  static clearHungerGameStat() {
+    Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hidden = false;
+
+  // eslint-disable-next-line no-useless-constructor
+  constructor(name) {
+    super(name);
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  // eslint-disable-next-line no-useless-constructor
+  constructor(name) {
+    super(name);
+  }
+
+  bite(target) {
+    if (target.hidden === false) {
+      target.health -= 50;
+    }
+
+    if (target.health <= 0) {
+      Animal.clearHungerGameStat();
+    }
+  }
 }
 
 module.exports = {
