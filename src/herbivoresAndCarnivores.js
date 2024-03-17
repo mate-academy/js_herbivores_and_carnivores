dc4069758f16863b9b4609d3496248fc1af729f7
Vadym <khromeicreate@gmail.com>
2024-03-17 19:22:36 +0200

    update task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,21 +1,19 @@
 'use strict';
 
 class Animal {
+  static MAX_HEALTH = 100;
+  static MIN_HEALTH = 0;
   static alive = [];
 
   constructor(name) {
     this.name = name;
-    this.health = 100;
+    this.health = Animal.MAX_HEALTH;
     Animal.alive.push(this);
   }
 
   checkHealth() {
-    if (this.health <= 0) {
-      const index = Animal.alive.indexOf(this);
-
-      if (index > -1) {
-        Animal.alive.splice(index, 1);
-      }
+    if (this.health <= Animal.MIN_HEALTH) {
+      Animal.alive = Animal.alive.filter((animal) => animal !== this);
     }
   }
 }
@@ -27,14 +25,18 @@ class Herbivore extends Animal {
   }
 
   hide() {
-    this.hidden = !this.hidden;
+    this.hidden = true;
+  }
+
+  unhide() {
+    this.hidden = false;
   }
 }
 
 class Carnivore extends Animal {
   bite(prey) {
     if (prey instanceof Herbivore && !prey.hidden) {
-      prey.health -= 50;
+      prey.health = Math.max(Animal.MIN_HEALTH, prey.health - 50);
       prey.checkHealth();
     }
   }
