04d0171fb0517044be860aa4c808c8beb8da0c79
Rafael Wesley <rafaelwesleylima@gmail.com>
2024-03-25 18:53:39 -0300

    final changes

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -5,7 +5,7 @@ class Animal {
   static alive = [];
 
   constructor(name) {
-    this.health = 100;
+    this.innerHealthVar = 100;
     this.name = name;
   }
 }
@@ -22,6 +22,25 @@ class Herbivore extends Animal {
   hide() {
     this.hidden = !this.hidden;
   }
+
+  set health(totalLifeDecreased) {
+    this.innerHealthVar = !this.hidden
+      ? totalLifeDecreased
+      : this.innerHealthVar;
+
+    if (this.innerHealthVar < 1) {
+      this.innerHealthVar = 0;
+
+      Animal.alive.splice(
+        Animal.alive.findIndex((e) => e === this),
+        1,
+      );
+    }
+  }
+
+  get health() {
+    return this.innerHealthVar;
+  }
 }
 
 class Carnivore extends Animal {
@@ -33,18 +52,21 @@ class Carnivore extends Animal {
   }
 
   bite(herbivore) {
-    if (!herbivore.hidden && !(herbivore instanceof Carnivore)) {
-      herbivore.health -= 50;
+    herbivore.health -= 50;
+  }
 
-      if (herbivore.health < 1) {
-        Animal.alive.splice(
-          Animal.alive.findIndex((e) => e === herbivore),
-          1,
-        );
-      }
-    }
+  set health(totalLifeDecreased) {}
+
+  get health() {
+    return this.innerHealthVar;
   }
 }
+
+const tiger = new Carnivore('Tiger');
+const zebra = new Herbivore('Zebra');
+
+tiger.bite(zebra);
+tiger.bite(zebra);
 
 module.exports = {
   Animal,
