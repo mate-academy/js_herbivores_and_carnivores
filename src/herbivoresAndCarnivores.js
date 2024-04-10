4a353ed54d03f10f9c51fc1f1559b85e57a1771e
Arthur Fujii <arthurfujii@gmail.com>
2024-04-10 18:57:30 -0300

    first commit

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,52 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(health = 100, name) {
+    this.health = health;
+    this.name = name;
+  }
+
+  static alive = [];
+  static get alive() {
+    if (this.alive === undefined) {
+      this.alive = [];
+      this.alive.push(this);
+    }
+
+    return this.alive;
+  }
+
+  static set alive(beast) {
+    if (beast.health === 0) {
+      this.alive.splice(
+        this.alive.find((a, index) => {
+          if (a === this) {
+            return index;
+          }
+        }),
+        1,
+      );
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(health, name, hidden = false) {
+    super(health, name);
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(creature) {
+    if (creature instanceof Herbivore && creature.hidden === false) {
+      creature.health -= 50;
+    }
+  }
 }
 
 module.exports = {
