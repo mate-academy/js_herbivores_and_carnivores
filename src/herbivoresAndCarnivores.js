c6fc30af282be3e05b9e8ed74f745b26237a88da
Prytula Olexandr <liga5872@gmail.com>
2024-04-12 13:53:18 +0300

    add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,47 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  static FULL_HEALTH = 100;
+
+  constructor(name) {
+    this.name = name;
+
+    this.health = Animal.FULL_HEALTH;
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  static HIDDEN = false;
+
+  constructor(name) {
+    super(name);
+
+    this.health = Herbivore.FULL_HEALTH;
+    this.hidden = Herbivore.HIDDEN;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+
+    this.health = Carnivore.FULL_HEALTH;
+  }
+
+  bite(herbivore) {
+    if (herbivore instanceof Herbivore && !herbivore.hidden) {
+      herbivore.health -= 50;
+    }
+
+    Animal.alive = Animal.alive.filter((live) => live.health > 0);
+  }
 }
 
 module.exports = {
