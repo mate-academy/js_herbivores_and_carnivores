aeea7888a8e28f48b4f4c44633f0e350f6260f3a
Bartek <kowalski_1410@wp.pl>
2024-04-14 22:22:45 +0200

    task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,45 @@
 
 class Animal {
   // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.health = 100;
+    this.name = name;
+    Animal.alive.push(this);
+  }
+
+  decreaseHealth() {
+    this.health -= 50;
+    if (this.health <= 0) {
+      Animal.alive = Animal.alive.filter(animal => animal !== this);
+    }
+  }
 }
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(health,name) {
+    super(health,name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  constructor(health,name) {
+    super(health,name);
+  }
+
+  bite(animal) {
+    if (animal instanceof Herbivore && animal.hidden === false) {
+      animal.decreaseHealth();
+    };
+  }
 }
 
 module.exports = {
