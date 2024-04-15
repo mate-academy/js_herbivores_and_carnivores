56e7b049c86a8eb60546cc78e0a950050e2bc9f2
Yurii Filiushkin <yurii.filiushkin@gmail.com>
2024-04-15 11:42:17 +0300

    sol

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,42 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health) {
+    Animal.alive.push(this);
+
+    this.name = name;
+    this.health = health || 100;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.x = 1;
+  }
+
+  bite(herOb) {
+    if (!(herOb instanceof Carnivore) && !herOb.hidden) {
+      herOb.health -= 50;
+    }
+
+    if (herOb.health <= 0) {
+      Animal.alive.splice(Animal.alive.indexOf(herOb), 1);
+    }
+  }
 }
 
 module.exports = {
