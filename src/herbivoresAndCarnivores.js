499106efae81a42d1d68a5647867de44950a6bdb
Mykhailo Lapchynskyi <mykhailolapchynskyi@Mykhailos-MacBook-Air.local>
2024-05-03 16:25:52 +0200

    task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,44 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static animalArr = [];
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.animalArr.push(this);
+  }
+
+  static get alive() {
+    return Animal.animalArr;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (animal instanceof Herbivore && !animal.hidden) {
+      const animalIndex = Animal.animalArr.findIndex((herb) => herb === animal);
+
+      if (animalIndex !== -1) {
+        if (Animal.animalArr[animalIndex].health > 50) {
+          Animal.animalArr[animalIndex].health -= 50;
+        } else {
+          Animal.animalArr.splice(animalIndex, 1);
+        }
+      }
+    }
+  }
 }
 
 module.exports = {
