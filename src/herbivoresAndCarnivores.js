d4c51081aec76361f3fbcb3e6c9ace38c802ec11
GHFLOW MATE <ghflowmate@outlook.com>
2024-05-15 15:54:55 +0300

    Update herbivoresAndCarnivores.js

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,36 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  constructor({ health = 100, name }) {
+    this.health = health;
+    this.name = name;
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(bittedAnimal) {
+    if (bittedAnimal instanceof Herbivore && !bittedAnimal.hidden) {
+      bittedAnimal.health -= 50;
+    }
+
+    if (bittedAnimal.health <= 0) {
+      Animal.alive = Animal.alive.filter((animal) => animal !== bittedAnimal);
+    }
+  }
 }
 
 module.exports = {
