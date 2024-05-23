3ef218e594885d229f212805d4dbad27713da740
Polina <polinabondar4@gmail.com>
2024-05-23 19:11:37 +0300

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,37 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.health = 100;
+    this.name = name;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hidden = false;
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herbivore) {
+    if (herbivore instanceof Herbivore && !herbivore.hidden) {
+      herbivore.health -= 50;
+
+      if (herbivore.health === 0) {
+        this.removeFromAlive(herbivore);
+      }
+    }
+  }
+
+  removeFromAlive(herbivore) {
+    Animal.alive = Animal.alive.filter((animal) => animal !== herbivore);
+  }
 }
 
 module.exports = {
@@ -17,3 +39,25 @@ module.exports = {
   Herbivore,
   Carnivore,
 };
+
+/*
+ ✓ Herbivore can be used as a constructor (2 ms)
+    ✓ Сarnivore can be used as a constructor (1 ms)
+    ✕ Herbivore should has falsy 'hidden' property (4 ms)
+    ✕ Herbivores and Carnivores should have health property equal to 100 (1 ms)
+    ✓ Herbivore should have 'hide' method which inverts 'hidden' property (1 ms)
+    ✓ Herbivores 'hide' method should be inherited
+    ✕ Сarnivore should have 'bite' method,
+  which decrease 'health' property of the herbivores
+    ✓ Сarnivores 'bite' method should be inherited (1 ms)
+    ✕ 'bite' method shouldn't decrease 'health' property of the herbivore,
+   if it has 'hidden === true
+    ✕ 'bite' method shouldn't decrease 'health' property
+    of the carnivores  (1 ms)
+    ✕ Carnivores and Herbivores should be in 'Animal.alive' array (1 ms)
+    ✕ Herbivores should be removed from 'Animal.alive' array,
+  when their health is 0
+    ✕ Herbivores should be removed from 'Animal.alive'
+    array, when their health is 0 (but Zebra is not the last defined animal)
+    ✕ Two of the same herbivore exist, but only one dies
+*/
