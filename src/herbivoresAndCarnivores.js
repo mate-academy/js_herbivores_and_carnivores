7c7c61459b19e202cc430726aab28be4f7ef6f8d
Svitlana-Vitryk <vitriksvitlana@gmail.com>
2024-05-29 18:38:03 +0300

    herbivores_and_carnivores

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,46 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
+
+  static removeDeadAnimals() {
+    Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+  }
+
+  bite(target) {
+    if (
+      target instanceof Carnivore ||
+      (target.hidden && target instanceof Herbivore)
+    ) {
+      return;
+    }
+
+    target.health -= 50;
+    Animal.removeDeadAnimals();
+  }
 }
 
 module.exports = {
