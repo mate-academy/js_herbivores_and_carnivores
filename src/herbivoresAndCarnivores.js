bdfb98da76e3625e49b0859b0273400fc3b41c8d
Kryvytskyi Volodymyr <v.kryvytskyy@mate.academy>
2024-05-31 18:19:12 +0300

    omni review 3

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,38 +1,27 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable no-unused-expressions */
-/* eslint-disable no-sequences */
-/* eslint-disable prettier/prettier */
 'use strict';
 
 class Animal {
   static alive = [];
+  constructor(name, health = 100, hidden = false) {
+    this.health = health;
+    this.name = name;
+    this.hidden = hidden;
 
-  constructor(name, health = 100) {
-    (this.name = name), (this.health = health), Animal.alive.push(this);
-  }
-
-  checkHealth() {
-    if (this.health <= 0) {
-      Animal.alive = Animal.alive.filter((animal) => animal !== this);
-    }
+    Animal.alive.push(this);
   }
 }
 
 class Herbivore extends Animal {
-  constructor(name) {
-    super(name), (this.hidden = false);
-  }
-
   hide() {
     this.hidden = true;
   }
 }
 
 class Carnivore extends Animal {
-  bite(obj) {
-    if (obj instanceof Herbivore && !obj.hidden) {
-      obj.health -= 50;
-      obj.checkHealth();
+  bite(target) {
+    if (target instanceof Herbivore && !target.hidden) {
+      target.health -= 50;
+      Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
     }
   }
 }
