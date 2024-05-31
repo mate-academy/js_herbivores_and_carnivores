86d9526abbdef0ed5a21a8278588c868385f1850
Kryvytskyi Volodymyr <v.kryvytskyy@mate.academy>
2024-05-31 18:05:40 +0300

    Update herbivoresAndCarnivores.js

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,19 +1,26 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-sequences */
+/* eslint-disable prettier/prettier */
 'use strict';
 
 class Animal {
   static alive = [];
 
-  constructor(health = 100, name) {
-    this.health = health;
-    this.name = name;
-    Animal.alive.push(this);
+  constructor(name, health = 100) {
+    (this.name = name), (this.health = health), Animal.alive.push(this);
+  }
+
+  checkHealth() {
+    if (this.health <= 0) {
+      Animal.alive = Animal.alive.filter((animal) => animal !== this);
+    }
   }
 }
 
 class Herbivore extends Animal {
-  hidden = false;
-  constructor(health, name) {
-    super();
+  constructor(name) {
+    super(name), (this.hidden = false);
   }
 
   hide() {
@@ -22,19 +29,10 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
-  constructor(health, name) {
-    super();
-  }
-
-  bite(animal) {
-    if (animal instanceof Herbivore && !animal.hidden) {
-      animal.health -= 50;
-    }
-
-    if (animal.health <= 0) {
-      Animal.alive = Animal.alive.filter(
-        (aliveAnimal) => aliveAnimal !== animal,
-      );
+  bite(obj) {
+    if (obj instanceof Herbivore && !obj.hidden) {
+      obj.health -= 50;
+      obj.checkHealth();
     }
   }
 }
