a606cb2910777b8ab5ccbb89cd03a3a1c43168d7
mikezhylka <115790814+mikezhylka@users.noreply.github.com>
2024-06-04 12:51:41 +0200

    'solution'

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,48 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(health = 100, name) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
+
+  die() {
+    Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, hidden, health = 100) {
+    super(hidden);
+    this.hidden = false;
+    this.health = health;
+    this.name = name;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health = 100, type = 'carnivore') {
+    super(health, name);
+    this.health = health;
+    this.type = type;
+  }
+
+  bite(animal) {
+    if (animal.hidden !== true && animal.type !== 'carnivore') {
+      animal.health -= 50;
+    }
+
+    if (animal.health <= 0) {
+      animal.die();
+    }
+  }
 }
 
 module.exports = {
