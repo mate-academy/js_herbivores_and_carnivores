545363d65f7a53ed8ea63fcdb5ff183dcec20f7b
LitviVit <Litv_jn71@student.itstep.org>
2024-06-06 15:06:37 +0300

    task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,49 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
+
+  static removeFromAlive(animal) {
+    const index = Animal.alive.indexOf(animal);
+
+    if (index !== -1) {
+      Animal.alive.splice(index, 1);
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  #hidden = false;
+
+  hide() {
+    this.#hidden = true;
+  }
+
+  get hidden() {
+    return this.#hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (
+      animal instanceof Carnivore ||
+      (animal instanceof Herbivore && animal.hidden)
+    ) {
+      return;
+    }
+    animal.health -= 50;
+
+    if (animal.health <= 0) {
+      Animal.removeFromAlive(animal);
+    }
+  }
 }
 
 module.exports = {
