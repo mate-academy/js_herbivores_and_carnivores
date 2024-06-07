44fca515daa91719bda170af9ad50484e9d7ce2e
Volodymyr Isaiev <isaiev.v.o@gmail.com>
2024-06-08 01:34:10 +0300

    Add animal classes

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,40 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  health = 100;
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    Animal.alive.push(this);
+  }
+
+  static get alive() {
+    return this.alive;
+  }
+  static set alive(aliveAnimals) {
+    this.alive = aliveAnimals;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hidden = false;
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(target) {
+    if (target.hidden === false) {
+      target.health -= 50;
+
+      if (target.health <= 0) {
+        Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
+      }
+    }
+  }
 }
 
 module.exports = {
