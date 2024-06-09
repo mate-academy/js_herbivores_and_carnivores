918cb8d2f825941fd7ab475646cf53bd62fceb41
Mykhailo Shvets <mikhailshvets48@gmail.com>
2024-06-09 17:05:16 +0300

    fixing after code review

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -6,12 +6,16 @@ class Animal {
   constructor(name, health = 100) {
     this.name = name;
     this.health = health;
-    this.hidden = false;
     Animal.alive.push(this);
   }
 }
 
 class Herbivore extends Animal {
+  constructor(name, health = 100) {
+    super(name, health);
+    this.hidden = false;
+  }
+
   hide() {
     this.hidden = true;
   }
