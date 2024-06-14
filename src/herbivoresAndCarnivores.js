dc050e3d5cb96ac78ee3942215a4c3a04f34080d
Stelmashchuk Pavlo <stelmaschukpavlo3@gmail.com>
2024-06-15 00:04:15 +0300

    add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,42 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.health = health;
+    this.name = name;
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animalBited) {
+    if (animalBited instanceof Herbivore && animalBited.hidden === false) {
+      animalBited.health -= 50;
+
+      if (animalBited.health <= 0) {
+        const filtredAnimals = Animal.alive.filter(
+          (animal) => animal !== animalBited,
+        );
+
+        Animal.alive = filtredAnimals;
+      }
+    }
+  }
 }
 
 module.exports = {
