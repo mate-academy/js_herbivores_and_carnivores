f15503c3afdeb0d54090db074ad6d5225c6599fb
Anastasiia Cagle <anastasiiacagle@gmail.com>
2024-06-19 20:09:21 +0300

    add changes

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -29,10 +29,6 @@ class Carnivore extends Animal {
   bite(animal) {
     if (animal instanceof Herbivore && !animal.hidden) {
       animal.health -= 50;
-
-      if (animal.health <= 0) {
-        Animal.removeDeadAnimals();
-      }
     }
   }
 }
