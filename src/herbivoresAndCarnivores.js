e88f349ff31cf9ca01e21ff169963090d7f04661
denys <denis.bielykh38@gmail.com>
2024-06-24 20:43:11 +0300

    solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,55 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  static MIN_HEALTH = 1;
+
+  health = 100;
+
+  static removeFromAlive() {
+    const liveAnimals = this.alive.filter(
+      (item) => item.health > this.MIN_HEALTH,
+    );
+
+    this.alive.length = 0;
+    this.alive.push(...liveAnimals);
+  }
+
+  constructor(name) {
+    this.name = name;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hidden = false;
+
+  constructor(name) {
+    super(name);
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    Animal.alive.push(this);
+  }
+
+  bite(animal) {
+    if (animal instanceof Carnivore || animal.hidden) {
+      return;
+    }
+
+    animal.health -= 50;
+
+    if (animal.health < Animal.MIN_HEALTH) {
+      Animal.removeFromAlive();
+    }
+  }
 }
 
 module.exports = {
