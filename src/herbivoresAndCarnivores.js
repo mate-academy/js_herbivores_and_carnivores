475b21321d4140b5fe2dac0ca155080d9f281bd9
Gennadiy Boreyko <gennadiyboreyko0@gmail.com>
2024-07-02 14:54:41 +0300

    create a Herbivore and a Carnivore class

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,46 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  static VALUE_OF_BITES = 50;
+
+  bite(beast) {
+    if (beast instanceof Carnivore || beast.hidden) {
+      return;
+    } else {
+      beast.health -= Carnivore.VALUE_OF_BITES;
+    }
+
+    if (beast.health <= 0) {
+      const indexBeast = Animal.alive.findIndex((item) => item === beast);
+
+      Animal.alive.splice(indexBeast, 1);
+    }
+
+    return Animal.alive;
+  }
 }
 
 module.exports = {
