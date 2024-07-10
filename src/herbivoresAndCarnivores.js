f5b4fb410a372539c8fcf2e9a1157dbda97c44a3
Nazarii Siryi <aakkrryyttoo99@gmail.com>
2024-07-11 02:55:04 +0300

    fix problem

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -8,6 +8,10 @@ class Animal {
     this.health = health;
     Animal.alive.push(this);
   }
+
+  die() {
+    Animal.alive = Animal.alive.filter((item) => item !== this);
+  }
 }
 
 class Herbivore extends Animal {
@@ -25,7 +29,10 @@ class Carnivore extends Animal {
   bite(animal) {
     if (animal instanceof Herbivore && !animal.hidden) {
       animal.health -= 50;
-      Animal.alive = Animal.alive.filter((item) => item.health > 0);
+    }
+
+    if (animal.health <= 0) {
+      animal.die();
     }
   }
 }
