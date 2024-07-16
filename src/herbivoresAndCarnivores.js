a0c02bbf690acb568d0611c6f161f8718897f83e
Pavlo <oksanakopalko@gmail.com>
2024-07-16 17:05:39 +0300

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -27,21 +27,16 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
-  bite(deer) {
-    if (deer instanceof Herbivore && !deer.hidden) {
-      deer.health -= 50;
+  bite(prey) {
+    if (prey instanceof Herbivore && !prey.hidden) {
+      prey.health -= 50;
+    }
 
-      if (deer.health <= 0) {
-        Animal.filterAnimal();
-      }
+    if (prey.health <= 0) {
+      Animal.filterAnimal();
     }
   }
 }
-
-// const deer = new Herbivore('Bembi');
-// const panther = new Carnivore('Bagira');
-// const lion = new Carnivore('King');
-// const rabbit = new Herbivore('Max');
 
 module.exports = {
   Animal,
