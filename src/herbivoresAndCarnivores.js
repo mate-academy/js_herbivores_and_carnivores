d7e06834684f94299872f0211849cb1fb215c2c0
Arthur <avers.target@gmail.com>
2024-07-17 16:41:27 +0200

    v2

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -29,14 +29,7 @@ class Carnivore extends Animal {
   bite(animal) {
     if (animal instanceof Herbivore && !animal.hidden) {
       animal.health -= 50;
-
-      if (animal.health <= 0) {
-        const index = Animal.alive.indexOf(animal);
-
-        if (index > -1) {
-          Animal.alive.splice(index, 1);
-        }
-      }
+      Animal.alive = Animal.alive.filter(el => el.health > 0);
     }
   }
 }
