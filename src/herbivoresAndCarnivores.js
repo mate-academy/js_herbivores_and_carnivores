a3f216cde7e24cce36845c9b186dbf88a5fe015f
Kirill Gasan <dp160190gkj@gmail.com>
2024-07-20 21:04:47 +0300

    Update Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -12,7 +12,7 @@ class Animal {
     Animal.alive.push(animal);
   }
 
-  static removeAnimal() {
+  static removeAnimals() {
     Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
   }
 }
@@ -36,11 +36,15 @@ class Carnivore extends Animal {
   }
 
   bite(animal) {
+    if (animal instanceof Carnivore) {
+      return;
+    }
+
     if (animal instanceof Herbivore && !animal.hidden) {
       animal.health -= 50;
     }
 
-    Animal.removeAnimal();
+    Animal.removeAnimals();
   }
 }
 
