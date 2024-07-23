da3acf3f3a628484c8f8d15d07dc140624c6ef5a
RimmaBuzuluk <rimmysya2000@gmail.com>
2024-07-23 15:14:26 +0300

    add change from ai

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -28,7 +28,7 @@ class Carnivore extends Animal {
     if (animal instanceof Herbivore && animal.hidden === false) {
       animal.health -= 50;
 
-      if (animal.health === 0) {
+      if (animal.health <= 0) {
         Animal.removeAnimal();
       }
     }
