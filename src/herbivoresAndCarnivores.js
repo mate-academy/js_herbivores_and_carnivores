e595be2616a87ff01713d97b5f6360af93b64f98
sergii-n-test <sergii.n.test@gmail.com>
2024-07-23 13:49:09 +0300

    New new

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,51 +1,40 @@
 'use strict';
 
 class Animal {
+  static alive = [];
+  health = 100;
   constructor(name) {
-    this.health = 100;
     this.name = name;
-
-    Animal.alive.push(this);
-  }
-
-  get isAlive() {
-    return this.health > 0;
-  }
-
-  applyDamage(healthPoints) {
-    this.health -= healthPoints;
-
-    if (this.health <= 0) {
-      Animal.cleanupDeadAnimals();
-    }
   }
 }
 
-Animal.alive = [];
-Animal.cleanupDeadAnimals = () => {
-  Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
-};
-
 class Herbivore extends Animal {
+  hidden = false;
   constructor(name) {
     super(name);
-    
-    this.hidden = false;
+    Animal.alive.push(this);
   }
+
   hide() {
     this.hidden = true;
   }
 }
 
 class Carnivore extends Animal {
-  bite(animal) {
-    if (
-      animal instanceof Herbivore
-      && !animal.hidden
-      && animal.isAlive
-    ) {
-      animal.applyDamage(50);
+  constructor(name) {
+    super(name);
+    Animal.alive.push(this);
+  }
+
+  bite(herbivore) {
+    if (herbivore.hidden === false && !(herbivore instanceof Carnivore)) {
+      herbivore.health -= 50;
     }
+
+    if (herbivore.health === 0) {
+      Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
+    }
+  }
 }
 
 module.exports = {
