be7650dc978e740d2eaa339afe8ef12d431aae9a
sergii-n-test <sergii.n.test@gmail.com>
2024-07-25 11:27:27 +0300

    Edit

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -25,10 +25,6 @@ class Carnivore extends Animal {
   bite(animal) {
     if (animal instanceof Herbivore && !animal.hidden && animal.health > 0) {
       animal.health -= 50;
-
-      if (animal.health <= 0) {
-        Animal.alive = Animal.alive.filter((a) => a.health > 0);
-      }
     }
   }
 }
