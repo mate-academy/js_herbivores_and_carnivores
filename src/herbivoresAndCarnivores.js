e14cf31f3d543c2bad631605f6d4d65d00a0a907
Yarik kedrych <yarik.kedrych9@gmail.com>
2024-07-30 12:10:02 +0300

    add solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,61 @@
 'use strict';
 
+'use strict';
+
 class Animal {
-  // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.hidden = false;
+
+    Animal.alive.push(this);
+  }
+
+  static alive = [];
+
+  isAlive() {
+    return this.health > 0;
+  }
+
+  decreaseHealth(amount) {
+    this.health -= amount;
+
+    if (!this.isAlive()) {
+      const index = Animal.alive.indexOf(this);
+
+      if (index !== -1) {
+        Animal.alive.splice(index, 1);
+      }
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.name = name;
+    this.health = 100;
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.name = name;
+    this.health = 100;
+    this.hidden = false;
+  }
+
+  bite(target) {
+    if (target instanceof Herbivore && !target.hidden) {
+      target.decreaseHealth(50);
+    }
+  }
 }
 
 module.exports = {
