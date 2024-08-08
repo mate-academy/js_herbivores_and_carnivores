172a3603c9bb24ae8222b37b99bf2a9b24a2fc5e
Yevhenii Lutsenko <evgenlutscenko@icloud.com>
2024-08-08 12:21:44 +0200

    done

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,35 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.health = 100;
+    this.name = name;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hidden = false;
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herbivore) {
+    if (!(herbivore instanceof Carnivore || herbivore.hidden === true)) {
+      herbivore.health -= 50;
+
+      if (herbivore.health === 0) {
+        const index = Animal.alive.findIndex((val) => val === herbivore);
+
+        Animal.alive.splice(index, 1);
+      }
+    }
+  }
 }
 
 module.exports = {
