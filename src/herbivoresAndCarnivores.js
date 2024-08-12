1388b1174d69b7e538fd8434ef6ba1c42c4061c7
sinner1993 <sergio1993goga@gmail.com>
2024-08-12 19:46:44 +0200

    1234

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,37 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.health = 100;
+    this.name = name;
+    this.hidden = false;
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+
+  bite(animal) {
+    if (animal instanceof Herbivore && !animal.hidden) {
+      animal.health -= 50;
+    }
+
+    Animal.alive.forEach((classObj, index, arr) => {
+      if (classObj instanceof Herbivore && classObj.health <= 0) {
+        arr.splice(index, 1);
+      }
+    });
+  }
 }
 
 module.exports = {
