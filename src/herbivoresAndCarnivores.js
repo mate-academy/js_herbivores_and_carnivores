41d8f3ba200fd910738612258a3e6bbb956c70e8
sinner1993 <sergio1993goga@gmail.com>
2024-08-13 15:57:05 +0200

    fixed harbivore

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -6,21 +6,23 @@ class Animal {
   constructor(name) {
     this.health = 100;
     this.name = name;
-    this.hidden = false;
+
 
     Animal.alive.push(this);
   }
 }
 
 class Herbivore extends Animal {
-
+  constructor () {
+    super()
+    this.hidden = false;
+  }
   hide() {
     this.hidden = true;
   }
 }
 
 class Carnivore extends Animal {
-
   bite(animal) {
     if (animal instanceof Herbivore && !animal.hidden) {
       animal.health -= 50;
