2381326bea8245317a8848634478e40688364b99
Camille Haus <camille_haus@hotmail.com>
2024-08-28 13:01:41 -0300

    Added part of the logic

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,44 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(health = 100, name) {
+    this.health = health;
+    this.name = name;
+    Animal.alive.push(this);
+  }
+
+  static alive = [];
+
+  die() {
+    Animal.alive = Animal.alive.filter((animal) => animal !== this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(hidden = false, health, name) {
+    super(health, name);
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  // constructor(health) {
+  //   super(health);
+  // }
+
+  bite(herbivore) {
+    if (Herbivore.hidden) {
+      this.health -= 50;
+    } else if (Herbivore.health === 0) {
+      const index = Animal.alive.findIndex(herbivore);
+
+      Animal.alive.splice(index, 1);
+    }
+  }
 }
 
 module.exports = {
