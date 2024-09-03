765dc3c5d4c6267f0ee8dd4c79b63ada93bcba05
Ann Maksay <annaorlova.dn@gmail.com>
2024-09-03 18:32:43 +0300

    feat: animal implementation

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,47 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  static removeFromAlive(creature) {
+    this.alive = this.alive.filter((animal) => animal !== creature);
+  }
+
+  constructor(health = 100, name) {
+    this.health = health;
+    this.name = name;
+
+    if (health > 0) {
+      Animal.alive.push(this);
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(health = 100, name) {
+    super(health, name);
+    this.hidden = true;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(health = 100, name) {
+    super(health, name);
+  }
+
+  bite(herbivore) {
+    if (herbivore instanceof Herbivore && !herbivore.hidden) {
+      herbivore.health -= 50;
+
+      if (herbivore.health <= 0) {
+        Animal.removeFromAlive(herbivore);
+      }
+    }
+  }
 }
 
 module.exports = {
