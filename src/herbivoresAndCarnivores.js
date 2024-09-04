01ecb6cb94abce050b0c2500d269fa529507d3f4
dmytrochernega <dimadneprfdb@gmail.coma>
2024-09-04 19:51:58 +0300

    created

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,39 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health, hidden = false) {
+    super(name, health);
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health = 100, hidden) {
+    super(name, health, hidden);
+  }
+  bite(grass) {
+    if (!grass.hidden && grass instanceof Herbivore) {
+      grass.health -= 50;
+
+      if (grass.health <= 0) {
+        Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
+      }
+    }
+  }
 }
 
 module.exports = {
