681a110289651718e5a8fd8b0f261c0137284b82
Oleh Voloshyn <ithehaski@gmail.com>
2024-09-08 13:42:43 +0300

    refactoring code

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -6,6 +6,7 @@ class Animal {
   constructor(name, health = 100) {
     this.name = name;
     this.health = health;
+    Animal.alive.push(this);
   }
 }
 
@@ -14,7 +15,6 @@ class Herbivore extends Animal {
   constructor(name, health = 100) {
     super(name, health);
     this.hidden = false;
-    Animal.alive.push(this);
   }
   hide() {
     this.hidden = true;
@@ -25,14 +25,15 @@ class Carnivore extends Animal {
   // write your code here
   constructor(name, health = 100) {
     super(name, health);
-    Animal.alive.push(this);
   }
   bite(herbivore) {
     if (!herbivore.hidden && !(herbivore instanceof Carnivore)) {
       herbivore.health -= 50;
     }
 
-    Animal.alive = Animal.alive.filter((el) => el.health > 0);
+    if (herbivore.health <= 0) {
+      Animal.alive = Animal.alive.filter((el) => el.health > 0);
+    }
 
     return herbivore.health;
   }
