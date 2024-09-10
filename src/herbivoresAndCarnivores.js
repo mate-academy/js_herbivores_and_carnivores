c88587915ff7bacf0eb3fb2e291f99b4a2237948
YuriiMykhailenko <urijmihajlenko3@gmail.com>
2024-09-10 21:20:37 +0300

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,13 +1,19 @@
 'use strict';
 
+const DEFAULT_HEALTH = 100;
+const DEFAULT_BITE_STRENGTH = 50;
+
 class Animal {
-  constructor(name) {
+  constructor(name, health = DEFAULT_HEALTH) {
     this.name = name;
-    this.health = 100;
+    this.health = health;
     Animal.alive.push(this);
   }
 
   static alive = [];
+  static updateAliveList() {
+    Animal.alive = Animal.alive.filter((creature) => creature.health > 0);
+  }
 }
 
 class Herbivore extends Animal {
@@ -19,12 +25,21 @@ class Herbivore extends Animal {
 }
 
 class Carnivore extends Animal {
+  constructor(
+    name,
+    health = DEFAULT_HEALTH,
+    biteStrength = DEFAULT_BITE_STRENGTH,
+  ) {
+    super(name, health);
+    this.biteStrength = biteStrength;
+  }
+
   bite(victim) {
     if (victim.hidden === false) {
-      victim.health -= 50;
+      victim.health -= this.biteStrength;
 
       if (victim.health <= 0) {
-        Animal.alive = Animal.alive.filter((creature) => creature.health > 0);
+        Animal.updateAliveList();
       }
     }
   }
