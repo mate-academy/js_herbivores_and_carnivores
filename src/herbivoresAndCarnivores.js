0748941e5b4677f79ade186a2722fb4a01859742
Dmitry <kononenko.dima2001@gmail.com>
2024-09-15 18:36:02 +0300

    solving task

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,38 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  static removeAnimal(animalToRemove) {
+    const animalIndex = Animal.alive.findIndex(
+      (animal) => animal === animalToRemove,
+    );
+
+    Animal.alive.splice(animalIndex, 1);
+  }
+  health = 100;
+  constructor(name) {
+    this.name = name;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hidden = false;
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(target) {
+    if (target instanceof Herbivore && target.hidden !== true) {
+      target.health -= 50;
+
+      if (target.health <= 0) {
+        Animal.removeAnimal(target);
+      }
+    }
+  }
 }
 
 module.exports = {
