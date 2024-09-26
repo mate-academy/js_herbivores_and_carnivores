8b5ca540e703e44731736a79c8103c4b4462b68e
Your_Name Your_Surname <your@email>
2024-09-26 15:05:57 +0300

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,40 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
+
+  static remove() {
+    Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(health, name, hidden = false) {
+    super(health, name);
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(arg) {
+    if (arg instanceof Herbivore && !arg.hidden) {
+      arg.health -= 50;
+
+      if (arg.health <= 0) {
+        Animal.remove();
+      }
+    }
+  }
 }
 
 module.exports = {
