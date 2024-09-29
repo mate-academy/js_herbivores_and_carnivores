a4ebf53f44e0317bb2b79be1e8901a9ad24333e0
an-marryKyslenko <olih.a@icloud.com>
2024-09-29 14:49:14 +0200

    add solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -5,34 +5,35 @@ class Animal {
   constructor(name, health = 100) {
     this.name = name;
     this.health = health;
-
   }
 }
 
 class Herbivore extends Animal {
   hidden = false;
-  constructor(name, health){
+  constructor(name, health) {
     super(name, health);
-    Animal.alive.push(this)
+    Animal.alive.push(this);
   }
   hide() {
     this.hidden = true;
   }
-
 }
 
 class Carnivore extends Animal {
-  constructor(name, health){
+  constructor(name, health) {
     super(name, health);
-    Animal.alive.push(this)
+    Animal.alive.push(this);
   }
   bite(animal) {
-    if(animal.hasOwnProperty('hidden')){
-      if(!animal.hidden) {
+    if (animal.hasOwnProperty('hidden')) {
+      if (!animal.hidden) {
         animal.health -= 50;
       }
-      if(animal.health <= 0) {
-        Animal.alive = Animal.alive.filter((aliveAnimal) => aliveAnimal !== animal);
+
+      if (animal.health <= 0) {
+        Animal.alive = Animal.alive.filter(
+          (aliveAnimal) => aliveAnimal !== animal,
+        );
       }
     }
   }
