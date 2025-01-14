7b457ab3ac7fe2f0663565dc652b0e702b0d1ec9
Flávio Miranda <nascimentoflavio588@hotmail.com>
2025-01-14 08:17:43 -0300

    Add task Herbivores and Carnivores solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,40 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  /**
+   * @param {string} name
+   */
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
+
+  checkHealth() {
+    Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hidden = false;
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  /**
+   * @param {Animal} target
+   */
+  bite(target) {
+    if (target instanceof Herbivore && !target.hidden) {
+      target.health -= 50;
+    }
+    this.checkHealth();
+  }
 }
 
 module.exports = {
