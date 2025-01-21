db82107b9b124d804d11e113b44b9621aa050c41
Velislav Georgiev <velislav.georgiev89@gmail.com>
2025-01-21 18:41:36 +0200

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,47 @@
 'use strict';
 
 class Animal {
+  static alive = [];
   // write your code here
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
+
+  die() {
+    Animal.alive = Animal.alive.filter((animal) => animal !== this);
+  }
 }
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+
+  bite(prey) {
+    if (
+      prey instanceof Herbivore &&
+      !prey.hidden &&
+      Animal.alive.includes(prey)
+    ) {
+      prey.health -= 50;
+
+      if (prey.health <= 0) {
+        prey.die();
+      }
+    }
+  }
 }
 
 module.exports = {
