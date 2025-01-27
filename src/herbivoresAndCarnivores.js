fd3270ba294adbaa3c8bacc4a956d09d4f4e4655
Nadiia Bulmak <nadgaval@gmail.com>
2025-01-27 18:17:32 +0200

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,46 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = []; // Массив с живыми животными
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+
+    Animal.alive.push(this); // Добавляем животное в список живых
+  }
+
+  static updateAlive() {
+    Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  #hidden = false;
+
+  get hidden() {
+    // для чтения значения
+    return this.#hidden;
+  }
+
+  hide() {
+    this.#hidden = true; // метод позволяет травоядным "прятаться"
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(victim) {
+    // Проверяем, является ли жертва хищником или спряталась
+    if (victim instanceof Carnivore || victim.hidden) {
+      // victim.hidden === true
+      return;
+    }
+
+    victim.health -= 50;
+
+    // Обновляем массив живых животных, если жертва умерла
+    Animal.updateAlive();
+  }
 }
 
 module.exports = {
