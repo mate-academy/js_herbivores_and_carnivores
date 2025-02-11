7860846c4a6653d293f9adb537ee57e3a00838f8
g.palyukh <g.palyukh@gmail.com>
2025-02-11 15:11:19 +0100

    Implement Herbivores and Сarnivores task

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,46 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health = 100, hidden = false) {
+    super(name, health);
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+  }
+
+  bite(herbivoreAnimal) {
+    if (
+      herbivoreAnimal instanceof Herbivore &&
+      herbivoreAnimal.hidden === false
+    ) {
+      herbivoreAnimal.health -= 50;
+    }
+
+    if (herbivoreAnimal.health <= 0) {
+      Animal.alive = Animal.alive.filter(
+        (animal) => animal !== herbivoreAnimal,
+      );
+    }
+  }
 }
 
 module.exports = {
