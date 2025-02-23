12832d358e4fafdfa7d9ad66974ae6ee7380b50b
Alona Mutruk <alonamutruk@gmail.com>
2025-02-23 20:48:34 +0200

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,19 +1,41 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hidden = false;
+
+  hide() {
+    this.hidden = true; // Тепер завжди приховуємо травоїдну тварину
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(target) {
+    if (target instanceof Herbivore && target !== this) {
+      // Перевіряємо, що ціль — це травоїдна тварина
+      if (target.hidden) {
+        return; // Якщо травоїдне сховалося — не кусати
+      }
+
+      target.health = Math.max(0, target.health - 50);
+
+      if (target.health === 0) {
+        const filterAlive = (animal) => animal !== target;
+
+        Animal.alive = Animal.alive.filter(filterAlive);
+      }
+    }
+  }
 }
 
-module.exports = {
-  Animal,
-  Herbivore,
-  Carnivore,
-};
+// Експортуємо класи для тестів
+module.exports = { Animal, Herbivore, Carnivore };
