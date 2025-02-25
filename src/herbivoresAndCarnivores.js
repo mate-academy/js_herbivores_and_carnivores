8b1aa2c6edde831f0b05adebe354e9c178cf81dd
Daniil Trusov <daniiltrusov00@gmail.com>
2025-02-25 14:14:40 +0200

    create solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,59 @@
 'use strict';
 
+const MIN_HEALTH = 0;
+const DEFAULT_HEALTH = 100;
+const CARNIVORE_DAMAGE = 50;
+
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = DEFAULT_HEALTH) {
+    this.name = name;
+    this.health = health;
+
+    if (health > MIN_HEALTH) {
+      Animal.alive.push(this);
+    }
+  }
+
+  get health() {
+    return this._health;
+  }
+
+  set health(newValue) {
+    this._health = Math.max(MIN_HEALTH, newValue);
+
+    if (this.health === MIN_HEALTH) {
+      Animal.alive = Animal.alive.filter(
+        (animal) => animal.health > MIN_HEALTH,
+      );
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hidden = false;
+
+  constructor(name, health = DEFAULT_HEALTH) {
+    super(name, health);
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health = DEFAULT_HEALTH) {
+    super(name, health);
+  }
+
+  bite(prey) {
+    if (prey instanceof Carnivore || prey.hidden) {
+      return;
+    }
+    prey.health -= CARNIVORE_DAMAGE;
+  }
 }
 
 module.exports = {
