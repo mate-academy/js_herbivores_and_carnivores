8b2b11031b5bde8f14d6ee952e7dc0e5b136dd38
Iwantschenko <160021860+Iwantschenko@users.noreply.github.com>
2025-02-26 17:39:30 +0200

    Done task

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,48 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  name = '';
+  health = 0;
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+
+    Animal.alive.push(this);
+  }
+
+  static isAlive() {
+    Animal.alive = Animal.alive.filter((item) => item.health !== 0);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hidden;
+
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  static BITE_DMG = 50;
+
+  bite(herbivore) {
+    if (herbivore instanceof Herbivore) {
+      if (!herbivore.hidden) {
+        herbivore.health -= Carnivore.BITE_DMG;
+      }
+
+      Animal.isAlive();
+    }
+  }
 }
 
 module.exports = {
