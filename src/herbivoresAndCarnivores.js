209b89fbb5eefd5549686a78a129c31567232a04
Ivenji <savgenkoe@gmail.com>
2025-03-10 23:07:49 -0400

    code is done

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,48 @@
-'use strict';
+'use strict'
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+      this.name = name;
+      this.health = health;
+      Animal.alive.push(this);
+  }
+
+  die() {
+    console.log(`${this.name} has died.`);
+    Animal.alive = Animal.alive.filter(i => i !== this);
+  }
+
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+      super(name, health);
+      this.hidden = false;
+  }
+
+  hide() {
+      this.hidden = true;
+      console.log(`${this.name} is now hiding.`);
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+      super(name, health);
+  }
+
+  bite(victim) {
+      if (victim instanceof Herbivore && !victim.hidden) {
+          victim.health -= 50;
+          if (victim.health <= 0) {
+            victim.die();
+          }
+      }
+  }
 }
+
 
 module.exports = {
   Animal,
