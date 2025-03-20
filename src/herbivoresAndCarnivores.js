28fd9a0218760dc14fd224b29a440fdd91c6c721
apr1ori <vladgolenok4@gmail.com>
2025-03-20 13:51:13 +0100

    completed task

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,46 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.alive = [];
+
+    Animal.alive.push(this);
+  }
+
+  die() {
+    Animal.alive = Animal.alive.filter((animal) => animal !== this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health, hidden) {
+    super();
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(...args) {
+    super();
+  }
+
+  bite(herb) {
+    if (herb.hidden === false && herb instanceof Herbivore) {
+      herb.health -= 50;
+    }
+
+    if (herb.health <= 0) {
+      herb.die();
+    }
+  }
 }
 
 module.exports = {
