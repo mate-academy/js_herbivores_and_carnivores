1e7e527dfac292a39ef4a2d4bb018c660a317e3a
Pavlenko Kostiantyn <kostiantyn.pavlenko@halliburton.com>
2025-03-25 16:24:23 +0200

    solution for animals task

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,53 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+    this.hidden = false;
+
+    if (this.health > 0) {
+      Animal.alive.push(this);
+    }
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+
+    if (this.health > 0) {
+      Animal.alive.push(this);
+    }
+  }
+
+  bite(animal) {
+    if (animal.__proto__ === Herbivore.prototype && animal.hidden === false) {
+      animal.health -= 50;
+
+      if (animal.health <= 0) {
+        Animal.alive = Animal.alive.reduce((acc, creature) => {
+          if (creature.health > 0) {
+            acc.push(creature);
+          }
+
+          return acc;
+        }, []);
+      }
+    }
+  }
 }
 
 module.exports = {
