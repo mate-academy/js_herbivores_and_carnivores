d41b9ff9014bd56f3c0a8d671ad4577cf14a53ec
Mykhailo Kviatkovskyi <mikhal71@ukr.net>
2025-04-02 21:59:31 +0300

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,41 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
+
+  die() {
+    Animal.alive = Animal.alive.filter((elem) => elem !== this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    return (this.hidden = true);
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(wild) {
+    if (!(wild instanceof Herbivore) || wild.hidden) {
+      return;
+    }
+
+    wild.health -= 50;
+
+    if (wild.health <= 0) {
+      wild.die();
+    }
+  }
 }
 
 module.exports = {
@@ -17,3 +43,45 @@ module.exports = {
   Herbivore,
   Carnivore,
 };
+
+// class Animal {
+//   // write your code here
+//   static alive = [];
+
+//   constructor(name) {
+//     this.name = name;
+//     this.health = 100;
+//     Animal.alive.push(this);
+//   }
+
+//   die() {
+//     Animal.alive = Animal.alive.filter((animal) => animal !== this);
+//   }
+// }
+
+// class Herbivore extends Animal {
+//   // write your code here
+//   constructor(name) {
+//     super(name);
+//     this.hidden = false;
+//   }
+
+//   hide() {
+//     this.hidden = true;
+//   }
+// }
+
+// class Carnivore extends Animal {
+//   // write your code here
+//   bite(target) {
+//     if (!(target instanceof Herbivore) || target.hidden) {
+//       return;
+//     }
+
+//     target.health -= 50;
+
+//     if (target.health <= 0) {
+//       target.die();
+//     }
+//   }
+// }
