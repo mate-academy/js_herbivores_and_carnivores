9c12a7ef94c6d06149782cd6716a0814159f5847
Tetiana Pyatachuk <pyatachuk26@gmail.com>
2025-04-05 17:33:37 +0300

    add solition

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,40 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(health = 100, name) {
+    this.health = health;
+    this.name = name;
+    Animal.alive.push(this); // Додаємо в загальний масив
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health = 100, hidden = false) {
+    super(health, name);
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(health, name);
+  }
+
+  bite(target) {
+    if (target instanceof Herbivore && !target.hidden && target.health > 0) {
+      target.health -= 50; // Зменшуємо здоров'я травоїдного
+    }
+
+    if (target.health <= 0) {
+      Animal.alive = Animal.alive.filter((animal) => animal !== target);
+    }
+  }
 }
 
 module.exports = {
