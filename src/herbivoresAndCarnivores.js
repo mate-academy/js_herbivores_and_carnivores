8b4fb4b5482dbecbc01f5f88203cce1cefda3279
Yaroslav Kostenko <y.kostenko@mate.academy>
2025-04-29 20:06:32 +0300

    Update herbivoresAndCarnivores.js

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,19 +1,46 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.hidden = false;
+
+    Animal.alive.push(this);
+  }
+
+  checkDeath() {
+    if (this.health <= 0) {
+      const index = Animal.alive.indexOf(this);
+      if (index !== -1) {
+        Animal.alive.splice(index, 1);
+      }
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+  }
+
+  bite(herbivore) {
+    if (!(herbivore instanceof Herbivore) || herbivore.hidden) return;
+    herbivore.health -= 50;
+    herbivore.checkDeath();
+  }
 }
 
-module.exports = {
-  Animal,
-  Herbivore,
-  Carnivore,
-};
+module.exports = { Animal, Herbivore, Carnivore };
