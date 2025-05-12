46dd783c0491338548755689bb801f5f66454005
YarRudenko <Yarik18rudenko@gmail.com>
2025-05-12 16:28:02 +0300

    tast solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,47 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    const animalObj = { name: this.name, health: this.health };
+
+    this._ref = animalObj;
+    Animal.alive.push(this);
+  }
+
+  static removeIfDead(instance) {
+    if (instance.health <= 0) {
+      Animal.alive = Animal.alive.filter((a) => a !== instance._ref);
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+    this._ref.hidden = this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (animal instanceof Herbivore && !animal.hidden) {
+      animal.health -= 50;
+
+      if (animal.health <= 0) {
+        Animal.alive = Animal.alive.filter((a) => a !== animal);
+      }
+    }
+  }
 }
 
 module.exports = {
