5114f3f66ed00be958e655328655dd3ba47734ba
Taras <luchakt2@gmail.com>
2025-05-20 10:59:39 +0300

    end of work

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,33 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health) {
+    if (typeof name !== 'string' || name.trim().length === 0) {
+      throw new Error('Invalid name');
+    }
+
+    this.health = Number.isFinite(health) ? health : 100;
+    this.name = name;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health, hidden = false) {
+    super(name, health);
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herbivore) {
+    if (herbivore instanceof Herbivore && !herbivore.hidden) {
+      herbivore.health -= 50;
+    }
+  }
 }
 
 module.exports = {
