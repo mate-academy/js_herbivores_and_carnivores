558d67255479870837c52b7b63fee3f7ce701e60
dopezinho <opugnero@gmail.com>
2025-06-30 19:29:58 -0300

    added solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,61 @@
+/* eslint-disable no-console */
+/* eslint-disable no-useless-constructor */
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
+
+  die() {
+    const index = Animal.alive.indexOf(this);
+
+    if (index > -1) {
+      Animal.alive.splice(index, 1);
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+    console.log(`${this.name} is now hiding.`);
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+  }
+
+  bite(target) {
+    if (target instanceof Herbivore && !target.hidden) {
+      console.log(`${this.name} bites ${target.name}.`);
+      target.health -= 50;
+
+      if (target.health <= 0) {
+        console.log(`${target.name} has died.`);
+        target.die();
+      }
+    } else if (target instanceof Carnivore) {
+      console.log(
+        `${this.name} cannot bite another carnivore, ${target.name}.`,
+      );
+    } else if (target.hidden) {
+      console.log(
+        `${this.name} cannot see ${target.name} because it is hiding.`,
+      );
+    }
+  }
 }
 
 module.exports = {
