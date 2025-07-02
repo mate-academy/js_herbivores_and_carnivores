848af12105285b8acaa1e6eda1b643e3d924ac1a
michelam-pseudo <michelammilian@outlook.com>
2025-07-02 01:26:06 -0300

    Resolução do exercício

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,49 @@
 
 class Animal {
   // write your code here
+  static alive = [];
+  health = 100;
+  name;
+
+  constructor(name) {
+    this.name = name;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
   // write your code here
+  hidden;
+
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  constructor(name) {
+    super(name);
+  }
+
+  bite(target) {
+    if (target instanceof Carnivore || target.hidden === true) {
+      return;
+    }
+
+    target.health = target.health - 50;
+
+    if (target.health === 0) {
+      const indexDead = Animal.alive.indexOf(target);
+
+      delete Animal.alive[indexDead];
+    }
+  }
 }
 
 module.exports = {
