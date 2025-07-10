0efd70e6e400e54bfa44c84360568fe466aea4ba
mattc108 <matthewcamacho108@gmail.com>
2025-07-10 17:30:31 -0500

    solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,50 @@
 
 class Animal {
   // write your code here
+  static alive = [];
+  health = 100;
+
+  constructor() {
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
   // write your code here
+  hidden = false;
+
+  constructor(name) {
+    super();
+    this.name = name;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
+
+  unhide() {
+    this.hidden = false;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  constructor(name) {
+    super();
+    this.name = name;
+  }
+
+  bite(animal) {
+    if (animal instanceof Herbivore) {
+      if (!animal.hidden) {
+        animal.health -= 50;
+
+        if (animal.health <= 0) {
+          Animal.alive = Animal.alive.filter((a) => a !== animal);
+        }
+      }
+    }
+  }
 }
 
 module.exports = {
