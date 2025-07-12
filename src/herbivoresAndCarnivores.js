d18a2e2e3d806251a776861e5931cb9aa68e77bc
galag <galagan.evgeniy@gmail.com>
2025-07-12 18:01:18 +0300

    'Fixes'

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -15,7 +15,7 @@ class Herbivore extends Animal {
     Animal.alive.push(this);
   }
   hide() {
-    this.hidden = !this.hidden;
+    this.hidden = true;
   }
 }
 
@@ -27,14 +27,7 @@ class Carnivore extends Animal {
   bite(herbivore) {
     if (herbivore instanceof Herbivore && herbivore.hidden === false) {
       herbivore.health = herbivore.health - 50;
-
-      if (herbivore.health <= 0) {
-        const index = Animal.alive.indexOf(herbivore);
-
-        if (index > -1) {
-          Animal.alive.splice(index, 1);
-        }
-      }
+      Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
     }
   }
 }
