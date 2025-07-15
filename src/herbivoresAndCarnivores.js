cecbd09fc54f33146322e241e7026fe811195d41
Julio Vergueiro <89145136+Juliovergueiro@users.noreply.github.com>
2025-07-15 09:17:54 -0300

    Comment

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -23,6 +23,12 @@ class Carnivore extends Animal {
   bite(victim) {
     if (victim instanceof Herbivore && victim.hidden === false) {
       victim.health -= 50;
+
+      if (victim.health === 0) {
+        const index = Animal.alive.indexOf(victim);
+
+        Animal.alive.splice(index, 1);
+      }
     }
   }
 }
