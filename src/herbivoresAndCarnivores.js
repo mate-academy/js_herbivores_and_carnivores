5765201b56f9a66614a6372de5460518a31b087f
Mykyta Musaiev <n.musaiev@gmail.com>
2025-07-16 16:42:18 +0300

    fixed

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -22,17 +22,11 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(target) {
-    if (target instanceof Carnivore || target.hidden) {
-      return;
-    }
+    if (target instanceof Herbivore && !target.hidden) {
+      target.health -= 50;
 
-    target.health -= 50;
-
-    if (target.health <= 0) {
-      const index = Animal.alive.findIndex((animal) => animal === target);
-
-      if (index !== -1) {
-        Animal.alive.splice(index, 1);
+      if (target.health <= 0) {
+        Animal.alive = Animal.alive.filter((animal) => animal !== target);
       }
     }
   }
