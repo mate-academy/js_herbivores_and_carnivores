70d8623522ca0e8a644e064d5fac1113d90712ce
Kateryna <kate.shyndina@gmail.com>
2025-07-21 23:02:12 +0300

    added solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,39 @@
 
 class Animal {
   // write your code here
+  health = 100;
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
   // write your code here
+
+  hidden = false;
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+
+  bite(herb) {
+    if (herb.hidden === false) {
+      herb.health -= 50;
+    }
+
+    if (herb.health <= 0) {
+      const kill = Animal.alive.indexOf(herb);
+
+      delete Animal.alive[kill];
+    }
+  }
 }
 
 module.exports = {
