1c9ee07f352cb315a263805c4011dcaa97dfa1be
dopezinho <opugnero@gmail.com>
2025-07-31 11:43:10 -0300

    solution added

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,59 +1,46 @@
-/* eslint-disable no-console */
-/* eslint-disable no-useless-constructor */
 'use strict';
 
 class Animal {
-  static alive = [];
-
-  constructor(name) {
+  // write your code here
+  constructor(name, health = 100) {
+    this.health = health;
     this.name = name;
-    this.health = 100;
     Animal.alive.push(this);
   }
 
-  die() {
-    const index = Animal.alive.indexOf(this);
+  static alive = [];
 
-    if (index > -1) {
-      Animal.alive.splice(index, 1);
-    }
+  die() {
+    Animal.alive = Animal.alive.filter((animal) => animal !== this);
   }
 }
 
 class Herbivore extends Animal {
+  // write your code here
   constructor(name) {
     super(name);
     this.hidden = false;
   }
-
   hide() {
     this.hidden = true;
-    console.log(`${this.name} is now hiding.`);
   }
 }
 
 class Carnivore extends Animal {
-  constructor(name) {
-    super(name);
-  }
+  // write your code here
 
-  bite(target) {
-    if (target instanceof Herbivore && !target.hidden) {
-      console.log(`${this.name} bites ${target.name}.`);
-      target.health -= 50;
+  bite(victim) {
+    if (!(victim instanceof Herbivore)) {
+      return;
+    }
 
-      if (target.health <= 0) {
-        console.log(`${target.name} has died.`);
-        target.die();
-      }
-    } else if (target instanceof Carnivore) {
-      console.log(
-        `${this.name} cannot bite another carnivore, ${target.name}.`,
-      );
-    } else if (target.hidden) {
-      console.log(
-        `${this.name} cannot see ${target.name} because it is hiding.`,
-      );
+    if (victim.hidden) {
+      return;
+    }
+    victim.health -= 50;
+
+    if (victim.health <= 0) {
+      victim.die();
     }
   }
 }
