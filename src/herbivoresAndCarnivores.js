cc3cff1086081870f610eddc9aed47c7a3d83bf7
Tetiana Susol <tetyanamyeskalo@gmail.com>
2025-08-10 19:42:21 +0300

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,41 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name = "Unnamed") {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
+
+  // Метод для перевірки, чи тварина жива
+  static removeIfDead(animal) {
+    if (animal.health <= 0) {
+      Animal.alive = Animal.alive.filter(a => a !== animal);
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(target) {
+    // Може кусати тільки травоїдних і тільки якщо вони не ховаються
+    if (target instanceof Herbivore && !target.hidden) {
+      target.health -= 50;
+      Animal.removeIfDead(target);
+    }
+  }
 }
 
 module.exports = {
