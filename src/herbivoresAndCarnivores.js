4dc8d25920f0efbc030450232bc447eeaa2b6e43
Olivio Neves <olivioservio@gmail.com>
2025-08-18 18:37:54 -0300

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,48 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    // adiciona no array de vivos
+    Animal.alive.push(this);
+  }
+
+  die() {
+    // remove apenas esta instância de alive
+    const idx = Animal.alive.indexOf(this);
+
+    if (idx !== -1) {
+      Animal.alive.splice(idx, 1);
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    // só morde herbívoros não escondidos
+    if (animal instanceof Herbivore && !animal.hidden) {
+      animal.health -= 50;
+
+      if (animal.health <= 0) {
+        animal.die();
+      }
+    }
+  }
 }
 
 module.exports = {
