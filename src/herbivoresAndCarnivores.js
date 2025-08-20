6976327ec8e426d08ea567ef5526802832f6290c
Demetrio dos Anjos <ddoanjos@gmail.com>
2025-08-19 21:28:50 -0300

    feat: Add Animal, Herbivore, and Carnivore classes

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,43 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
+
+  isDead() {
+    return this.health <= 0;
+  }
+
+  checkStatus() {
+    if (this.isDead()) {
+      Animal.alive = Animal.alive.filter((animal) => animal !== this);
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herbivore) {
+    if (herbivore instanceof Herbivore && !herbivore.hidden) {
+      herbivore.health -= 50;
+      herbivore.checkStatus();
+    }
+  }
 }
 
 module.exports = {
