1221005ca9f3cfc4992067f63f574890fe5f3c17
DropPredator <ionchiccc@gmail.com>
2025-08-22 11:42:17 +0300

    task is ready

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,42 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  health = 100;
+
+  constructor(name) {
+    this.name = name;
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hidden = false;
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal = {}) {
+    if (animal instanceof Herbivore && animal.hidden === false) {
+      animal.health -= 50;
+
+      if (animal.health <= 0) {
+        const index = Animal.alive.findIndex((beast) => {
+          if (animal === beast) {
+            return true;
+          }
+
+          return false;
+        });
+
+        Animal.alive.splice(index, 1);
+      }
+    }
+  }
 }
 
 module.exports = {
