46337d87e80cd111786be418f4ca4b4176645127
Olga Korynenko <olga.korynenko@gmail.com>
2025-08-31 23:20:53 +0400

    create an animal

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,47 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(health = 100, name) {
+    this.health = health;
+    this.name = name;
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(health = 100, name) {
+    super(health, name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(health = 100, name) {
+    super(health, name);
+  }
+
+  bite(prey) {
+    if (prey instanceof Herbivore && !prey.hidden) {
+      prey.health -= 50;
+    } else if (prey instanceof Carnivore) {
+      return;
+    }
+
+    if (prey.health <= 0) {
+      const preyIndex = Animal.alive.indexOf(prey);
+
+      if (preyIndex > -1) {
+        Animal.alive.splice(preyIndex, 1);
+      }
+    }
+  }
 }
 
 module.exports = {
