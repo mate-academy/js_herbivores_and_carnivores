bd20dbbd3b55a0c01e16cd451d74c8ead013fc64
A-Melnykk <969696245n@gmail.com>
2025-09-02 08:55:47 +0300

    add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -14,13 +14,7 @@ class Animal {
   }
 
   checkAlive() {
-    if (this.health <= 0) {
-      const index = Animal.alive.indexOf(this);
-
-      if (index !== -1) {
-        Animal.alive.splice(index, 1);
-      }
-    }
+    Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
   }
 }
 
