980df3bc1ab2b596e8f3fedf8548cb2acc6fee1b
Alex Mirror <example@dot.com>
2025-09-03 17:23:51 +0200

    Solution1

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -18,7 +18,7 @@ class Herbivore extends Animal {
   }
 
   hide() {
-    this.hidden = !this.hidden;
+    this.hidden = true;
   }
 }
 
@@ -31,14 +31,7 @@ class Carnivore extends Animal {
   bite(target) {
     if (target instanceof Herbivore && !target.hidden) {
       target.health -= 50;
-
-      if (target.health <= 0) {
-        const index = Animal.alive.indexOf(target);
-
-        if (index > -1) {
-          Animal.alive.splice(index, 1);
-        }
-      }
+      Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
     }
   }
 }
