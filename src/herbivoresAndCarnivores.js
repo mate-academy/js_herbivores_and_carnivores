f493151644af79e41dc7715207659118300e54b3
Zoryana Fedorovska <zfedorovska@gmail.com>
2025-09-09 19:03:55 +0300

    added code for Animal

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,51 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
+
+  static _removeFromAlive(instance) {
+    const i = Animal.alive.indexOf(instance);
+
+    if (i !== -1) {
+      Animal.alive.splice(i, 1);
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(target) {
+    // працює лише для травоїдних, які не ховаються
+    if (!(target instanceof Herbivore)) {
+      return;
+    }
+
+    if (target.hidden) {
+      return;
+    }
+
+    target.health -= 50;
+
+    if (target.health <= 0) {
+      Animal._removeFromAlive(target);
+    }
+  }
 }
 
 module.exports = {
