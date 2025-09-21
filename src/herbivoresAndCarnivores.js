fb959b8a4abe69863d1d5683315a62f166aee95c
Oleksiy Rozsokha <xanderiooo21@gmail.com>
2025-09-22 01:50:24 +0300

    Complete herbivoresAndCarnivores task

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,49 @@
 'use strict';
 
 class Animal {
-  // write your code here
-}
+  static alive = [];
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
+  alive() {
+    return this.alive;
+  }
+  die() {
+    Animal.alive = Animal.alive.filter((animal) => animal !== this);
 
+    // const index = Animal.alive.indexOf(victim);
+
+    // if (index !== -1) {
+    //   Animal.alive.splice(index, 1);
+    // }
+  }
+}
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+  }
+  hide() {
+    this.hidden = true;
+  }
 }
-
 class Carnivore extends Animal {
-  // write your code here
+  // eslint-disable-next-line no-useless-constructor
+  constructor(name, health) {
+    super(name, health);
+  }
+
+  bite(victim) {
+    if (victim instanceof Herbivore && !victim.hidden) {
+      victim.health -= 50;
+    }
+
+    if (victim.health <= 0) {
+      victim.die();
+    }
+  }
 }
 
 module.exports = {
