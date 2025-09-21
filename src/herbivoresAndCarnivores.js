1c22f8f2b3019685651158ca9f2e7cf15498a4b8
Mykyta <yakubamykyta@gmail.com>
2025-09-21 12:36:56 +0300

    solve task

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,37 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  health = 100;
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    Animal.alive.push(this);
+  }
+
+  static die(animal) {
+    Animal.alive.splice(Animal.alive.indexOf(animal), 1);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hidden = false;
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (animal instanceof Herbivore && !animal.hidden) {
+      animal.health -= 50;
+
+      if (animal.health <= 0) {
+        Animal.die(animal);
+      }
+    }
+  }
 }
 
 module.exports = {
