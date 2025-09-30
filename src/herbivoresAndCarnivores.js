5abe36fe2291dd494e819284f374e89006046164
Matheus Sousa <theus.sousaa@gmail.com>
2025-09-29 22:09:34 -0300

    finished project

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,40 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, hidden = false) {
+    super(name);
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herb) {
+    if (herb instanceof Herbivore && herb.hidden === false) {
+      herb.health -= 50;
+    }
+
+    if (herb.health <= 0) {
+      const killAnimal = Animal.alive.indexOf(herb);
+
+      if (killAnimal > -1) {
+        Animal.alive.splice(killAnimal, 1);
+      }
+    }
+  }
 }
 
 module.exports = {
