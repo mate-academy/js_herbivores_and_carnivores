4cde3eec8fa06eae82fed1f952eeb9d46cfa0f75
Boris Mund <boris17527684@gmail.com>
2025-10-03 20:29:08 +0300

    new classes

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,35 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herbivore) {
+    if (herbivore instanceof Herbivore && !herbivore.hidden) {
+      herbivore.health -= 50;
+    }
+    Animal.alive = Animal.alive.filter((anim) => anim.health > 0);
+  }
 }
+
+module.exports = {
+  Animal,
+  Herbivore,
+  Carnivore,
+};
 
 module.exports = {
   Animal,
