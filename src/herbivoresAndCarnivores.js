740acb49e5616ded71b0fdcca53ac1987ffe0b4a
Mariia <proninamaria2002@gmail.com>
2025-10-06 11:51:48 +0300

    Added herbivoresAndCarnivores solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,43 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
+
+  die() {
+    Animal.alive = Animal.alive.filter(a => a !== this);
+  }
+
+  set health(value) {
+    this._health = value;
+    if (this._health <= 0) {
+      this.die();
+    }
+  }
+
+  get health() {
+    return this._health;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(victim) {
+    if (!(victim instanceof Herbivore)) return;
+    if (victim.hidden) return;
+
+    victim.health -= 50;
+  }
 }
 
 module.exports = {
