c306000a6fed1541e159cb3beeca10bbe4e244e5
Alla Karakaliuk <alla.uriivna@gmail.com>
2025-10-10 01:29:52 +0400

    fixed critical specification and checklist violations

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -3,16 +3,16 @@
 class Animal {
   static alive = [];
 
-  constructor(name, health, alive) {
+  constructor(name, health = 100) {
     this.name = name;
-    this.health = 100;
+    this.health = health;
     Animal.alive.push(this);
   }
 }
 
 class Herbivore extends Animal {
-  constructor(name, hidden, alive) {
-    super(name, alive);
+  constructor(name) {
+    super(name);
     this.hidden = false;
   }
 
@@ -30,11 +30,7 @@ class Carnivore extends Animal {
     target.health -= 50;
 
     if (target.health <= 0) {
-      const index = Animal.alive.indexOf(target);
-
-      if (index !== -1) {
-        Animal.alive.splice(index, 1);
-      }
+      Animal.alive = Animal.alive.filter((a) => a.health > 0);
     }
   }
 }
