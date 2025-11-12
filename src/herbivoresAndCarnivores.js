cf56a37cf4c718bc647d91c41ea194346d2020ab
horbenkoOleksandr <horbenko.oleksandr.v@gmail.com>
2025-11-12 02:14:34 +0200

    Add codes

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,47 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+
+    Animal.alive.push(this);
+  }
+
+  checkAlive() {
+    if (this.health <= 0) {
+      const index = Animal.alive.indexOf(this);
+
+      if (index > -1) {
+        Animal.alive.splice(index, 1);
+      }
+    }
+  }
+
+  getreduceHealth(numberReducedHealth) {
+    this.health -= numberReducedHealth;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(herbivoreObj) {
+    if (herbivoreObj instanceof Herbivore && !herbivoreObj.hidden) {
+      herbivoreObj.getreduceHealth(50);
+      herbivoreObj.checkAlive();
+    }
+  }
 }
 
 module.exports = {
