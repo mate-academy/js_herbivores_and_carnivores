cc46711157db62e950a3d8e0aac085f97b6051f4
Aleks Pashkevych <alekspashkevych@gmail.com>
2025-11-12 19:34:07 +0200

    created the first version of the task, need to analize

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,46 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  constructor(name = `Not Given`, health = 100) {
+    this.name = name;
+    this.health = health;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+
+    if (this.health > 0) {
+      Animal.alive.push(this);
+    }
+  }
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+
+    if (this.health > 0 && !Animal.alive.includes(this)) {
+      Animal.alive.push(this);
+    }
+  }
+  bite(toBite) {
+    if (toBite !== 0 && toBite.health !== undefined) {
+      toBite.health -= 50;
+    }
+
+    if (this.health <= 0) {
+      const ind = Animal.alive.indexOf(this);
+
+      Animal.alive.splice(ind, 1);
+    }
+  }
 }
 
 module.exports = {
