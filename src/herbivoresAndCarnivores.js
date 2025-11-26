29d044cbf0011bca683aec430df162f46ab52d10
Tymofii <zrozik@MacBook-Air-Tymofii.local>
2025-11-26 13:58:54 +0200

    commit fixes

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -17,7 +17,7 @@ class Animal {
     this._health = value;
 
     if (value <= 0) {
-      const index = Animal.alive.indexOf(this);
+      const index = Animal.alive.filter((animal) => animal !== this);
 
       if (index !== -1) {
         Animal.alive.splice(index, 1);
