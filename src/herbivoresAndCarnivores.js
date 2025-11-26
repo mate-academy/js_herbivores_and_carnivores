d389a0aa0009b2b336e67f5809e7a219f9efb998
Tymofii <zrozik@MacBook-Air-Tymofii.local>
2025-11-26 13:53:02 +0200

    fix

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,16 +1,48 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this._health = 0;
+    this.hidden = false;
+
+    this.health = health;
+
+    Animal.alive.push(this);
+  }
+
+  set health(value) {
+    this._health = value;
+
+    if (value <= 0) {
+      const index = Animal.alive.indexOf(this);
+
+      if (index !== -1) {
+        Animal.alive.splice(index, 1);
+      }
+    }
+  }
+
+  get health() {
+    return this._health;
+  }
 }
 
-class Herbivore extends Animal {
-  // write your code here
-}
+class Herbivore extends Animal {}
 
-class Carnivore extends Animal {
-  // write your code here
-}
+Herbivore.prototype.hide = function () {
+  this.hidden = true;
+};
+
+class Carnivore extends Animal {}
+
+Carnivore.prototype.bite = function (animal) {
+  if (animal instanceof Herbivore && !animal.hidden) {
+    animal.health -= 50;
+  }
+};
 
 module.exports = {
   Animal,
