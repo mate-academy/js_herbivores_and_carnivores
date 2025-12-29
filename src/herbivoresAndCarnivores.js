c9e2e5942cacd29bf58380394d6cd8b7bd0e04c4
Milana <your@email.com>
2025-12-29 21:29:36 +0200

    Solution2

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -12,11 +12,7 @@ class Animal {
 
   checkHealth() {
     if (this.health <= 0) {
-      const index = Animal.alive.indexOf(this);
-
-      if (index !== -1) {
-        Animal.alive.splice(index, 1);
-      }
+      Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
     }
   }
 }
