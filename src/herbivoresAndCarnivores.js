dd1700049782898b245c75bfe19f88f5d0600e0e
akhvarastyan-web <a.khvarastyan@gmail.com>
2025-12-29 21:06:26 +0200

    second

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -11,11 +11,7 @@ class Animal {
 
   checkVitality() {
     if (this.health <= 0) {
-      const index = Animal.alive.indexOf(this);
-
-      if (index !== -1) {
-        Animal.alive.splice(index, 1);
-      }
+      Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
     }
   }
 }
