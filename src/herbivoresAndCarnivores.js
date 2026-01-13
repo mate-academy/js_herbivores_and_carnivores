39c3870b95af0255962ca6bbe6290e9d400dd29a
Mykyta Oliinyk <mykyta.oliinyk0@gmail.com>
2026-01-13 18:33:53 +0100

    feat: add solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,41 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor() {
+    this.health = 100;
+    this.name = '';
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor() {
+    super();
+    this.hidden = false;
+    Animal.alive.push(this);
+  }
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor() {
+    super();
+    Animal.alive.push(this);
+  }
+
+  bite(object) {
+    if (object instanceof Carnivore || object.hidden === true) {
+      return;
+    }
+    object.health -= 50;
+
+    if (object.health <= 0) {
+      Animal.alive.splice(Animal.alive.indexOf(object), 1);
+    }
+  }
 }
 
 module.exports = {
