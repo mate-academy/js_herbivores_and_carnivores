ee5fa7960a34e083270fbe441df11e84be62c32f
Yurii Buriak <yuriiburiak8@gmail.com>
2026-01-27 21:08:49 +0100

    Remove dead animals from Animal.alive using filter

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -30,11 +30,7 @@ class Carnivore extends Animal {
     target.health -= 50;
 
     if (target.health <= 0) {
-      const index = Animal.alive.indexOf(target);
-
-      if (index !== -1) {
-        Animal.alive.splice(index, 1);
-      }
+      Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
     }
   }
 }
