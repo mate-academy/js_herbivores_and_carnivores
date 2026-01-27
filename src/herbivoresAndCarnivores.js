893ed418c941e7d22f2fc63935a643080820d4f5
Yurii Buriak <yuriiburiak8@gmail.com>
2026-01-27 20:21:13 +0100

    Add Animal base class with static alive registry

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,20 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+
 }
 
 class Carnivore extends Animal {
-  // write your code here
+
 }
 
 module.exports = {
