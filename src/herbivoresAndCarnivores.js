1c464a2f534a61489f82319f26d280364d4a603e
Natalia Panchuk <Foreverdanda@ukr.net>
2026-01-28 18:25:28 +0200

    add task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,43 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = 100, hidden) {
+    this.name = name;
+    this.health = health;
+    this.hidden = hidden || false;
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health = 100, hidden) {
+    super(name, health, hidden);
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health = 100, hidden) {
+    super(name, health, hidden);
+  }
+
+  bite(target) {
+    if (target instanceof Carnivore || target.hidden) {
+      return;
+    }
+
+    target.health -= 50;
+
+    if (target.health <= 0) {
+      Animal.alive = Animal.alive.filter((el) => el !== target);
+    }
+  }
 }
 
 module.exports = {
