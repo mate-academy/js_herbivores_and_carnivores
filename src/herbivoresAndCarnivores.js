264a16d9a2334b77618efa2bc7cb044adac94fd5
Yevhen Serdiuk <yevhen.srdk@gmail.com>
2026-02-02 21:50:49 +0200

    Replaced the splice method with filter

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -32,9 +32,7 @@ class Carnivore extends Animal {
     if (herbivore.hidden === false && 'hide' in herbivore) {
       herbivore.health -= 50;
 
-      if (herbivore.health <= 0) {
-        Animal.alive.splice(Animal.alive.indexOf(herbivore), 1);
-      }
+      Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
     }
   }
 }
