b6e276250e0a0e8836fecca2d5f29346bb6eb764
Sofiia Lohai <logay1810@gmail.com>
2026-02-23 13:47:29 +0100

    Task solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,45 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    Animal.alive.push(this);
+  }
+
+  die() {
+    const i = Animal.alive.indexOf(this);
+
+    if (i !== -1) {
+      Animal.alive.splice(i, 1);
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (animal instanceof Herbivore && animal.hidden !== true) {
+      animal.health -= 50;
+    }
+
+    if (animal.health <= 0) {
+      animal.die();
+    }
+  }
 }
 
 module.exports = {
