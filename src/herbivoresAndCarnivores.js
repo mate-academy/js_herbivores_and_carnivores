7308cd2c340e22e133ac7689e197f8800ecfe1b7
Tiago <o-seu-email-do-github@exemplo.com>
2026-02-23 12:11:36 +0000

    feat: implement Herbivores and Carnivores with ES6 Classes

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,39 @@
 
 class Animal {
   // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+
+  bite(alvo) {
+    if (alvo instanceof Herbivore && !alvo.hidden) {
+      alvo.health -= 50;
+
+      if (alvo.health <= 0) {
+        Animal.alive = Animal.alive.filter((animal) => animal !== alvo);
+      }
+    }
+  }
 }
 
 module.exports = {
