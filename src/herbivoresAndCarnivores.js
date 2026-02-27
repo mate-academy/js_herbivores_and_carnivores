71d31f2230fe574e6e8a1342f5f663be2abbac45
Ola <aleksandra-chomicz@outlook.com>
2026-02-27 11:25:32 +0100

    task completed

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,44 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health) {
+    this.name = name;
+    this.health = health || 100;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(health, name) {
+    super(health, name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(target) {
+    const targetIndex = Animal.alive.indexOf(target);
+
+    if (!(target instanceof Herbivore)) {
+      return;
+    }
+
+    if (target.hidden) {
+      return;
+    }
+
+    Animal.alive[targetIndex].health -= 50;
+
+    if (Animal.alive[targetIndex].health <= 0) {
+      Animal.alive.splice(targetIndex, 1);
+    }
+  }
 }
 
 module.exports = {
