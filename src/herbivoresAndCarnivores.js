856f451565164d4fae1102e3724965aa9f371325
Natalia Lunkevych <gadyka08@gmail.com>
2026-03-13 18:10:43 +0200

    fixed

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,50 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health) {
+    this.name = name;
+    this.health = health || 100;
+    Animal.alive.push(this);
+  }
+  checkLifeStatus() {
+    if (this.health <= 0) {
+      // Шукаємо, під яким номером (індексом) ця тварина в списку
+      const index = Animal.alive.indexOf(this);
+      // Якщо знайшли (індекс не -1), то видаляємо 1 елемент за цим індексом
+
+      if (index !== -1) {
+        Animal.alive.splice(index, 1);
+      }
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  cconstructor(name, health = 100, hidden = false) {
+    super(name, health);
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name);
+    this.health = health;
+  }
+
+  bite(target) {
+    if (target instanceof Carnivore || target.hidden) {
+      return;
+    }
+    target.health -= 50;
+    target.checkLifeStatus();
+  }
 }
 
 module.exports = {
