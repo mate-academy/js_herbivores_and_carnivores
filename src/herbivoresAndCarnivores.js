f12ecb38ab9d2cf1ea439c0d74fa91e067f78e1b
linska <linskatanya@gmail.com>
2026-04-20 09:21:23 +0300

    feat: implement animal hierarchy with herbivores and carnivores

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -19,11 +19,7 @@ class Animal {
   }
 
   die() {
-    const index = Animal.alive.indexOf(this);
-
-    if (index !== -1) {
-      Animal.alive.splice(index, 1);
-    }
+    Animal.alive = Animal.alive.filter((a) => a !== this);
   }
 }
 
