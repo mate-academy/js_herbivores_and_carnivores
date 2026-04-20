9b63093920e9f98fd17716dfebddd2a0c59ab619
Inna K <innakonopatcka@gmail.com>
2026-04-20 15:35:31 +0300

    completed all tasks

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,49 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    Animal.alive.push(this);
+  }
+
+  die() {
+    Animal.alive = Animal.alive.filter((animal) => animal !== this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(target) {
+    // не можна кусати хижаків
+    if (!(target instanceof Herbivore)) {
+      return;
+    }
+
+    // не можна кусати, якщо сховався
+    if (target.hidden) {
+      return;
+    }
+
+    target.health -= 50;
+
+    if (target.health <= 0) {
+      target.die();
+    }
+  }
 }
 
 module.exports = {
