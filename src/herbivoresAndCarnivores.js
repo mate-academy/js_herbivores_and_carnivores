86c52033ad3df0884049ef9c80743d32ebe0e9f8
Artem <139005232+CyborgNinjaHat@users.noreply.github.com>
2026-04-20 19:48:33 +0000

    feat: implement Animal, Herbivore, and Carnivore classes.

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,68 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static #alive = [];
+
+  static get alive() {
+    return [...Animal.#alive];
+  }
+
+  #health;
+  #name;
+
+  constructor(name, health = 100) {
+    this.#name = name;
+
+    this.#health = Math.max(0, health);
+
+    if (this.#health > 0) {
+      Animal.#alive.push(this);
+    }
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get health() {
+    return this.#health;
+  }
+
+  set health(value) {
+    this.#health = Math.max(0, value);
+
+    if (this.#health === 0) {
+      Animal.#alive = Animal.#alive.filter((animal) => animal !== this);
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  #hidden = false;
+
+  get hidden() {
+    return this.#hidden;
+  }
+
+  set hidden(state) {
+    this.#hidden = state;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(target) {
+    if (target.hidden || !(target instanceof Herbivore)) {
+      return false;
+    }
+
+    target.health -= 50;
+
+    return true;
+  }
 }
 
 module.exports = {
