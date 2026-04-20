96c7140b0401c68366b34046b69b877a068c9b40
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2026-04-20 14:26:38 +0300

    finish

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,47 @@
 'use strict';
-
 class Animal {
-  // write your code here
-}
+  static alive = [];
 
+  constructor(name, health = 100) {
+    this._health = health;
+    this.name = name;
+    Animal.alive.push(this);
+  }
+
+  get health() {
+    return this._health;
+  }
+
+  set health(newHealth) {
+    this._health = newHealth;
+
+    if (this._health <= 0) {
+      Animal.alive = Animal.alive.filter((animal) => animal !== this);
+    }
+  }
+}
 class Herbivore extends Animal {
-  // write your code here
-}
+  constructor(health = 100, name, hidden) {
+    super(health, name);
+    this.hidden = false;
+  }
 
+  hide() {
+    this.hidden = true;
+  }
+}
 class Carnivore extends Animal {
-  // write your code here
+  constructor(health = 100, name) {
+    super(health, name);
+  }
+
+  bite(herbivore) {
+    if (herbivore.hidden === true || herbivore instanceof Carnivore) {
+      return herbivore.name;
+    }
+
+    herbivore.health -= 50;
+  }
 }
 
 module.exports = {
