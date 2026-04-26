a39afb03a89444d4644be994e7ee173621046244
Rostyslav` <hrostyslav@ukr.net`>
2026-04-26 21:07:46 +0300

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -10,27 +10,20 @@ class Animal {
   }
 
   checkTargetHealh(target) {
-    if(target.health <= 0) {
-      Animal.alive = Animal.alive.filter(animal => animal.health > 0);
+    if (target.health <= 0) {
+      Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
     }
   }
 }
 
 class Herbivore extends Animal {
-  constructor(name) {
-    super(name);
-    this.hidden = false;
-  }
+    hidden = false;
   hide() {
     this.hidden = true;
   }
 }
 
 class Carnivore extends Animal {
-  constructor(name) {
-    super(name);
-  }
-
   bite(target) {
     if (!(target instanceof Herbivore) || target.hidden) {
       return;
