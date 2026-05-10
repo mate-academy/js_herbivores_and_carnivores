87f750668aea4a714b40e5bce41cf0e3c507633c
Olesia Hryshchenko <gyygle18pp@gmail.com>
2026-05-10 18:05:25 +0300

    fix: rename checkLifeStatus method

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -11,11 +11,7 @@ class Animal {
   }
   checkLifeStatus() {
     if (this.health <= 0) {
-      const index = Animal.alive.indexOf(this);
-
-      if (index !== -1) {
-        Animal.alive.splice(index, 1);
-      }
+      Animal.alive = Animal.alive.filter((animal) => animal !== this);
     }
   }
 }
