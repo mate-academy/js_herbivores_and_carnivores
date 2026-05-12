645df9932c334e9095bb8329cb7b7fbb17141454
Goat <vova.cher@yahoo.com>
2026-05-12 15:12:16 +0300

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -39,10 +39,8 @@ class Carnivore extends Animal {
 
     // Якщо тварина померла
     if (animal.health <= 0) {
-      const index = Animal.alive.indexOf(animal);
-
-      if (index !== -1) {
-        Animal.alive.splice(index, 1);
+      if (animal.health <= 0) {
+        Animal.alive = Animal.alive.filter((a) => a !== animal);
       }
     }
   }
