4dbc131b96488350f6983af90f73014feb690cdd
Goat <vova.cher@yahoo.com>
2026-05-12 15:06:33 +0300

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,51 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    // Не кусаємо хижаків
+    if (animal instanceof Carnivore) {
+      return;
+    }
+
+    // Не кусаємо прихованих травоїдних
+    if (animal.hidden) {
+      return;
+    }
+
+    animal.health -= 50;
+
+    // Якщо тварина померла
+    if (animal.health <= 0) {
+      const index = Animal.alive.indexOf(animal);
+
+      if (index !== -1) {
+        Animal.alive.splice(index, 1);
+      }
+    }
+  }
 }
 
 module.exports = {
