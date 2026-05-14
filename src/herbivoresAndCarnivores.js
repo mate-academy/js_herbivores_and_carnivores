ac37a9ee366da978b51055d17ba01b18308df6c1
PrinceValue <princeossoa@gmail.com>
2026-05-14 19:01:14 +0300

    done

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,36 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health, hidden = false) {
+    super(name, health);
+    this.hidden = hidden;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(n) {
+    if (n instanceof Herbivore && n.hidden !== true) {
+      n.health -= 50;
+
+      if (n.health <= 0) {
+        Animal.alive.splice(Animal.alive.indexOf(n), 1);
+      }
+    }
+  }
 }
 
 module.exports = {
