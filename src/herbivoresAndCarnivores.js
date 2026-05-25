870674b01438e0e592a57e63a32e7128c4b67904
interpunktus <240490702+interpunktus@users.noreply.github.com>
2026-05-25 21:06:26 +0300

    try1

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,41 @@
 'use strict';
 
 class Animal {
-  // write your code here
-}
+  static alive = [];
 
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
+}
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(victim) {
+    if (victim.hidden === true || victim instanceof Carnivore) {
+      return;
+    }
+
+    victim.health -= 50;
+
+    if (victim.health <= 0) {
+      const index = Animal.alive.indexOf(victim);
+
+      if (index !== -1) {
+        Animal.alive.splice(index, 1);
+      }
+    }
+  }
 }
 
 module.exports = {
