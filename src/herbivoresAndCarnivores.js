61451275da8fced540143935da684ac9f017f267
Iryna Kaida <kaidairyna@gmail.com>
2026-06-04 14:38:01 +0300

    feat: add Animal.alive array and bite conditions

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,9 +1,12 @@
 'use strict';
 
 class Animal {
+  static alive = [];
+
   constructor(name) {
     this.health = 100;
     this.name = name;
+    Animal.alive.push(this);
   }
 }
 
@@ -19,7 +22,14 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(animal) {
+    if (animal instanceof Carnivore || animal.hidden === true) {
+      return;
+    }
     animal.health -= 50;
+
+    if (animal.health <= 0) {
+      Animal.alive = Animal.alive.filter((a) => a !== animal);
+    }
   }
 }
 
