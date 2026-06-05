249ffa3ee3f06574ab6fe971dbe5d3076bb56b9e
Adel Pereira <nic.gis.office@gmail.com>
2026-06-05 17:39:46 +0100

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -27,9 +27,7 @@ class Carnivore extends Animal {
       herbivore.health -= 50;
 
       if (herbivore.health <= 0) {
-        const idx = Animal.alive.indexOf(herbivore);
-
-        Animal.alive.splice(idx, 1);
+        Animal.alive = Animal.alive.filter((el) => el.health > 0);
       }
     }
   }
