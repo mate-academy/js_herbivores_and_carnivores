2307df17b3f57aed30ebb0568be8d8561c0a9421
Vadim <vadim.bazaliy228@gmail.com>
2026-06-22 17:32:55 +0300

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,19 +1,51 @@
 'use strict';
 
-class Animal {
-  // write your code here
+export class Animal {
+  static alive = [];
+
+  /**
+   * @param {string} name
+   */
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    Animal.alive.push(this);
+  }
+
+  // Допоміжний метод для перевірки здоров'я та видалення зі списку живих
+  checkLife() {
+    if (this.health <= 0) {
+      Animal.alive = Animal.alive.filter((animal) => animal !== this);
+    }
+  }
 }
 
-class Herbivore extends Animal {
-  // write your code here
+export class Herbivore extends Animal {
+  /**
+   * @param {string} name
+   */
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
-class Carnivore extends Animal {
-  // write your code here
-}
+export class Carnivore extends Animal {
+  /**
+   * @param {Animal} target
+   */
+  bite(target) {
+    if (target instanceof Carnivore || target.hidden) {
+      return;
+    }
 
-module.exports = {
-  Animal,
-  Herbivore,
-  Carnivore,
-};
+    target.health -= 50;
+
+    target.checkLife();
+  }
+}
