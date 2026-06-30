5917882c05cb301fdd84630ecc9a502c91721fbc
Sofiia Hryshko <sofiahryshko@gmail.com>
2026-06-30 21:54:13 +0300

    Update Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -34,13 +34,7 @@ class Carnivore extends Animal {
 
     herbivore.health -= 50;
 
-    if (herbivore.health <= 0) {
-      const index = Animal.alive.indexOf(herbivore);
-
-      if (index !== -1) {
-        Animal.alive.splice(index, 1);
-      }
-    }
+    Animal.alive = Animal.alive.filter((animal) => animal.health > 0);
   }
 }
 
