7574c3d50b1dc6572a728b44175b4366547cd9f3
Vladyslav Vyshnivskyi <vladyslav.vyshnivskyi@avanquest.com>
2026-07-04 13:56:49 +0300

    Add solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,37 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.hidden = false;
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    if (animal.hidden || animal instanceof Carnivore) {
+      return;
+    }
+
+    animal.health -= 50;
+
+    if (animal.health <= 0 && Animal.alive.indexOf(animal) !== -1) {
+      const index = Animal.alive.indexOf(animal);
+
+      Animal.alive.splice(index, 1);
+    }
+  }
 }
 
 module.exports = {
