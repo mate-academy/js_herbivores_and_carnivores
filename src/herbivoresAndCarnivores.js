0be67cf638d4b3818b1b36791f193741aad7081e
ExtymAndriy <petrovskiyandriyko@gmail.com>
2026-07-09 16:18:22 +0300

    Solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,53 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
+
+  dieIfNeeded() {
+    if (this.health <= 0) {
+      const idx = Animal.alive.indexOf(this);
+
+      if (idx !== -1) {
+        Animal.alive.splice(idx, 1);
+      }
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(target) {
+    // do nothing if target is a carnivore
+    if (target instanceof Carnivore) {
+      return;
+    }
+
+    // if target is hiding, bite has no effect
+    if (target.hidden) {
+      return;
+    }
+
+    if (typeof target.health === 'number') {
+      target.health -= 50;
+      target.dieIfNeeded();
+    }
+  }
 }
 
 module.exports = {
