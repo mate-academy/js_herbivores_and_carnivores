9a785e54a1aa78ab0c968aee399f9590494f24e4
YatsivDEN <stefanijul11@gmail.com>
2026-07-10 14:15:08 +0300

    Changed task due to review

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -28,7 +28,7 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(target) {
-    if (target instanceof Carnivore || target.hidden) {
+    if (!(target instanceof Herbivore) || target.hidden) {
       return;
     }
 
