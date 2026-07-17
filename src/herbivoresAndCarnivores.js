bc361ebccfbfb8e2e7450162a7a79e922d604b3e
Petrenko Oleksii <petrenko1694@gmail.com>
2026-07-17 20:05:02 +0300

    Herbivores-and-Carnivores

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,39 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(species) {
+    if (species instanceof Herbivore && species.hidden === false) {
+      species.health -= 50;
+    }
+
+    const dead = Animal.alive.find((el) => el.health <= 0);
+
+    if (dead) {
+      Animal.alive = Animal.alive.filter((el) => el !== dead);
+    }
+  }
 }
 
 module.exports = {
