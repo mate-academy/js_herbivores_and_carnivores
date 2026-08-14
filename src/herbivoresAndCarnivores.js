7cfaf27496c0b4c3fe21cbaa1602be2b738c7de0
ALex <sasha.hivuk@gmail.com>
2026-08-14 11:28:54 +0300

    task witch class

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,40 @@
 
 class Animal {
   // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 }
 
 class Herbivore extends Animal {
   // write your code here
+  constructor(name, health) {
+    super(name, health);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
   // write your code here
+  bite(target) {
+    if (target instanceof Herbivore && !target.hidden) {
+      target.health -= 50;
+
+      if (target.health <= 0) {
+        const indT = Animal.alive.indexOf(target);
+
+        Animal.alive.splice(indT, 1);
+      }
+    }
+  }
 }
 
 module.exports = {
