8397a00ba3e6647fefb579af1b7e97e692e8e452
sofiatsebenko <sofialtd50@gmail.com>
2026-08-21 07:04:28 +0300

    fixed

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,9 +2,10 @@
 
 class Animal {
   static alive = [];
-  constructor(health = 100, name) {
-    this.health = health;
+
+  constructor(name) {
     this.name = name;
+    this.health = 100;
 
     Animal.alive.push(this);
   }
@@ -26,14 +27,9 @@ class Carnivore extends Animal {
 
     animal.health -= 50;
 
-    if (animal.health <= 0) {
-      const index = Animal.alive.indexOf(animal);
-
-      Animal.alive.splice(index, 1);
-    }
+    Animal.alive = Animal.alive.filter(animal => animal.health > 0);
   }
 }
-
 
 
 module.exports = {
