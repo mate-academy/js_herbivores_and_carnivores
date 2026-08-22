3716d2f277614bf6a517a6aa9c7a1f8fd5216b40
Artem Mayboroda <artmay77777@gmail.com>
2026-08-22 22:29:53 +0300

    Format herbivores and carnivores solution

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -25,15 +25,12 @@ class Herbivore extends Animal {
 
 class Carnivore extends Animal {
   bite(animal) {
-    if (
-      animal instanceof Herbivore
-      && animal.hidden === false
-    ) {
+    if (animal instanceof Herbivore && animal.hidden === false) {
       animal.health -= 50;
 
       if (animal.health <= 0) {
         Animal.alive = Animal.alive.filter(
-          aliveAnimal => aliveAnimal !== animal,
+          (aliveAnimal) => aliveAnimal !== animal,
         );
       }
     }
