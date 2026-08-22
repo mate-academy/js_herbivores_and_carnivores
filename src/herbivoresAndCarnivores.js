46db2cfc7f5a921f49de86a10cc5af3deab9cb9f
kirushamatvieiev008 <mkiril087@gmail.com>
2026-08-22 15:45:01 +0300

    fixed #2

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -27,14 +27,14 @@ class Carnivore extends Animal {
     Animal.alive.push(this);
   }
   bite(animal) {
-    if (!(animal instanceof Carnivore) && !animal.hidden) {
+    if (animal instanceof Herbivore && !animal.hidden) {
       animal.health -= 50;
     }
 
     if (animal.health <= 0) {
-      Animal.alive = Animal.alive.filter((aliveAnimal) => {
-        return aliveAnimal.health > 0;
-      });
+      Animal.alive = Animal.alive.filter(
+        (aliveAnimal) => aliveAnimal.health > 0,
+      );
     }
   }
 }
