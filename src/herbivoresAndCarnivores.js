7323cafd58c8b8ede3bff9f0346e1d79545d6928
„Nikita <nikita.rysiev@gmail.com>
2026-08-22 14:03:35 +0200

    Changed in 'if' health loop

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -17,11 +17,7 @@ class Animal {
     this._health = value;
 
     if (this._health <= 0) {
-      const index = Animal.alive.indexOf(this);
-
-      if (index > -1) {
-        Animal.alive.splice(index, 1);
-      }
+      Animal.alive = Animal.alive.filter((animal) => animal !== this);
     }
   }
 }
