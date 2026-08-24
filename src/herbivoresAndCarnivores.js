e105489c26499ced466ae48179891dabd111be56
sofiatsebenko <sofialtd50@gmail.com>
2026-08-24 13:25:05 +0300

    fixed

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -27,7 +27,7 @@ class Carnivore extends Animal {
 
     animal.health -= 50;
 
-    Animal.alive = Animal.alive.filter(animal => animal.health > 0);
+    Animal.alive = Animal.alive.filter(a => a.health > 0);
   }
 }
 
