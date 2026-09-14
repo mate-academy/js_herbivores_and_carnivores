3dc62254ab56bf43df7809effde06efc20b13cbe
Balazs Radvanyi <balazs@riltech.co>
2026-09-14 12:34:30 +0200

    feat: implemented Animal class

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,46 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name, health = 100) {
+    this.health = health;
+    this.name = name;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  hidden = false;
+
+  constructor(name, health) {
+    super(name, health);
+
+    Animal.alive.push(this);
+  }
+
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health) {
+    super(name, health);
+
+    Animal.alive.push(this);
+  }
+
+  bite(prey) {
+    if (prey instanceof Carnivore || prey.hidden === true) {
+      return;
+    }
+
+    prey.health -= 50;
+
+    if (prey.health <= 0) {
+      Animal.alive = Animal.alive.filter((animal) => animal !== prey);
+    }
+  }
 }
 
 module.exports = {
